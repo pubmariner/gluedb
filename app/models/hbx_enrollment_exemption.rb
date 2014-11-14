7a045fa53fecfbc6bc955feed52a1aae5a160703
Dan Thomas <dan.thomas@dc.gov>
2014-11-14 16:56:53 -0500

    Rename class name and fields to comply with system conventions

--- app/models/hbx_enrollment_exemption.rb
+++ app/models/hbx_enrollment_exemption.rb
@@ -1,19 +1,16 @@
-class EnrollmentExemption
+class HbxEnrollmentExemption
   include Mongoid::Document
   include Mongoid::Timestamps
-  include Mongoid::Versioning
-  include Mongoid::Paranoia
 
-  # TYPES = %w[]
   KINDS = %W[hardship health_care_ministry_member incarceration indian_tribe_member religious_conscience]
 
-  auto_increment :_id
-  field :certificate_number, type: String
+  embedded_in :application_group
+
+  auto_increment :_id, seed: 9999
   field :kind, type: String
+  field :certificate_number, type: String
   field :start_date, type: Date
   field :end_date, type: Date
-
-  embedded_in :application_group
 
   embeds_many :comments
   accepts_nested_attributes_for :comments, reject_if: proc { |attribs| attribs['content'].blank? }, allow_destroy: true
