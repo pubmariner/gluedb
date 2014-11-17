9d37cfe9439cc3d062814f5dc3b2a6cfc964bb39
Dan Thomas <dan.thomas@dc.gov>
2014-11-16 19:14:52 -0500

    Add insurance assistance eligibility attributes.  Add HbxEnrollmentExemption as embedded parent.

--- app/models/applicant_link.rb
+++ app/models/applicant_link.rb
@@ -3,10 +3,15 @@ class ApplicantLink
 
   embedded_in :tax_household
   embedded_in :hbx_enrollment
-  embedded_in :eligibility_determinations
+  embedded_in :hbx_enrollment_exemption
+  embedded_in :eligibility_determination
 
   field :person_id, type: Moped::BSON::ObjectId
+  field :is_primary_applicant, type: Boolean
   field :is_active, type: Boolean, default: true
+
+  field :is_ia_eligible, type: Boolean, default: false
+  field :is_medicaid_chip_eligible, type: Boolean, default: false
 
   def person=(person_instance)
     return unless person_instance.is_a? Person
