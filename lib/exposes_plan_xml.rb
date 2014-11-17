4e427d488141a4422f19880d3548eb9c6bb99b8e
Trey Evans <lewis.r.evans@gmail.com>
2014-11-17 16:29:21 -0500

    Fixing plan parser.

--- lib/exposes_plan_xml.rb
+++ lib/exposes_plan_xml.rb
@@ -4,44 +4,48 @@ class ExposesPlanXml
   end
 
   def qhp_id
-    @parser.at_xpath('//ns1:qhp_id').text
+    @parser.at_xpath('//ns1:qhp_id',namespaces).text
   end
 
   def plan_exchange_id
-    @parser.at_xpath('//ns1:plan_exchange_id').text
+    @parser.at_xpath('//ns1:plan_exchange_id',namespaces).text
   end
 
   def carrier_id
-    @parser.at_xpath('//ns1:carrier_id').text
+    @parser.at_xpath('//ns1:carrier_id',namespaces).text
   end
 
   def carrier_name
-    @parser.at_xpath('//ns1:carrier_name').text
+    @parser.at_xpath('//ns1:carrier_name',namespaces).text
   end
 
   def name
-    @parser.at_xpath('//ns1:plan_name').text
+    @parser.at_xpath('//ns1:plan_name',namespaces).text
   end
 
   def coverage_type
-    @parser.at_xpath('//ns1:coverage_type').text
+    @parser.at_xpath('//ns1:coverage_type',namespaces).text
   end
 
   def original_effective_date
-    @parser.at_xpath('//ns1:original_effective_date').text
+    @parser.at_xpath('//ns1:original_effective_date',namespaces).text
   end
 
   def group_id
-    node = @parser.at_xpath('//ns1:group_id')
+    node = @parser.at_xpath('//ns1:group_id',namespaces)
     (node.nil?) ? nil : node.text
   end
 
   def metal_level_code
-    @parser.at_xpath('//ns1:metal_level_code').text
+    @parser.at_xpath('//ns1:metal_level_code',namespaces).text
   end
 
   def policy_number
-    node = @parser.at_xpath('//ns1:policy_number')
+    node = @parser.at_xpath('//ns1:policy_number',namespaces)
     (node.nil?) ? '' : node.text
+  end
+
+  def namespaces
+    { :ns1 => "http://dchealthlink.com/vocabulary/20131030/employer" }
   end
 end
