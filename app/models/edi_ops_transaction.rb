708510ec3704b1b29ff03cf52d71b074116cd393
Dan Thomas <dan.thomas@dc.gov>
2014-11-18 12:23:41 -0500

    Add aasm, comments and assignment attributes

--- app/models/edi_ops_transaction.rb
+++ app/models/edi_ops_transaction.rb
@@ -1,6 +1,8 @@
 class EdiOpsTransaction
   include Mongoid::Document
   include Mongoid::Timestamps
+  include AASM
+
 
   field :qualifying_reason_uri, type: String
   field :enrollment_group_uri, type: String
@@ -8,5 +10,28 @@ class EdiOpsTransaction
   field :event_key, type: String
   field :event_name, type: String
 
-  field :status, type: String
+  field :assigned_to, type: String
+  field :resolved_by, type: String
+
+  field :aasm_state, type: String
+
+  embeds_many :comments, cascade_callbacks: true
+  accepts_nested_attributes_for :comments, reject_if: proc { |attribs| attribs['content'].blank? }, allow_destroy: true
+
+  index({aasm_state:  1})
+
+  aasm do
+    state :open, initial: true
+    state :assigned
+    state :resolved
+
+    event assign: do
+      transitions from: :open, to: :assigned
+    end
+
+    event resolve: do
+      transitions from: :assigned, to: :resolved
+    end
+  end
+   
 end
