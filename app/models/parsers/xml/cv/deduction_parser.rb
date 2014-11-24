a6f6bca146933f37e3e2320ba4a1569d25629a95
Sagar Arlekar <sagar.arlekar@gmail.com>
2014-11-24 17:57:02 -0500

    corrected typo

--- app/models/parsers/xml/cv/deduction_parser.rb
+++ app/models/parsers/xml/cv/deduction_parser.rb
@@ -2,7 +2,7 @@ module Parsers::Xml::Cv
 
   class DeductionParser < Parsers::Xml::Cv::IncomeParser
 
-    tag 'deductions'
+    tag 'deduction'
 
     xpath_income_prefix = "cv:deduction"
     xpath_total_income_by_year_prefix = "cv:total_deductions_by_year"
