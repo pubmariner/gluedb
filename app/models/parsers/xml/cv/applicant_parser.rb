ed77b73fed9c3956dd563df57a737978e28700bc
Sagar Arlekar <sagar.arlekar@gmail.com>
2014-11-24 15:29:05 -0500

    changed boolean to string

--- app/models/parsers/xml/cv/applicant_parser.rb
+++ app/models/parsers/xml/cv/applicant_parser.rb
@@ -7,11 +7,11 @@ module Parsers::Xml::Cv
     tag 'applicant'
     namespace 'cv'
 
-    element :id, String, tag:'id/cv:id'
-    element :tax_household_id, String, xpath:'cv:id'
+
+    element :id, String, tag: "id/cv:id"
     has_one :person, Parsers::Xml::Cv::PersonParser, tag:'person'
     has_many :person_relationships, Parsers::Xml::Cv::PersonRelationshipParser, tag:'person_relationships'
     #element :person_demographics, Parsers::Xml::Cv::PersonDemographicsParser, tag:'person_demographics' TODO
-    element :is_primary_applicant, Boolean, tag:'is_primary_applicant'
+    element :is_primary_applicant, String, tag:'is_primary_applicant'
   end
 end
