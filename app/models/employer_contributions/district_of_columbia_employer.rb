43183ce0d81497581ac359e8759728be6f20b3c3
Trey Evans <lewis.r.evans@gmail.com>
2014-11-24 14:28:28 -0500

    Updated employer contribution amount calculations.
    The one dependency this has introduced is need for the passed in
    enrollees to have the reference premium supplied.

--- app/models/employer_contributions/district_of_columbia_employer.rb
+++ app/models/employer_contributions/district_of_columbia_employer.rb
@@ -7,5 +7,20 @@ module EmployerContributions
 
     validates_presence_of :employee_max_percent
     validates_presence_of :dependent_max_percent
+
+    def contribution_for(enrollment)
+      enrollment.enrollee.inject(0.00) do |total, en|
+        total += employer_contribution_for(en)
+      end
+    end
+
+    def employer_contribution_for(enrollee)
+      [reference_contribution_for(enrollee), enrollee.pre_amt].min
+    end
+
+    def reference_contribution_for(enrollee)
+      reference_percent = enrollee.subscriber? ? employee_max_percent : dependent_max_percent
+      enrollee.reference_premium * reference_percent * 0.01
+    end
   end
 end
