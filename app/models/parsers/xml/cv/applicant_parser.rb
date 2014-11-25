ed0c87794ce25dd776c62510a4a5596f16f809a3
Sagar Arlekar <sagar.arlekar@gmail.com>
2014-11-25 12:41:04 -0500

    added new fields

--- app/models/parsers/xml/cv/applicant_parser.rb
+++ app/models/parsers/xml/cv/applicant_parser.rb
@@ -7,13 +7,15 @@ module Parsers::Xml::Cv
     tag 'applicant'
     namespace 'cv'
 
-
     element :id, String, tag: "id/cv:id"
     has_one :person, Parsers::Xml::Cv::PersonParser, tag:'person'
     has_many :person_relationships, Parsers::Xml::Cv::PersonRelationshipParser, xpath:'cv:person_relationships'
     has_one :person_demographics, Parsers::Xml::Cv::PersonDemographicsParser, tag:'person_demographics'
     element :is_primary_applicant, String, tag: 'is_primary_applicant'
+    element :tax_household_id, String, tag: 'tax_household_id'
+    element :is_coverage_applicant, String, tag: 'is_coverage_applicant'
+    element :is_head_of_household, String, tag: 'is_head_of_household'
     has_many :financial_statements, Parsers::Xml::Cv::FinancialStatementParser, xpath:'cv:financial_statements'
-
+    element :is_active, String, tag: 'is_active'
   end
 end
