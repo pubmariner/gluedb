33e6e2c3f56b95af107c8b531d336c7cad44b743
Trey Evans <lewis.r.evans@gmail.com>
2014-11-25 15:23:26 -0500

    Do it all at once.

--- lib/import_application_groups.rb
+++ lib/import_application_groups.rb
@@ -6,9 +6,9 @@ class ImportApplicationGroups
 
   def run
     xml = Nokogiri::XML(File.open(@file_path))
-    ns = { :cv => "http://openhbx.org/api/terms/1.0"}
-    xml.xpath("//cv:application_groups/cv:application_group") do |node| 
-      ag = Parsers::Xml::Cv::ApplicationGroup.parse(node.canonicalize, :single => true)
+    ags = Parsers::Xml::Cv::ApplicationGroup.parse(xml.root.canonicalize)
+    ags.each do |ag|
+      puts ag.individual_requests
     end
   end
 
