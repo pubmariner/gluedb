04a5d23b356380a6cceff2ae3aabb84ecefa5f7b
Sagar Arlekar <sagar.arlekar@gmail.com>
2014-12-02 15:25:36 -0500

    now correctly parsing :tax_household_size_total_count

--- app/models/parsers/xml/cv/tax_household_parser.rb
+++ app/models/parsers/xml/cv/tax_household_parser.rb
@@ -9,7 +9,7 @@ module Parsers::Xml::Cv
 
     element :id, String, tag: 'id/cv:id'
     element :primary_applicant_id, String, tag: 'primary_applicant_id/cv:id'
-    element :tax_household_size, String, tag: 'tax_household_size'
+    element :tax_household_size_total_count, String, tag: 'tax_household_size/cv:total_count'
     element :total_incomes_by_year, String, tag: 'total_incomes_by_year'
     element :is_active, Boolean, tag:'is_active'
     has_many :tax_household_members, Parsers::Xml::Cv::TaxHouseholdMemberParser, tag: 'tax_household_members'
