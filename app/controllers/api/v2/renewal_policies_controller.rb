71e02795b4655368e9cb0c6426ba4001cfb7a938
Kevin Wei <kevin.tw.wei@gmail.com>
2014-12-04 15:18:35 -0500

    added total amounts to renewal partial. Removed index from controller.

--- app/controllers/api/v2/renewal_policies_controller.rb
+++ app/controllers/api/v2/renewal_policies_controller.rb
@@ -1,28 +1,9 @@
 class Api::V2::RenewalPoliciesController < ApplicationController
-  def index
-    clean_eg_id = Regexp.new(Regexp.escape(params[:enrollment_group_id].to_s))
-
-    search = {"eg_id" => clean_eg_id}
-    if(!params[:ids].nil? && !params[:ids].empty?)
-      search['_id'] = {"$in" => params[:ids]}
-    end
-
-    @policies = Policy.where(search)
-
-    page_number = params[:page]
-    page_number ||= 1
-    @policies = @policies.page(page_number).per(20)
-
-    Caches::MongoidCache.with_cache_for(Carrier) do
-      render "index"
-    end
-  end
 
   def show
     @policy = Policy.find(params[:id])
 
-    m_ids = @policy.enrollees.map(&:m_id)
-    member_repo = Caches::MemberCache.new(m_ids)
+    member_repo = Caches::MemberCache.new(@policy.enrollees.map(&:m_id))
     calc = Premiums::PolicyCalculator.new(member_repo)
 
     if @policy.is_shop?
