f6dab70dd849360e5fc0c57c4892fb0430702c21
Sagar Arlekar <sagar.arlekar@gmail.com>
2014-12-04 15:22:39 -0500

    parser for 'income by year'

---
+++ app/models/parsers/xml/cv/income_by_year_parser.rb
@@ -0,0 +1,20 @@
+module Parsers::Xml::Cv
+
+  class IncomeByYearParser
+    include HappyMapper
+
+    register_namespace "cv", "http://openhbx.org/api/terms/1.0"
+    tag 'total_income_by_year'
+    namespace 'cv'
+
+    element :calendar_year, String, tag: 'calendar_year'
+    element :total_amount, String, tag: 'total_amount'
+
+    def to_hash
+      {
+          calendar_year: calendar_year,
+          total_amount: total_amount
+      }
+    end
+  end
+end
