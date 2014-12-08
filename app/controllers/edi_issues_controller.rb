5a0f2cb949733f6ed4d7619a28d7283648b3912f
Kevin Wei <kevin.tw.wei@gmail.com>
2014-12-08 16:46:56 -0500

    simplifying edi issues controller

--- app/controllers/edi_issues_controller.rb
+++ app/controllers/edi_issues_controller.rb
@@ -1,76 +1,20 @@
 class EdiIssuesController < ApplicationController
-###helper_method :sort_column, :sort_direction  
+  before_filter :select_issue, only: [:edit, :update]
 
   def index
-
-    @q = params[:q]
-      sort_col, direction = "enrollment_id", "desc"
-      sort_col  = params[:sort] if params[:sort]
-      direction = params[:direction] if params[:direction]
-      
-      
-        if params[:aasm_state] == "open"
-          edi_issues = EdiOpsTransaction.where(aasm_state: "open")
-        else 
-          if params[:aasm_state] == "close"
-            edi_issues = EdiOpsTransaction.where(aasm_state: "close")
-          else
-            if params[:aasm_state] == "Assigned"
-              edi_issues = EdiOpsTransaction.where(aasm_state: "assigned")
-            else
-               edi_issues = EdiOpsTransaction.all 
-            end   
-          end
-        end
-       
-         
-      if params[:sort] 
-         if params[:direction] == "asc"
-           @edi_issues = edi_issues.sort!{|e, f| e.enrollment_id <=> f.enrollment_id}
-         else
-           @edi_issues = edi_issues.sort!{|e, f| f.enrollment_id <=> e.enrollment_id}
-         end   
-      else
-        @edi_issues = edi_issues
-      end  
-
-    
-    
-      respond_to do |format|
-        format.html # index.html.erb
-        format.json { render json: @edi_issues }
-      end
-  end  
-
-  def show
-   @edi_issue = EdiOpsTransaction.find(params[:id])
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @edi_issue }
-    end
+    @edi_issues = EdiOpsTransaction.all.by_oldest
   end
 
-  
-  def edit
-     @edi_issue =  EdiOpsTransaction.find(params[:id])
-
-  end  
-
-
+  def show
+    @edi_issue = EdiOpsTransaction.find(params[:id])
+  end
 
   def update
-    @edi_issue =  EdiOpsTransaction.find(params[:id])
+    @task.update_attributes(task_params)
+  end
 
-    if @edi_issue.update_attributes(params[:edi_ops_transaction])
-      redirect_to edi_issues_path
-    else
-      render "edit"
-    end 
-     
-  end  
-
-
-
-
+  private
+    def set_tasks
+      @edi_issue = EdiOpsTransaction.find(params[:id])
+    end
 end
