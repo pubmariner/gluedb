514caf6bbfe436187e6456f1e721b1b5cf375a10
Kevin Wei <kevin.tw.wei@gmail.com>
2014-12-15 15:51:46 -0500

    EDI Ops Queue refactoring. Match Views and Controllers with Model name. Fix sorting on index. Fix default sort order. Allow assigning and resolving tickets to automatically update state.

---
+++ app/controllers/edi_ops_transactions_controller.rb
@@ -0,0 +1,34 @@
+class EdiOpsTransactionsController < ApplicationController
+  helper_method :sort_column, :sort_direction, :edi_ops_transaction_path
+
+  def index
+    @q = params[:q]
+    @qf = params[:qf]
+    @qd = params[:qd]
+    @edi_ops_transactions = EdiOpsTransaction.order_by(sort_column.to_sym.send(sort_direction)).search(@q, @qf, @qd).page(params[:page]).per(20)
+  end
+
+  def edit
+    @edi_ops_transaction = EdiOpsTransaction.find(params[:id])
+  end
+
+  def update
+    @edi_ops_transaction =  EdiOpsTransaction.find(params[:id])
+
+    if @edi_ops_transaction.update_attributes(params[:edi_ops_transaction])
+      redirect_to edi_ops_transactions_path
+    else
+      render "edit"
+    end
+
+  end
+
+  private
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+    end
+
+    def sort_column
+      EdiOpsTransaction.fields.include?(params[:sort]) ? params[:sort] : "submitted_timestamp"
+    end
+end
