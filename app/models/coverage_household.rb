cb7b598929b6c46111d4dec154a0f6412688c876
Sagar Arlekar <sagar.arlekar@gmail.com>
2014-12-18 18:06:25 -0500

    validates :presence_of_coverage_household_members

--- app/models/coverage_household.rb
+++ app/models/coverage_household.rb
@@ -6,12 +6,20 @@ class CoverageHousehold
   # The pool of all applicants eligible for enrollment during a certain time period
 
   embedded_in :household
-  
+
   field :submitted_at, type: DateTime
 
   embeds_many :coverage_household_members
   accepts_nested_attributes_for :coverage_household_members
 
+
+  validates :presence_of_coverage_household_members
+
+  def coverage_household_members
+    if self.coverage_household_members.size == 0
+      self.errors.add(:base, "Should have atleast one coverage_household_member")
+    end
+  end
 
   def application_group
     return nil unless household
@@ -21,5 +29,6 @@ class CoverageHousehold
   def applicant_ids
     coverage_household_members.map(&:applicant_id)
   end
+
 
 end
