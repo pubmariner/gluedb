4df0dd001d0afaec180a7214124f15015e625376
Sagar Arlekar <sagar.arlekar@gmail.com>
2014-12-19 15:43:44 -0500

    1) using save_list to saved embedded docs 2) moving person_demographics from applicant to member 3) assigning applicant_id to coverage_household_member

--- lib/application_group_builder.rb
+++ lib/application_group_builder.rb
@@ -2,7 +2,10 @@ class ApplicationGroupBuilder
 
   attr_reader :application_group
 
+  attr_reader :save_list
+
   def initialize(param, person_mapper)
+    @save_list = []
     @is_update = true # we assume that this is a update existing application group workflow
     @applicants_params = param[:applicants]
     param = param.slice(:e_case_id, :submitted_at, :e_status_code, :application_type)
@@ -12,25 +15,39 @@ class ApplicationGroupBuilder
     if @application_group.nil?
       @application_group = ApplicationGroup.new(param) #we create a new application group from the xml
       @is_update = false # means this is a create
+      add_irsgroup # we need a atleast 1 irsgroup hence adding a blank one
     end
 
     @application_group.updated_by = "curam_system_service"
 
     get_household
-    add_irsgroup # we need a atleast 1 irsgroup hence adding a blank one
   end
 
   def add_applicant(applicant_params)
 
     if @application_group.applicants.map(&:person_id).include? applicant_params[:person].id
+      puts "Added already existing applicant"
       applicant = @application_group.applicants.where(person_id:applicant_params[:person].id).first
     else
-
+      puts "Added a new applicant"
       applicant = @application_group.applicants.build(filter_applicant_params(applicant_params))
-
+      member = applicant.person.members.select do |m| m.authority? end.first
+      set_person_demographics(member, applicant_params[:person_demographics])
+      @save_list << member
+      @save_list << applicant
     end
 
     applicant
+  end
+
+  def set_person_demographics(member, person_demographics_params)
+    member.dob = person_demographics_params["dob"] if person_demographics_params["dob"]
+    member.death_date = person_demographics_params["death_date"] if person_demographics_params["death_date"]
+    member.ssn = person_demographics_params["ssn"] if person_demographics_params["ssn"]
+    member.gender = person_demographics_params["gender"] if person_demographics_params["gender"]
+    member.ethnicity = person_demographics_params["ethnicity"] if person_demographics_params["ethnicity"]
+    member.race = person_demographics_params["race"] if person_demographics_params["race"]
+    member.marital_status = person_demographics_params["marital_status"] if person_demographics_params["marital_status"]
   end
 
   def filter_applicant_params(applicant_params)
@@ -38,7 +55,6 @@ class ApplicationGroupBuilder
         :is_primary_applicant,
         :is_coverage_applicant,
         :is_head_of_household,
-        :person_demographics,
         :person)
     applicant_params.delete_if do |k, v|
       v.nil?
@@ -49,28 +65,32 @@ class ApplicationGroupBuilder
 
     return @household if @household
 
-    puts "households #{self.application_group.households.inspect}"
-
     if !@is_update
-      puts "Updating"
+      puts "New Application Group Case"
       @household = self.application_group.households.build #if new application group then create new household
+      @save_list << @household
     elsif have_applicants_changed?
-      puts "have_applicants_changed?"
+      puts "Update Application Group Case - Applicants have changed. Creating new household"
       @household = self.application_group.households.build #if applicants have changed then create new household
+      @save_list << @household
     else
-      puts "else?"
+      puts "Update Application Group Case. Using latest household."
       #TODO to use .is_active household instead of .last
-      @household = self.application_group.latest_coverage_household #if update and applicants haven't changed then use the latest household in use
+      @household = self.application_group.households.last #if update and applicants haven't changed then use the latest household in use
     end
-
-    puts "households #{self.application_group.households.inspect}"
 
     return @household
 
   end
 
   def have_applicants_changed?
-    if @application_group.applicants.map(&:id).sort == @applicants_params.map do |applicants_param| applicants_param[:applicant_id] end.sort
+    current_list = @application_group.applicants.map do |applicant| applicant.person_id end.sort
+    new_list = @applicants_params.map do |applicants_param| applicants_param[:person].id end.sort
+
+    puts current_list.inspect
+    puts new_list.inspect
+
+    if current_list == new_list
       return false
     else
       return true
@@ -82,7 +102,10 @@ class ApplicationGroupBuilder
     coverage_household = @household.coverage_households.build({submitted_at: Time.now})
 
     @application_group.applicants.each do |applicant|
-      coverage_household.coverage_household_members << applicant if applicant.is_coverage_applicant
+      if applicant.is_coverage_applicant
+        coverage_household_member = coverage_household.coverage_household_members.build
+        coverage_household_member.applicant_id = applicant.id
+      end
     end
 
   end
@@ -93,8 +116,8 @@ class ApplicationGroupBuilder
 
       hbx_enrollement = @household.hbx_enrollments.build
       hbx_enrollement.policy = policy
-      hbx_enrollement.employer = Employer.find(policy.employer_id) unless policy.employer_id.blank?
-      hbx_enrollement.broker   = Broker.find(policy.broker_id) unless policy.broker_id.blank?
+      #hbx_enrollement.employer = Employer.find(policy.employer_id) unless policy.employer_id.blank?
+      #hbx_enrollement.broker   = Broker.find(policy.broker_id) unless policy.broker_id.blank?
       #hbx_enrollement.primary_applicant = alpha_person
       #hbx_enrollement.allocated_aptc_in_dollars = policy.allocated_aptc
       hbx_enrollement.enrollment_group_id = policy.eg_id
@@ -114,7 +137,7 @@ class ApplicationGroupBuilder
           applicant = @application_group.find_applicant_by_person(person)
 
           hbx_enrollement_member = hbx_enrollement.hbx_enrollment_members.build({applicant: applicant,
-                                                         premium_amount_in_dollars: enrollee.pre_amt})
+                                                         premium_amount_in_cents: enrollee.pre_amt})
           hbx_enrollement_member.is_subscriber = true if (enrollee.rel_code == "self")
 
         rescue FloatDomainError
