650697067e9f0634c0ff289afff7c9a4a63401d9
Trey Evans <lewis.r.evans@gmail.com>
2014-12-19 15:59:37 -0500

    Adding template models.

--- app/models/pdf_templates/notice_input.rb
+++ app/models/pdf_templates/notice_input.rb
@@ -3,10 +3,16 @@ module PdfTemplates
     include Virtus.model
 
     attribute :primary_name, String
+    attribute :primary_identifier, String
+    attribute :primary_address, PdfTemplates::NoticeAddress
 
     attribute :covered_individuals, Array[String]
 
+    attribute :health_plan_name, String
+    attribute :dental_plan_name, String
     attribute :health_premium, String
     attribute :dental_premium, String
+
+    attribute :notice_date, Date
   end
 end
