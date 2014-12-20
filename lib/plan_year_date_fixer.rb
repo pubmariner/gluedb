c67c6560f854b91547581c8f34e19b9ea82e5aaa
Sagar Arlekar <sagar.arlekar@gmail.com>
2014-12-19 19:36:27 -0500

    utility of fix nil end dates in PlanYear

---
+++ lib/plan_year_date_fixer.rb
@@ -0,0 +1,18 @@
+class PlanYearDateFixer
+
+  def initialize(plan_year)
+    @plan_year = plan_year
+  end
+
+  # will set end_date to 31st Dec of year. Year is taken from start_date.
+  def fix_end_date
+    if @plan_year.end_date.nil?
+      year = @plan_year.start_date.year
+      @plan_year.end_date = Date.new(year, 12, 31)
+    end
+    @plan_year
+  end
+end
+
+#plan_year = PlanYear.first
+#puts PlanYearDateFixer.new(plan_year).fix_end_date.inspect
