3c71fd289e348fdc3ff5437a6e7b933f359fe70a
Trey Evans <lewis.r.evans@gmail.com>
2014-12-19 19:40:17 -0500

    Fixing querying.

--- app/models/policy_queries.rb
+++ app/models/policy_queries.rb
@@ -5,15 +5,10 @@ class PolicyQueries
     }
   end
   def self.without_aptc
-    {
-      "$or" => [
-        {"applied_aptc" => {
-          "$in" => [nil, "0.0", "0.00", "0"]
-        }},
-        {"applied_aptc" => {
-          "$exists" => false
-        }}
-      ]
+    { 
+      "applied_aptc" => {
+         "$in" => [nil, "0.0", "0.00", "0"] 
+      }
     }
   end
 end
