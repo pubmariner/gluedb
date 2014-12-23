47f29356d2335610aebcf19f9aa1fdcdcbf6ac93
Trey Evans <lewis.r.evans@gmail.com>
2014-12-23 10:03:13 -0500

    Updating deployment.

--- script/queries/calculate_gap_policies.rb
+++ script/queries/calculate_gap_policies.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-pols = Policy.where(PolicyStatus::Active.as_of(Date.parse("12/31/2014")).query)
+pols = Policy.where(PolicyStatus::Active.as_of(Date.parse("12/31/2014")).query).where({:employer_id => nil})
 
 p_map = Person.collection.aggregate([{"$unwind"=> "$members"}, {"$project" => {"_id" => 0, member_id: "$members.hbx_member_id", person_id: "$_id"}}])
 
@@ -19,6 +19,7 @@ next_year_pols = Policy.where({
     :rel_code => "self",
     :coverage_start => {"$gt" => Date.new(2014,12,31)}
   }}})
+
 next_year_pols.each do |pol|
   ct = ct_cache.lookup(pol.plan_id)
   sub_person = p_repo[pol.subscriber.m_id]
@@ -34,32 +35,19 @@ gap_policies = []
 pols.each do |pol|
   if pol.subscriber.coverage_end.blank?
     if !pol.subscriber.coverage_start.blank?
-        ct = ct_cache.lookup(pol.plan_id)
-        sub_person = p_repo[pol.subscriber.m_id]
-        begin
-            if (!excluded_people.include?([sub_person, ct]))
-              if (pol.subscriber.coverage_start > Date.new(2014,11,30))
-                gap_policies << pol.id
-              end
-            end
-        rescue
-          raise([sub_person.id, pol.id].inspect)
+      ct = ct_cache.lookup(pol.plan_id)
+      sub_person = p_repo[pol.subscriber.m_id]
+      begin
+        if (!excluded_people.include?([sub_person, ct]))
+          if (pol.subscriber.coverage_start > Date.new(2014,11,30))
+            gap_policies << pol.id
+          end
         end
+      rescue
+        raise([sub_person.id, pol.id].inspect)
+      end
     end
   end
 end
 
-CSV.open("dec_policies.csv", "w") do |csv|
-  csv << ["Enrollment Group", "HBX", "SSN", "DOB", "First", "Middle", "Last"]
-  gap_policies.each do |gp|
-    pol = Policy.find(gp)
-    subscriber = pol.subscriber
-    s_person = subscriber.person
-    auth_member = subscriber.person.authority_member
-    csv << [
-     pol.eg_id,
-     auth_member.hbx_member_id, auth_member.ssn, auth_member.dob,
-     s_person.name_first, s_person.name_middle, s_person.name_last
-    ]
-  end
-end
+puts gap_policies.length
