6a7de2d4226cf90bc2315f739b71c994843619b7
Trey Evans <lewis.r.evans@gmail.com>
2014-12-23 12:04:08 -0500

    Fixing issue with publisher.

--- app/models/services/policy_publisher.rb
+++ app/models/services/policy_publisher.rb
@@ -1,7 +1,7 @@
 module Services
   class PolicyPublisher
     def self.publish_cancel(p_id)
-      ::Workflow::CancelPolicy.new.call(pid)
+      ::Workflow::CancelPolicy.new.call(p_id)
     end
 
     def self.publish(q_reason_uri, p_id)
