0529e6576bd0b5bd8d8a46f008979d2a745a5a73
Raghuram <raghuramg83@gmail.com>
2015-01-02 15:22:54 -0500

    renewal reports buider code updates

--- lib/renewals_generator.rb
+++ lib/renewals_generator.rb
@@ -25,38 +25,31 @@ class RenewalsGenerator
       @folder_name = "DCHBX_#{Date.today.strftime('%d_%m_%Y')}_#{folder_number}"
       Dir.mkdir "#{Rails.root.to_s}/#{@type}_pdf_reports/#{@folder_name}"
 
+
       member_ids[lower_bound..upper_bound].each do |m_id|
         member_policies = policies.by_member_id(m_id)
+
         if @type == 'qhp'
           member_policies += uqhp_policies.by_member_id(m_id).select{|x| x.plan.coverage_type == 'dental'}
         else
           matched = qhp_policies.by_member_id(m_id)
           if matched.count > 0
-            matched += member_policies.select{|x| x.plan.coverage_type == 'dental'} 
-            policy_hash = matched.inject({}) do |data, policy|
-              data["#{policy.subscriber.m_id}-#{policy.plan.hios_plan_id}"] = policy
-              data
-            end
-            non_duplicate_policies = policy_hash.values
-            dentals = group_policies_for_noticies(non_duplicate_policies).inject([]) do |data, policies|
+            matched += member_policies.select{|x| x.plan.coverage_type == 'dental'}
+            dentals = group_policies_for_noticies(unique_policies(matched)).inject([]) do |data, policies|
               data << (policies[0].nil? ? nil : policies[1])
-            end   
+            end
             if dentals && dentals.compact.size > 0
               member_policies = member_policies.uniq - dentals.compact.uniq
             end
           end
         end
 
-        policy_hash = member_policies.inject({}) do |data, policy|
-          data["#{policy.subscriber.m_id}-#{policy.plan.hios_plan_id}"] = policy
-          data
-        end
-
-        non_duplicate_policies = policy_hash.values
-        group_policies_for_noticies(non_duplicate_policies).each do |policies|
+        group_policies_for_noticies(unique_policies(member_policies)).each do |policies|
           begin
             next if (@type == 'qhp') && policies[0].nil?
-            notice = build_input_object(policies)
+            renewal_notice = Generators::Reports::RenewalNoticeInput.new
+            notice = renewal_notice.create(policies)
+            @hbx_member_id = notice.primary_identifier
             write_report(notice)
           rescue Exception => e
             @logger.info "#{policies.inspect}-----#{e.inspect}"
@@ -71,66 +64,14 @@ class RenewalsGenerator
     end
   end
 
-  def build_input_object(policies)
-    health, dental = policies
-    notice = PdfTemplates::NoticeInput.new
-    base_policy = health || dental
-
-    if base_policy.subscriber.nil?
-      raise 'no subscriber present' 
+  def unique_policies(policies)
+    policy_hash = policies.inject({}) do |data, policy|
+      data["#{policy.subscriber.m_id}-#{policy.plan.hios_plan_id}"] = policy
+      data
     end
 
-    primary = base_policy.subscriber.person
-    members = base_policy.enrollees_sans_subscriber.map{|enrollee| enrollee.person.name_full}
-
-    notice.primary_name = primary.name_full
-    notice.primary_identifier = primary.authority_member.hbx_member_id
-    @hbx_member_id = notice.primary_identifier
-    if primary.addresses.empty?
-      raise 'subscriber address empty'
-    end
-
-    append_address(primary, notice)
-    notice.covered_individuals = members
-    notice = append_health_policy(health, notice) if health
-    notice = append_dental_policy(dental, notice) if dental
-
-    return notice
+    policy_hash.values
   end
-
-  def append_address(primary, notice)
-    primary_address = primary.addresses[0]
-    address = PdfTemplates::NoticeAddress.new
-    address.street_1 = primary_address.address_1
-    address.street_2 = primary_address.address_2
-    address.city = primary_address.city
-    address.state = primary_address.state
-    address.zip = primary_address.zip
-    notice.primary_address = address
-    notice
-  end
-
-  def append_health_policy(health, notice)
-    notice.health_plan_name = health.plan.name
-    notice.health_premium = health.pre_amt_tot
-    if @type == 'qhp'
-      notice.health_aptc = health.applied_aptc
-      notice.health_responsible_amt = health.tot_res_amt
-    end
-    notice    
-  end
-
-  def append_dental_policy(dental, notice)
-    notice.dental_plan_name = dental.plan.name
-    notice.dental_premium = dental.pre_amt_tot
-    if @type == 'qhp'
-      notice.dental_aptc = dental.applied_aptc
-      notice.dental_responsible_amt = dental.tot_res_amt
-    end
-    notice
-  end
-
-  private
 
   def group_policies_for_noticies(policies)
     health_policies = policies.select{|x| x.plan.coverage_type == 'health'}
@@ -157,6 +98,8 @@ class RenewalsGenerator
       policy_groups + dental_policies.map{|dental| [nil, dental]}
     end
   end
+
+  private
 
   def write_report(notice)
     pdf = Generators::Reports::Renewals.new(notice, @type)
