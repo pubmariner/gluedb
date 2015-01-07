c3b9fdfa017bab2c47e6621630a07bdca720aa9a
Trey Evans <lewis.r.evans@gmail.com>
2015-01-07 10:13:33 -0500

    Fixing to filter easy duplicates.

---
+++ script/queries/duplicate_2015s.rb
@@ -0,0 +1,64 @@
+require 'csv'
+p_repo = {}
+
+p_map = Person.collection.aggregate([{"$unwind"=> "$members"}, {"$project" => {"_id" => 0, member_id: "$members.hbx_member_id", person_id: "$_id"}}])
+
+ct_cache = Caches::Mongoid::SinglePropertyLookup.new(Plan, "coverage_type")
+
+p_map.each do |val|
+  p_repo[val["member_id"]] = val["person_id"]
+end
+
+next_year_pols = Policy.where({
+  :enrollees => {"$elemMatch" => {
+    :rel_code => "self",
+    :coverage_start => {"$gt" => Date.new(2014,12,31)},
+    :coverage_end => nil
+  }}})
+
+p_count = Hash.new 
+
+next_year_pols.each do |pol|
+  ct = ct_cache.lookup(pol.plan_id)
+  p_id = p_repo[pol.subscriber.m_id]
+  if !p_count.has_key?(p_id)
+    p_count[p_id] = {:health => 0, :dental => 0 , :healths => [], :dentals =>[]} 
+  end
+  p_count[p_id][ct.to_sym] = p_count[p_id][ct.to_sym] + 1
+  p_count[p_id][(ct + "s").to_sym] = p_count[p_id][(ct + "s").to_sym].push(pol.plan_id).uniq
+end
+
+duplicate_dentals = []
+duplicate_healths = []
+
+dupes = p_count.select do |k,v|
+  selected = false
+  if (v[:health] > 1)
+    if v[:health] > v[:healths].length
+      duplicate_healths << [k, v]
+      selected = true
+    end
+  end
+  if (v[:dental] > 1)
+    if v[:dental] > v[:dentals].length
+      duplicate_dentals << [k, v]
+      selected = true
+    end
+  end
+  selected
+end
+
+puts duplicate_dentals.keys.inspect
+puts duplicate_healths.keys.inspect
+
+puts dupes.keys.length
+=begin
+CSV.open("people_with_duplicate_2015s.csv", "w") do |csv|
+  csv << ["HBX ID", "First", "Last", "DOB", "SSN", "Health Policies", "Dental Policies"]
+  dupes.each do |k, v|
+    per = Person.find(k)
+    auth_mem = per.authority_member
+    csv << [auth_mem.hbx_member_id, per.name_first, per.name_last, auth_mem.dob.strftime("%Y-%m-%d"), auth_mem.ssn, v[:health], v[:dental]]
+  end
+end
+=end
