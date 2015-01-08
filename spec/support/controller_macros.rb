b91affff842281c0042d362f7c94f4e91187d274
Kevin Wei <kevin.tw.wei@gmail.com>
2015-01-08 15:13:11 -0500

    Fix spec for authorization lock on employer resource

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -2,7 +2,7 @@ module ControllerMacros
   def login_user
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      user = create :user
+      user = create :user, :edi_ops
       sign_in user
     end
   end
