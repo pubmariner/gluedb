9a48a9e2140843c5735f8c7c00989054131c1d69
Trey Evans <lewis.r.evans@gmail.com>
2015-01-08 16:48:14 -0500

    Fixing dob of string issue.

--- app/models/person_match_strategies/first_last_dob.rb
+++ app/models/person_match_strategies/first_last_dob.rb
@@ -3,7 +3,8 @@ module PersonMatchStrategies
     def match(options = {})
       name_first_regex = Regexp.compile(Regexp.escape(options[:name_first].to_s.strip.downcase), true)
       name_last_regex = Regexp.compile(Regexp.escape(options[:name_last].to_s.strip.downcase), true)
-      found_people = Person.where({"members.dob" => options[:dob], "name_first" => name_first_regex, "name_last" => name_last_regex})
+      search_dob = cast_dob(options[:dob])
+      found_people = Person.where({"members.dob" => search_dob, "name_first" => name_first_regex, "name_last" => name_last_regex})
       if found_people.any?
         if found_people.many?
           raise AmbiguousMatchError.new("Multiple people with same first, last, and dob: #{options[:name_first]}, #{options[:name_last]}, #{options[:dob]}")
@@ -13,6 +14,15 @@ module PersonMatchStrategies
       else
         [nil, nil]
       end
+    end
+
+    def cast_dob(dob)
+      if dob.kind_of?(Date)
+        return dob
+      elsif dob.kind_of?(DateTime)
+        return dob
+      end
+      Date.parse(dob)
     end
 
     def select_authority_member(person, options)
