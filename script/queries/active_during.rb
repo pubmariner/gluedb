9ed64bd5c847f41768821e3309d04d0005312f2e
Trey Evans <lewis.r.evans@gmail.com>
2015-01-09 11:28:41 -0500

    Fixing query.

--- script/queries/active_during.rb
+++ script/queries/active_during.rb
@@ -1,3 +1,7 @@
-pols = PolicyStatus::Active.between(Date.new(2013,12,31), Date.new(2014,12,31)).results
+plans = Plan.where({:metal_level => {"$not" => /catastrophic/i}, :coverage_type => /health/i}).map(&:id)
+
+pols = PolicyStatus::Active.between(Date.new(2013,12,31), Date.new(2014,12,31)).results.where({
+  :plan_id => {"$in" => plans}, :employer_id => nil
+})
 
 puts pols.count
