493966c5f30571da985e1ab42552a345e32c4c5b
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-01-14 16:54:14 -0500

    validate :integrity_of_coverage_household_members

--- app/models/coverage_household.rb
+++ app/models/coverage_household.rb
@@ -14,6 +14,8 @@ class CoverageHousehold
 
   validate :presence_of_coverage_household_members
 
+  validate :integrity_of_coverage_household_members
+
   def presence_of_coverage_household_members
     if self.coverage_household_members.size == 0
       self.errors.add(:base, "Should have atleast one coverage_household_member")
@@ -29,5 +31,23 @@ class CoverageHousehold
     coverage_household_members.map(&:applicant_id)
   end
 
+  def integrity_of_coverage_household_members
+
+    return unless application_group
+
+    people_in_coverage_household = self.applicants.flat_map(&:person) - [nil]
+
+    enrollees = application_group.primary_applicant.person.policies.flat_map(&:enrollees).uniq
+
+    people_in_policies = enrollees.map do |enrollee|
+      Person.find_by_member_id(enrollee.m_id)
+    end
+
+    same_people = people_in_coverage_household.map(&:id).uniq.sort == people_in_policies.map(&:id).uniq.sort
+
+    unless same_people
+      self.errors.add(:base, "Applicants in coverage household are not the same as people covered in policies")
+    end
+  end
 
 end
