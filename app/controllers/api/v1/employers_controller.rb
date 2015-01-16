a4b5bad853bea3a7a7c5dd2d338b6591521be142
Kevin Wei <kevin.tw.wei@gmail.com>
2015-01-16 10:41:40 -0500

    v1 group version of employer endpoint accepting multiple FEIN's

--- app/controllers/api/v1/employers_controller.rb
+++ app/controllers/api/v1/employers_controller.rb
@@ -16,4 +16,15 @@ class Api::V1::EmployersController < ApplicationController
   def old_cv
     @employer = Employer.find(params[:id])
   end
+
+  def old_group_index
+    clean_fein = Regexp.new(Regexp.escape(params[:feins].to_s))
+
+    search = {'fein' => clean_fein}
+    if(!params[:feins].nil? && !params[:feins].empty?)
+      search["fien"] = {"$in" => params[:feins]}
+    end
+
+    @employers = Employer.where(search)
+  end
 end
