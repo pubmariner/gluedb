457465dcb7d63ed373c3f08e845276d3e3c0ea70
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-01-21 16:59:16 -0500

    new factories

---
+++ spec/factories/hbx_enrollment.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :hbx_enrollment do
+    kind 'unassisted_qhp'
+    enrollment_group_id 'John'
+    applied_aptc_in_cents 1000
+    elected_aptc_in_cents 1000
+    is_active true
+    submitted_at DateTime.today
+
+    after(:create) do |hbx_enrollment|
+
+    end
+  end
+end
