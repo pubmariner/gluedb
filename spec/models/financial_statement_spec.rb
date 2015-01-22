df5dba7043b1f9eaab50241368ae7d735a439e8c
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-01-22 12:37:10 -0500

    added new specs. corrected old spec.

--- spec/models/financial_statement_spec.rb
+++ spec/models/financial_statement_spec.rb
@@ -3,6 +3,8 @@ require 'active_support/time'
 
 describe FinancialStatement do
 
+  TEN_DOLLARS = 1000
+
   before(:all) do
 
     @financial_statement = FinancialStatement.new
@@ -47,6 +49,7 @@ describe FinancialStatement do
     income4.end_date = nil
     income4.frequency = "monthly"
 
+    @income5 = income1
 
     @financial_statement.incomes << income1 << income2 << income3 << income4
 
@@ -59,12 +62,62 @@ end
 
   context "yearwise income computation" do
 
+    it "should compute the income correctly with end_date as nil" do
+
+      financial_statement = FinancialStatement.new
+      income1 = Income.new
+      income1.amount_in_cents = 111400
+      income1.start_date = Date.new(2012, 04, 12)
+      income1.end_date = nil
+      income1.frequency = "monthly"
+
+      financial_statement.incomes << income1
+
+      income_hash = financial_statement.compute_yearwise(financial_statement.incomes)
+
+      expect(((income_hash[2014]) - 1336800).abs).to be < TEN_DOLLARS
+
+    end
+
+    it "should compute prorated income" do
+
+      financial_statement = FinancialStatement.new
+      income1 = Income.new
+      income1.amount_in_cents = 10000
+      income1.start_date = Date.new(2012, 01, 15)
+      income1.end_date = Date.new(2012, 02, 14)
+      income1.frequency = "monthly"
+
+      financial_statement.incomes << income1
+
+      income_hash = financial_statement.compute_yearwise(financial_statement.incomes)
+
+      puts "income_hash[2012] #{income_hash[2012]}"
+
+      expect(((income_hash[2012]) - 10000).abs).to be < TEN_DOLLARS
+
+    end
+
     it "should compute the income hash" do
 
+      financial_statement = FinancialStatement.new
+      income1 = Income.new
+      income1.amount_in_cents = 100000
+      income1.start_date = Date.new(2012, 01, 01)
+      income1.end_date = Date.new(2014, 01, 30)
+      income1.frequency = "monthly"
 
-      income_hash = @financial_statement.compute_yearwise(@financial_statement.incomes)
+      income2 = Income.new
+      income2.amount_in_cents = 1000000
+      income2.start_date = Date.new(2014, 01, 01)
+      income2.end_date = nil
+      income2.frequency = "yearly"
 
-      expect(((income_hash[2014]) - 5553600).abs).to be < 1000
+      financial_statement.incomes << income1 << income2
+
+      income_hash = financial_statement.compute_yearwise(financial_statement.incomes)
+
+      expect(((income_hash[2014]) - (96923 + 1000000)).abs).to be < TEN_DOLLARS
     end
   end
 
@@ -72,15 +125,13 @@ end
 
     it "should compute the deductions hash" do
 
-
       deduction_hash = @financial_statement.compute_yearwise(@financial_statement.deductions)
 
-      expect(((deduction_hash[2014]) - 470).abs).to be < 1000
+      expect(((deduction_hash[2014]) - 470).abs).to be < TEN_DOLLARS
     end
   end
 
   it "should check for benifits in current year" do
-
 
     expect(@financial_statement.is_receiving_benefit?).to be_true
   end
