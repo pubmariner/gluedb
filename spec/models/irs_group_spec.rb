1b87a199314894342d0b68f3c8321172f1c4c021
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-01-27 12:48:05 -0500

    fixed spec to suit changes in class

--- spec/models/irs_group_spec.rb
+++ spec/models/irs_group_spec.rb
@@ -4,21 +4,20 @@ require 'irs_groups/irs_group_builder'
 describe IrsGroup do
 
   before(:each) do
-    @family = Family.new
+    @family = Family.new({submitted_at:DateTime.now})
     @family.households.build({is_active:true})
     @irs_group_builder = IrsGroupBuilder.new(@family)
     @irs_group = @irs_group_builder.build
   end
 
   it 'should set effective start and end date' do
-    @family.save
+    @irs_group_builder.save
     expect(@irs_group.effective_start_date).to eq(@family.active_household.effective_start_date)
     expect(@irs_group.effective_end_date).to eq(@family.active_household.effective_end_date)
   end
 
   it 'should set a 16 digit id' do
-    @family.save
+    @irs_group_builder.save
     expect(@irs_group.id.to_s.length).to eq(16)
   end
-
 end
