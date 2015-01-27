9aff750de609b99c570c531796d7efaf2f14303f
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-01-27 12:48:18 -0500

    fixed spec to suit changes in class

--- spec/lib/irs_groups/irs_group_builder_spec.rb
+++ spec/lib/irs_groups/irs_group_builder_spec.rb
@@ -4,7 +4,7 @@ require 'irs_groups/irs_group_builder'
 describe IrsGroupBuilder do
 
   before(:each) do
-    @family = Family.new
+    @family = Family.new({submitted_at:DateTime.now})
     @family.households.build({is_active:true})
     @irs_group_builder = IrsGroupBuilder.new(@family)
   end
@@ -20,7 +20,7 @@ describe IrsGroupBuilder do
 
   it 'returns a IrsGroup object with Id of length 16' do
     irs_group = @irs_group_builder.build
-    irs_group.save
+    @irs_group_builder.save
     expect(irs_group.id.to_s.length).to eq(16)
   end
 
