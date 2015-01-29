0a86117f9f41fe53dfd1157d710bbe9ebc79fcfd
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-01-29 12:14:38 -0500

    fixed logging bug

--- lib/import_application_groups.rb
+++ lib/import_application_groups.rb
@@ -136,7 +136,7 @@ class ImportApplicationGroups
         value = uc.validate(ig_request, listener)
       end
 
-      @@logger.info "#{DateTime.now.to_s}" + "Family e_case_id:#{application_group_builder.family.e_case_id}" + "message:'CreateOrUpdatePerson did not succees for all'" unless all_valid
+      @@logger.info "#{DateTime.now.to_s}" + "Family e_case_id:#{ag.to_hash[:e_case_id]}" + "message:'CreateOrUpdatePerson did not succeed for all'" unless all_valid
 
       ig_requests.each do |ig_request|
         listener = PersonImportListener.new(ig_request[:applicant_id], p_tracker)
