f4a33a7ff4181a67a18c1c670d5c6b04a8fa9dae
Sean Carley <milythael.mourning@gmail.com>
2015-01-30 11:31:03 -0600

    Changed name of local variable to match family, not application group.

--- app/models/queries/person_with_no_families.rb
+++ app/models/queries/person_with_no_families.rb
@@ -4,9 +4,9 @@ module Queries
     end
 
     def execute
-      people_in_application_groups = Family.all.flat_map(&:family_members).flat_map(&:person).compact.uniq
+      people_in_families = Family.all.flat_map(&:family_members).flat_map(&:person).compact.uniq
       all_people = Person.all
-      all_people - people_in_application_groups
+      all_people - people_in_families
     end
   end
 end
