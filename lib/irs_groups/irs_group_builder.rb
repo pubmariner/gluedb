cf0a4ae332d9f84e27e2c9857ab5fa392c6ba5a3
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-02-02 19:43:27 -0500

    code cleanup

--- lib/irs_groups/irs_group_builder.rb
+++ lib/irs_groups/irs_group_builder.rb
@@ -20,73 +20,20 @@ class IrsGroupBuilder
   end
 
   def update
-      if retain_irs_group?
-          assign_exisiting_irs_group_to_new_household
-      end
-      #set_current_and_previous_households
-      #decision_matrix
+    assign_exisiting_irs_group_to_new_household
   end
 
-  # returns true if we take the irsgroup from previous household and apply it to new household.
-  # this happens when the number of coverage households has remained the same.
-  # returns false otherswise. i.e. when we have to split/merge irsgroups
-  def retain_irs_group?
-    all_households = @family.households.sort_by(&:submitted_at)
-    return false if all_households.length == 1
 
-    previous_household, current_household = all_households[all_households.length-2, all_households.length]
-    current_household.coverage_households.length == previous_household.coverage_households.length
-  end
-
+  # if by updating the family we have created a new household,
+  # then we should take the Irs Group from previously active household and assign it to the newly active household
   def assign_exisiting_irs_group_to_new_household
     all_households = @family.households.sort_by(&:submitted_at)
     previous_household, current_household = all_households[all_households.length-2, all_households.length]
+
+    return unless current_household.irs_group_id.blank? # irs group already present, so do nothing
+
     current_household.irs_group_id =  previous_household.irs_group_id
     current_household.save!
-  end
-
-  def decision_matrix
-
-    if coverage_household_changed? && tax_household_changed?
-      manipulate_irs_group
-    elsif coverage_household_changed? && !tax_household_changed?
-      retain_irs_group
-    elsif !coverage_household_changed? && tax_household_changed?
-      retain_irs_group
-    else #!coverage_household_changed? && !tax_household_changed?
-      retain_irs_group
-    end
-
-  end
-
-  def coverage_household_changed?
-    if @current_household.coverage_households.length != @previous_household.coverage_households.length
-      return true
-    else
-      return false
-    end
-  end
-
-  def tax_household_changed?
-    if @current_household.tax_households.length != @previous_household.tax_households.length
-      return true
-    else
-      return false
-    end
-  end
-
-  def manipulate_irs_group
-
-  end
-
-  def retain_irs_group
-    @current_household.irs_group_id =  @previous_household.irs_group_id
-    current_household.save!
-  end
-
-  def set_current_and_previous_households
-    all_households = @family.households.sort_by(&:submitted_at)
-    @previous_household, @current_household = all_households[all_households.length-2, all_households.length]
   end
 
 end
