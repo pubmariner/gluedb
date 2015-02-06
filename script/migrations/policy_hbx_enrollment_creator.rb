5322d2c9be026747a506c8fd7e8c0d268e5cb56a
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-02-06 17:00:54 -0500

    created hbx_enrollment in a family for policy which already did not have a hbx_enrollment in that family

---
+++ script/migrations/policy_hbx_enrollment_creator.rb
@@ -0,0 +1,43 @@
+class PolicyHbxEnrollmentCreator
+
+  def initialize(policy, family)
+    @policy = policy
+    @family = family
+  end
+
+  def create
+      hbx_enrollement = @family.active_household.hbx_enrollments.build
+      hbx_enrollement.policy = @policy
+      @family.primary_applicant.broker_id = Broker.find(@policy.broker_id) unless @policy.broker_id.blank?
+
+      hbx_enrollement.enrollment_group_id = @policy.eg_id
+      hbx_enrollement.elected_aptc_in_dollars = @policy.elected_aptc
+      hbx_enrollement.applied_aptc_in_dollars = @policy.applied_aptc
+      hbx_enrollement.submitted_at = Time.now
+
+      hbx_enrollement.kind = "employer_sponsored" unless @policy.employer_id.blank?
+      hbx_enrollement.kind = "unassisted_qhp" if (hbx_enrollement.applied_aptc_in_cents == 0 && @policy.employer.blank?)
+      hbx_enrollement.kind = "insurance_assisted_qhp" if (hbx_enrollement.applied_aptc_in_cents > 0 && @policy.employer.blank?)
+
+      @policy.enrollees.each do |enrollee|
+        begin
+          person = Person.find_for_member_id(enrollee.m_id)
+
+          @family.family_members << FamilyMember.new(person: person) unless @family.person_is_family_member?(person)
+          family_member = @family.find_family_member_by_person(person)
+
+          hbx_enrollement_member = hbx_enrollement.hbx_enrollment_members.build({family_member: family_member,
+                                                                                 premium_amount_in_cents: enrollee.pre_amt})
+          hbx_enrollement_member.is_subscriber = true if (enrollee.rel_code == "self")
+
+        rescue FloatDomainError
+          # puts "Error: invalid premium amount for enrollee: #{enrollee.inspect}"
+          next
+        end
+      end
+
+      @family.save
+      @family
+
+  end
+end
---
+++ script/migrations/policy_hbx_enrollment_creator_script.rb
@@ -0,0 +1,28 @@
+require File.join(Rails.root, "script", "migrations", "policy_hbx_enrollment_creator")
+require File.join(Rails.root, "app", "models", "queries", "policies_with_no_hbx_enrollment")
+require File.join(Rails.root, "app", "models", "queries", "family_with_given_policy")
+
+@@logger = Logger.new("#{Rails.root}/log/policy_hbx_enrollment_creator_#{Time.now.to_s.gsub(' ','')}.log")
+
+policies = Queries::PoliciesWithNoHbxEnrollments.new.execute[0..5]
+
+policies = []
+policies << Policy.find('47067')
+puts "policies #{policies.inspect}"
+
+policies.each do |policy|
+  begin
+    family = FamilyWithGivenPolicy.new(policy.id).execute
+
+    if family.nil?
+      @@logger.info "#{DateTime.now.to_s} policy.id:#{policy.id} has no family to belong to"
+      next
+    end
+
+    family = PolicyHbxEnrollmentCreator.new(policy, family).create
+
+  rescue Exception=>e
+    @@logger.info "#{DateTime.now.to_s} policy.id:#{policy.id} error message:#{e.message}"
+  end
+end
+
