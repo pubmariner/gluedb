fb5375a32182f545e727bfd35697e33663834b23
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-02-09 17:45:44 -0500

    removed exception handling

--- script/queries/import_families.rb
+++ script/queries/import_families.rb
@@ -1,12 +1,4 @@
-path = "/Users/CitadelFirm/Downloads/projects/hbx/RenewalReports_150121233949247.xml"
+path = "/Users/CitadelFirm/Downloads/projects/hbx/app_groups_aug14.xml"
 
-@@logger = Logger.new("#{Rails.root}/log/family_#{Time.now.utc.iso8601}.log")
-
-begin
-  iag = ImportFamilies.new(path)
-  iag.run
-rescue Exception=>e
-  @@logger.info "#{DateTime.now.to_s} class:#{self.class.name} method:#{__method__.to_s}\n"+
-                    "message:#{e.message}\n" +
-                    "backtrace:#{e.backtrace.inspect}\n"
-end
+iag = ImportFamilies.new(path)
+iag.run
