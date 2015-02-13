1fcffa7c9a4b089fb5b612dbbe81cfe650dda649
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-02-13 10:19:15 -0500

    specs

---
+++ spec/script/migrations/policy_hbx_enrollment_creator_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+require File.join(Rails.root, "script", "migrations", "policy_hbx_enrollment")
+
+describe PolicyHbxEnrollmentCreator do
+
+  it 'should do something' do
+
+    true.should == false
+  end
+end
