232278c09a6dca82c8c176de27037e7ed3224acf
Raghuram <raghuramg83@gmail.com>
2015-02-17 13:39:50 -0500

    Added missing report models

---
+++ app/models/pdf_templates/tax_household_coverage.rb
@@ -0,0 +1,13 @@
+ module PdfTemplates
+  class TaxHouseholdCoverage
+    include Virtus.model
+
+
+    attribute :calender_month, Integer
+    attribute :primary, PdfTemplates::Enrollee
+    attribute :spouse, PdfTemplates::Enrollee
+    attribute :dependents, Array[PdfTemplates::Enrollee]
+
+    attribute :policy_ids, Array[Integer]
+  end
+end
