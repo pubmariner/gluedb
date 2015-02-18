ccb939bbfca62c8a1f70f6312439a0cf77c074db
Trey Evans <lewis.r.evans@gmail.com>
2015-02-18 13:10:06 -0500

    Hotfix for leaking FDs.
    Fixes #411.

--- app/models/clients/member_document_client.rb
+++ app/models/clients/member_document_client.rb
@@ -4,16 +4,22 @@ module Clients
       if authority_member.blank?
         return []
       end
-      req = Amqp::Requestor.default
-      di, rprops, rbody = req.request({
-        :routing_key => "member_documents.find_by_hbx_member_id",
-        :headers => {
-          :hbx_member_id => authority_member.hbx_member_id
-        }
-      }, "")
-      dlr = Parsers::DocumentListResponse.parse(rbody)
-      return [] if dlr.document.blank?
-      dlr.document
+      conn = Bunny.new(ExchangeInformation.amqp_uri)
+      begin
+        conn.start
+        req = Amqp::Requestor.new(conn)
+        di, rprops, rbody = req.request({
+          :routing_key => "member_documents.find_by_hbx_member_id",
+          :headers => {
+            :hbx_member_id => authority_member.hbx_member_id
+          }
+        }, "")
+        dlr = Parsers::DocumentListResponse.parse(rbody)
+        return [] if dlr.document.blank?
+        dlr.document
+      ensure
+        conn.close
+      end
     end
   end
 end
