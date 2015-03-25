20407182ad8ea271981c7a5678ffd4a9b005835e
Trey Evans <lewis.r.evans@gmail.com>
2015-03-25 10:25:49 -0400

    Audit nfp scripts.

---
+++ script/queries/correlate_audits.rb
@@ -0,0 +1,48 @@
+require 'csv'
+
+hbx_records = []
+nfp_records = []
+
+CSV.foreach("nfp_only.csv") do |row|
+  nfp_records << (row + ["NFP"])
+end
+
+CSV.foreach("hbx_only.csv") do |row|
+  hbx_records << (row + ["HBX"])
+end
+
+all_groups = (nfp_records + hbx_records).group_by { |r| r[8] }
+mismatches = all_groups.values.select { |ag| ag.length > 1 }
+rest = all_groups.values.select { |ag| ag.length < 2 }
+
+rest_nfp = rest.select { |ag| ag.last.last == "NFP" }
+rest_hbx = rest.select { |ag| ag.last.last == "HBX" }
+
+CSV.open("audit_issues_2015.csv", "w") do |csv|
+  csv << ["Multiple Policies"]
+
+  mismatches.each do |v|
+    v.sort_by { |v| v.last }.each do |row|
+      csv << row
+    end
+    csv << []
+  end
+
+  csv << []
+  csv << []
+  csv << ["Single Policies - HBX Only", "These individuals have only one policy for 2015, and that policy is only with the HBX"]
+  rest_hbx.each do |v|
+    v.each do |row|
+      csv << row 
+    end
+  end
+
+  csv << []
+  csv << []
+  csv << ["Single Policies - NFP Only", "These individuals have only one policy for 2015, and that policy is only with NFP."]
+  rest_nfp.each do |v|
+    v.each do |row|
+      csv << row 
+    end
+  end
+end
