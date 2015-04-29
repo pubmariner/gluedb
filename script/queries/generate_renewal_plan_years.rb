6ab846b45923ec2e6ddc4a118c1b23db05cf126b
Trey Evans <lewis.r.evans@gmail.com>
2015-04-29 12:51:11 -0400

    Adding renewal plan year generation.

---
+++ script/queries/generate_renewal_plan_years.rb
@@ -0,0 +1,26 @@
+clone_start_date = Date.new(2014,6,1)
+
+new_start_date = Date.new(2015, 6, 1)
+new_end_date = Date.new(2016, 5, 31)
+
+plan_years = PlanYear.where(:start_date => clone_start_date)
+
+plan_years.each do |plan_year|
+  if plan_year.contribution_strategy.present?
+    contribution_strategy = plan_year.contribution_strategy
+    reference_plan = contribution_strategy.reference_plan
+    next_years_plan = reference_plan.renewal_plan
+    new_plan_year = PlanYear.create!({
+      :start_date => new_start_date,
+      :end_date => new_end_date,
+      :broker_id => plan_year.broker_id,
+      :employer_id => plan_year.employer_id
+    })
+    contribution_strategy = EmployerContributions::DistrictOfColumbiaEmployer.create!({
+      :plan_year_id => new_plan_year.id,
+      :reference_plan_id => reference_plan.id,
+      :employee_max_percent => contribution_strategy.employee_max_percent,
+      :dependent_max_percent => contribution_strategy.dependent_max_percent
+    })
+  end
+end
