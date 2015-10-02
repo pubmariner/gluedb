2f3c75979c8eb13421001fba79083aa8165fca88
Trey Evans <lewis.r.evans@gmail.com>
2015-10-02 12:33:58 -0400

    Refs #2579.  Work on broker update.
    Now reads message, but does not properly parse out properties.
    
    Refs #2579.

--- app/models/listeners/broker_updated_listener.rb
+++ app/models/listeners/broker_updated_listener.rb
@@ -1,4 +1,16 @@
 module Listeners
-  class BrokerUpdatedListener
+  class BrokerUpdatedListener < ::Amqp::Client
+    def on_message(delivery_info, properties, body)
+      headers = (properties.headers || {})
+      broker_id = headers.to_hash.stringify_keys['broker_id']
+      brokers = Broker.by_npn(broker_id)
+      if brokers.any?
+        broker = brokers.first
+        broker.update_attributes({})
+      else
+        Broker.create!({})
+      end
+      channel.acknowledge(delivery_info.delivery_tag, false)
+    end
   end
 end
--- spec/models/listeners/broker_updated_listener.rb
+++ spec/models/listeners/broker_updated_listener.rb
@@ -3,18 +3,34 @@ require 'rails_helper'
 describe Listeners::BrokerUpdatedListener do
   let(:broker) { double }
   let(:broker_hbx_id) { "a broker npn" }
+  let(:message_body) { "" }
+  let(:message_properties) { double(:headers => message_headers) }
+  let(:message_headers) { { :broker_id => broker_hbx_id } }
+  let(:channel) { double }
+  let(:queue) { double }
+  let(:delivery_info) { double(:delivery_tag => "a message tag") }
+  let(:expected_broker_properties) { { } }
+
+  subject { Listeners::BrokerUpdatedListener.new(channel, queue) }
 
   before :each do
     allow(Broker).to receive(:by_npn).with(broker_hbx_id).and_return(matching_brokers)
+    allow(channel).to receive(:acknowledge).with("a message tag", false)
   end
 
   describe "given an broker which doesn't exist" do
     let(:matching_brokers) { [] }
-    it "should create that broker"
+    it "should create that broker" do
+      expect(Broker).to receive(:create!).with(expected_broker_properties)
+      subject.on_message(delivery_info, message_properties, message_body)
+    end
   end
 
   describe "given an broker which exists" do
     let(:matching_brokers) { [broker] }
-    it "should update that broker"
+    it "should update that broker" do
+      expect(broker).to receive(:update_attributes).with(expected_broker_properties)
+      subject.on_message(delivery_info, message_properties, message_body)
+    end
   end
 end
