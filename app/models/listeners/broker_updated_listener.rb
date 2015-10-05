a37caf64d688d6705dbcbad6e2edf3b5dd91fa8a
Trey Evans <lewis.r.evans@gmail.com>
2015-10-05 11:36:29 -0400

    Fixes #2579.  Listener to create/update brokers.

--- app/models/listeners/broker_updated_listener.rb
+++ app/models/listeners/broker_updated_listener.rb
@@ -1,16 +1,99 @@
 module Listeners
   class BrokerUpdatedListener < ::Amqp::Client
-    def on_message(delivery_info, properties, body)
+    VOCAB_NS = {:v => "http://openhbx.org/api/terms/1.0"}
+    FailAction = Struct.new(:ack, :requeue, :event_name, :message)
+
+    def on_message(delivery_info, properties, body, time_provider = Time)
       headers = (properties.headers || {})
       broker_id = headers.to_hash.stringify_keys['broker_id']
-      brokers = Broker.by_npn(broker_id)
-      if brokers.any?
-        broker = brokers.first
-        broker.update_attributes({})
-      else
-        Broker.create!({})
+
+      sc = ShortCircuit.on(:fail) do |fail_action|
+        log_event("error", fail_action.event_name, broker_id, fail_action.message, time_provider)
+        if fail_action.ack
+          channel.acknowledge(delivery_info.delivery_tag, false)
+        else
+          channel.nack(delivery_info.delivery_tag, false, fail_action.requeue)
+        end
       end
-      channel.acknowledge(delivery_info.delivery_tag, false)
+      sc.and_then do |b_id|
+        brokers = Broker.by_npn(b_id)
+        if brokers.any?
+          broker_attributes = get_broker_properties(b_id, false)
+          broker = brokers.first
+          if broker.update_attributes(broker_attributes)
+            log_event("info", "broker_updated", b_id, "", time_provider)
+          else
+            error_payload = JSON.dump({
+              :broker_attributes => broker_attributes,
+              :errors => broker.errors.full_messages
+            })
+            throw :fail, FailAction.new(true, true, "invalid_broker_update", error_payload)
+          end
+        else
+          broker_attributes = get_broker_properties(b_id, false)
+          new_broker = Broker.new(broker_attributes)
+          if new_broker.save
+            log_event("info", "broker_created", b_id, "", time_provider)
+          else
+            error_payload = JSON.dump({
+              :broker_attributes => broker_attributes,
+              :errors => new_broker.errors.full_messages
+            })
+            throw :fail, FailAction.new(true, true, "invalid_broker_creation", error_payload)
+          end
+        end
+        channel.acknowledge(delivery_info.delivery_tag, false)
+      end
+      sc.call(broker_id)
+    end
+
+    def get_broker_properties(broker_id, new_broker)
+      xml_string = request_resource(broker_id)
+      xml = Nokogiri::XML(xml_string)
+      {
+        :name_first => Maybe.new(xml.at_xpath("//v:person/v:person_name/v:person_given_name", VOCAB_NS)).content.value,
+        :name_last => Maybe.new(xml.at_xpath("//v:person/v:person_name/v:person_surname", VOCAB_NS)).content.value,
+        :name_middle => Maybe.new(xml.at_xpath("//v:person/v:person_name/v:person_middle_name", VOCAB_NS)).content.value,
+        :name_pfx => Maybe.new(xml.at_xpath("//v:person/v:person_name/v:person_name_prefix_text", VOCAB_NS)).content.value,
+        :name_sfx => Maybe.new(xml.at_xpath("//v:person/v:person_name/v:person_name_suffix_text", VOCAB_NS)).content.value,
+        :npn => Maybe.new(xml.at_xpath("//v:broker_role/v:npn", VOCAB_NS)).content.value
+      }
+    end
+
+    def request_resource(broker_id)
+      di, rprops, rbody = request({
+        :headers => {
+          :broker_id => broker_id
+        }
+      },"",10)
+      if rprops.nil?
+        throw :fail, FailAction.new(false, true, "resource_lookup_timeout", "")
+      end
+      r_headers = rprops.headers || {}
+      r_status = r_headers.stringify_keys["return_status"]
+      if "404" == r_status.to_s
+        throw :fail, FailAction.new(true, true, "non_existant_resource", "")
+      end
+      rbody
+    end
+
+    def log_event(level, event_name, broker_id, message, time_provider)
+      e_ex_name = ExchangeInformation.event_publish_exchange
+      chan = connection.create_channel
+      e_ex = chan.fanout(e_ex_name, {:durable => true})
+      e_ex.publish(message, {
+        :routing_key => "#{level}.application.gluedb.broker_update_listener.#{event_name}",
+        :timestamp => time_provider.now.to_i,
+        :headers => {
+          :broker_id => broker_id
+        }
+      })
+      chan.close
+    end
+
+    def self.queue_name
+      ec = ExchangeInformation
+      "#{ec.hbx_id}.#{ec.environment}.q.glue.broker_updated_listener"
     end
   end
 end
--- spec/models/listeners/broker_updated_listener.rb
+++ spec/models/listeners/broker_updated_listener.rb
@@ -6,31 +6,144 @@ describe Listeners::BrokerUpdatedListener do
   let(:message_body) { "" }
   let(:message_properties) { double(:headers => message_headers) }
   let(:message_headers) { { :broker_id => broker_hbx_id } }
-  let(:channel) { double }
+  let(:connection) { double }
+  let(:channel) { double(:connection => connection) }
   let(:queue) { double }
-  let(:delivery_info) { double(:delivery_tag => "a message tag") }
-  let(:expected_broker_properties) { { } }
+  let(:message_tag) { "a message tag" }
+  let(:delivery_info) { double(:delivery_tag => message_tag) }
+  let(:expected_broker_properties) { {
+    :name_first => "Jane",
+    :name_last => "Curtin",
+    :name_middle => "Phred",
+    :name_pfx => "Dr",
+    :name_sfx => "III",
+    :npn => "2068981"
+  } }
+  let(:mock_requestor) { double }
+  let(:r_di) { double }
+  let(:error_channel) { double(:close =>  nil) }
+  let(:mock_event_exchange_name) { "mock event exchange name" }
+  let(:mock_event_exchange) { double }
+  let(:time_provider) { double( :now => 1 ) }
 
   subject { Listeners::BrokerUpdatedListener.new(channel, queue) }
 
   before :each do
+    allow(error_channel).to receive(:fanout).with(mock_event_exchange_name, {:durable => true}).and_return(mock_event_exchange)
+    allow(ExchangeInformation).to receive(:event_publish_exchange).and_return(mock_event_exchange_name)
+    allow(Amqp::Requestor).to receive(:new).with(connection).and_return(mock_requestor)
     allow(Broker).to receive(:by_npn).with(broker_hbx_id).and_return(matching_brokers)
+    allow(mock_requestor).to receive(:request).with(
+      {:headers => { :broker_id => broker_hbx_id }}, "", 10
+    ).and_return([r_di, r_props, resource_body])
     allow(channel).to receive(:acknowledge).with("a message tag", false)
   end
 
-  describe "given an broker which doesn't exist" do
+  describe "given a broker which doesn't exist" do
+    let(:resource_body) { File.read(File.join(Rails.root, "spec/data/resources/broker.xml")) }
+    let(:r_props) { double(:headers => {}) }
     let(:matching_brokers) { [] }
-    it "should create that broker" do
-      expect(Broker).to receive(:create!).with(expected_broker_properties)
-      subject.on_message(delivery_info, message_properties, message_body)
+    let(:mock_new_broker) { double(:save => valid_broker_value) }
+
+    before :each do
+      allow(Broker).to receive(:new).with(expected_broker_properties).and_return(mock_new_broker)
+    end
+
+    describe "with valid new broker info" do
+      let(:valid_broker_value) { true }
+
+      it "should create that broker" do
+        allow(connection).to receive(:create_channel).and_return(error_channel)
+        expect(mock_event_exchange).to receive(:publish).with("", {:routing_key=>"info.application.gluedb.broker_update_listener.broker_created", :timestamp=>1, :headers=>{:broker_id=> broker_hbx_id }})
+        expect(channel).to receive(:acknowledge).with(message_tag,false)
+        subject.on_message(delivery_info, message_properties, message_body, time_provider)
+      end
+    end
+
+    describe "with invalid new broker info" do
+      let(:mock_errors) { double(:full_messages => ["an error message"]) }
+      let(:valid_broker_value) { false }
+      let(:expected_error_payload) {
+        JSON.dump({
+          :broker_attributes => expected_broker_properties,
+          :errors =>  ["an error message"]
+        })
+      }
+
+      before(:each) do
+        allow(connection).to receive(:create_channel).and_return(error_channel)
+        allow(mock_new_broker).to receive(:errors).and_return(mock_errors)
+      end
+
+      it "should not create that broker and log an error" do
+        expect(mock_event_exchange).to receive(:publish).with(expected_error_payload, {:routing_key=>"error.application.gluedb.broker_update_listener.invalid_broker_creation", :timestamp=>1, :headers=>{:broker_id=> broker_hbx_id }})
+        expect(channel).to receive(:acknowledge).with(message_tag,false)
+        subject.on_message(delivery_info, message_properties, message_body, time_provider)
+      end
     end
   end
 
-  describe "given an broker which exists" do
+  describe "given a broker which exists" do
+    let(:resource_body) { File.read(File.join(Rails.root, "spec/data/resources/broker.xml")) }
+    let(:r_props) { double(:headers => {}) }
     let(:matching_brokers) { [broker] }
-    it "should update that broker" do
-      expect(broker).to receive(:update_attributes).with(expected_broker_properties)
-      subject.on_message(delivery_info, message_properties, message_body)
+    describe "with valid update information" do
+      before :each do
+        allow(broker).to receive(:update_attributes).with(expected_broker_properties).and_return(true)
+      end
+
+      it "should update that broker" do
+        allow(connection).to receive(:create_channel).and_return(error_channel)
+        expect(mock_event_exchange).to receive(:publish).with("", {:routing_key=>"info.application.gluedb.broker_update_listener.broker_updated", :timestamp=>1, :headers=>{:broker_id=> broker_hbx_id }})
+        expect(channel).to receive(:acknowledge).with(message_tag,false)
+        subject.on_message(delivery_info, message_properties, message_body, time_provider)
+      end
+    end
+
+    describe "with invalid update information" do
+      let(:mock_errors) { double(:full_messages => ["an error message"]) }
+      let(:expected_error_payload) {
+        JSON.dump({
+          :broker_attributes => expected_broker_properties,
+          :errors =>  ["an error message"]
+        })
+      }
+      before :each do
+        allow(broker).to receive(:update_attributes).with(expected_broker_properties).and_return(false)
+        allow(connection).to receive(:create_channel).and_return(error_channel)
+        allow(broker).to receive(:errors).and_return(mock_errors)
+      end
+
+      it "should not update that broker and log an error" do
+        expect(mock_event_exchange).to receive(:publish).with(expected_error_payload, {:routing_key=>"error.application.gluedb.broker_update_listener.invalid_broker_update", :timestamp=>1, :headers=>{:broker_id=> broker_hbx_id }})
+        expect(channel).to receive(:acknowledge).with(message_tag,false)
+        subject.on_message(delivery_info, message_properties, message_body, time_provider)
+      end
+    end
+  end
+
+  describe "with a requestor that times out" do
+    let(:resource_body) { nil }
+    let(:r_props) { nil }
+    let(:matching_brokers) { [] }
+    it "should re-queue the message for retry" do
+      allow(connection).to receive(:create_channel).and_return(error_channel)
+      expect(mock_event_exchange).to receive(:publish).with("", {:routing_key=>"error.application.gluedb.broker_update_listener.resource_lookup_timeout", :timestamp=>1, :headers=>{:broker_id=> broker_hbx_id }})
+      expect(channel).to receive(:nack).with(message_tag,false,true)
+      subject.on_message(delivery_info, message_properties, message_body, time_provider)
+    end
+  end
+
+  describe "with a requestor gets a 404" do
+    let(:resource_body) { "" }
+    let(:r_props) { double(:headers => {:return_status => "404"}) }
+    let(:matching_brokers) { [] }
+
+    it "should log the missing resource error to the bus, and ack the message" do
+      allow(connection).to receive(:create_channel).and_return(error_channel)
+      expect(mock_event_exchange).to receive(:publish).with("", {:routing_key=>"error.application.gluedb.broker_update_listener.non_existant_resource", :timestamp=>1, :headers=>{:broker_id=> broker_hbx_id }})
+      expect(channel).to receive(:acknowledge).with(message_tag,false)
+      subject.on_message(delivery_info, message_properties, message_body, time_provider)
     end
   end
 end
