3fe589d96810e0bd7c8d7d1200bc55ce9abf7a71
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-10-29 16:06:35 -0400

    Refs #3506 importing aptc into policy

--- script/migrations/import_curam_aptc.rb
+++ script/migrations/import_curam_aptc.rb
@@ -7,10 +7,31 @@ def person_match(first_name, ssn, dob)
   return people
 end
 
+def get_policy(person)
+  Policy.new #will plug in real policies later
+end
+
+def compute_aptc(premium, plan, max_aptc, percent)
+  if max_aptc*percent > premium*plan.ehb
+    return premium*plan.ehb
+  else
+    return max_aptc*percent
+  end
+end
+
+
+def update_policy(policy, row)
+  policy.allocated_aptc = row[12]
+  policy.elected_aptc = policy.allocated_aptc * row[16]
+  policy.applied = compute_aptc( row[7], plan, row[12], row[16]) #compute_aptc(premium, plan, max_aptc, percent)
+  policy.csr_amt = row[13] || nil
+end
+
 CSV.foreach(File.path(file), :headers=>true) do |row|
   persons = person_match(row[0], row[3], row[2])
   if persons
     person = persons.first
+    update_policy(get_policy(person), row)
     puts "FOUND #{row[0]} #{row[1]} #{row[2]} #{row[3]}"
   else
     puts "NOT FOUND #{row[0]} #{row[1]} #{row[3]} #{row[2]}"
