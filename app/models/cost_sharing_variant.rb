a82527e4fbcc82b89a89cf62c1c0de59f8aa25be
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-10-29 15:49:12 -0400

    new models for aptc import

--- app/models/cost_sharing_variant.rb
+++ app/models/cost_sharing_variant.rb
@@ -4,7 +4,6 @@ class CostSharingVariant
   field :start_on, type: Date
   field :end_on, type: Date
   field :percent, type: BigDecimal, default: 0.00
-  field :percent, type: BigDecimal, default: 0.00
 
   embedded_in :policy
 end
