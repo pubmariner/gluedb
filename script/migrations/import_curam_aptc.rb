1538fd28b1e1b7379f2a18298430ac9f11ee1b2b
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-10-31 16:13:22 -0400

    Refs #3506 added logging, took code from dev

--- script/migrations/import_curam_aptc.rb
+++ script/migrations/import_curam_aptc.rb
@@ -1,60 +1,72 @@
-file = "/Users/CitadelFirm/Downloads/enrollment_data_102815.csv"
+file = "/Users/CitadelFirm/Downloads/policies-for-aptc/matched_policies.csv"
+@logger = Logger.new("#{Rails.root}/log/curam_aptc_import#{Time.now.to_s.gsub(' ', '')}.log")
 
 def person_match(first_name, ssn, dob)
   dob = Date.strptime(dob, "%m/%d/%Y")
-  people = Person.unscoped.where({"members.ssn" => @ssn}).and({"members.dob" => dob}).and({"name_first" => first_name})
+  people = Person.unscoped.where({"members.ssn" => ssn}).and({"members.dob" => dob}).and({"name_first" => first_name}).to_a
   puts "multiple matches" if people.length > 1
   return people
 end
 
-def get_policy(person)
-  Policy.new #will plug in real policies later
-end
 
 def compute_aptc(premium, plan, max_aptc, percent)
-
   if max_aptc*percent > premium*plan.ehb
     return premium*plan.ehb
   else
     return max_aptc*percent
   end
-
 end
 
-
 def update_policy(policy, row)
-  policy.aptc_maximums << AptcMaximum.new({start_on: Date.new(2016, 01, 01),
-                                           max_aptc:row[12],
-                                           aptc_percent:row[16]})
+  max_aptc = row[12].to_i || 0.0
+  percent = row[16].to_f
+  premium = row[7].to_f
+  csr = row[13].to_i || nil
+  max_aptc_2015 = row[15].to_i || 0.0
 
-  policy.aptc_maximums << AptcMaximum.new({start_on: policy.policy_start,
-                                           end_on: Date.new(2015, 12, 31),
-                                           max_aptc:row[15],
-                                           aptc_percent:row[16]})
+  aptc_maximum_2016 = AptcMaximum.new({start_on: Date.new(2016, 01, 01),
+                                       max_aptc:max_aptc,
+                                       aptc_percent:percent})
+  policy.aptc_maximums << aptc_maximum_2016
 
-  policy.cost_sharing_variants << CostSharingVariant.new(start_on: Date.new(2016, 01, 01),
-                                                         percent: row[13])
+  aptc_maximum_2015 =  AptcMaximum.new({start_on: policy.policy_start,
+                                        end_on: Date.new(2015, 12, 31),
+                                        max_aptc:max_aptc_2015,
+                                        aptc_percent:percent})
+  policy.aptc_maximums << aptc_maximum_2015
+
+  cost_sharing_variant = CostSharingVariant.new(start_on: Date.new(2016, 01, 01),
+                                                percent: csr)
+  policy.cost_sharing_variants << cost_sharing_variant
 
   aptc_credit =  AptcCredit.new({start_on: Date.new(2016, 01, 01),
-                                         aptc: compute_aptc( row[7], plan, row[12], row[16]),
-                                         pre_amt_tot: row[7]
+                                         aptc: compute_aptc( premium, policy.plan, max_aptc, percent),
+                                         pre_amt_tot: premium
                                         })
-  tot_res_amt = row[7] - aptc_credit.aptc
+
+  tot_res_amt = premium - aptc_credit.aptc
   aptc_credit.tot_res_amt = tot_res_amt < 0 ? 0 : tot_res_amt
 
   policy.aptc_credits << aptc_credit
 
-  policy.save
+  @logger.info "#{row[0]},#{row[1]},#{row[2]},#{row[4]},#{aptc_maximum_2016.max_aptc},#{aptc_maximum_2016.aptc_percent},#{aptc_maximum_2015.max_aptc},#{aptc_maximum_2015.aptc_percent}"
 
+  policy.save
 end
 
-CSV.foreach(File.path(file), :headers=>true) do |row|
-  persons = person_match(row[0], row[3], row[2])
-  if persons
-    person = persons.first
-    update_policy(get_policy(person), row)
-    puts "FOUND #{row[0]} #{row[1]} #{row[2]} #{row[3]}"
+CSV.foreach(File.path(file)) do |row|
+  people = person_match(row[0], row[3], row[2])
+  policy = nil
+  if people.present?
+    person = people.first
+    if row[17] == "No Valid Policies"
+      #puts "#{row[0]} #{row[1]} multiple policies found"
+      next
+    else
+      policy = Policy.where(id: row[17]).first
+    end
+    update_policy(policy, row)
   else
-    puts "NOT FOUND #{row[0]} #{row[1]} #{row[3]} #{row[2]}"
+    puts "NOT FOUND #{row[0]} #{row[1]}"
   end
 end
