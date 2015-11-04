ecb71146e411c32dfc5da3f4c6bb468252105293
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-11-04 15:31:01 -0500

    changed 2016 plan import script from create new plans to update existing plans

--- db/seedfiles/plans_2016_json.rb
+++ db/seedfiles/plans_2016_json.rb
@@ -5,12 +5,11 @@ plan_file.close
 plan_data = JSON.load(data)
 puts "Before total #{Plan.count} plans"
 puts "#{plan_data.size} plans in json file"
-puts "Deleting existing 2016 plans"
-Plan.where(year:2016).delete
 plan_data.each do |pd|
-  plan = Plan.new(pd)
-  plan.id = pd["id"]
-  plan.save!
+  plan = Plan.where(hios_plan_id:pd["hios_plan_id"]).first
+  plan.update_attributes(pd)
+  plan.id = pd['id'] #use the id from json
+  plan.save
 end
 puts "After total #{Plan.count} plans"
 puts "Finished importing 2016 plans"
