a5d9e18185afb669bc364babc21dd192c311f485
Zoheb Nensey <zoheb.nensey@gmail.com>
2015-11-10 15:41:44 -0500

    Minor modifications to employer export, renewal plan year generator, and shop audit generator

--- script/queries/employer_export.rb
+++ script/queries/employer_export.rb
@@ -4,7 +4,7 @@ require 'pry'
 file = CSV.open("employer_export.csv", "w")
 file << %w{ employer.name employer.hbx_id employer.name_first employer.name_middle employer.name_last employer.fein
             employer.addresses.address_1, employer.addresses.address_2, employer.addresses.city, employer.addresses.state, employer.addresses.zip, employer.phones.phone_number, employer.emails.email_address
-            plan_year.start_date
+            plan_year.start_date, 
             broker.name_first broker.name_last employer.broker.npn}
 
 Employer.all.each do |employer|
