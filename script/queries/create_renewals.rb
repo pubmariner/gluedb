f855276c607cf44df129e64a8acb96749355de9a
Zoheb <Zoheb2@zohebneomputer2.home>
2015-12-03 23:04:13 -0500

    Renewal creation script now prints error

--- script/queries/create_renewals.rb
+++ script/queries/create_renewals.rb
@@ -4,8 +4,8 @@ feins = []
 
 emp_ids = Employer.where(:fein => { "$in" => feins } ).map(&:id)
 
-pols_mems = Policy.where(PolicyStatus::Active.as_of(Date.new(2015,9,30)).query).where({ "employer_id" => { "$in" => emp_ids }})
-pols = Policy.where(PolicyStatus::Active.as_of(Date.new(2015,9,30)).query).where({ "employer_id" => { "$in" => emp_ids }}).no_timeout
+pols_mems = Policy.where(PolicyStatus::Active.as_of(Date.new(2015,12,31)).query).where({ "employer_id" => { "$in" => emp_ids }})
+pols = Policy.where(PolicyStatus::Active.as_of(Date.new(2015,12,31)).query).where({ "employer_id" => { "$in" => emp_ids }}).no_timeout
 
 m_ids = []
 
@@ -21,7 +21,7 @@ Caches::MongoidCache.allocate(Employer)
 Caches::MongoidCache.allocate(Plan)
 Caches::MongoidCache.allocate(Carrier)
 
-polgen = PolicyIdGenerator.new(10)
+#polgen = PolicyIdGenerator.new(10)
 
 FileUtils.rm_rf(Dir.glob('renewals/*'))
 
@@ -34,7 +34,7 @@ pols.each do |pol|
     else
       if (sub_member.person.authority_member.hbx_member_id == pol.subscriber.m_id)
         begin
-          r_pol = pol.clone_for_renewal(Date.new(2015,10,1))
+          r_pol = pol.clone_for_renewal(Date.new(2016,1,1))
           calc.apply_calculations(r_pol)
           p_id = polgen.get_id
           out_file = File.open("renewals/#{p_id}.xml",'w')
@@ -43,13 +43,15 @@ pols.each do |pol|
           ms = CanonicalVocabulary::MaintenanceSerializer.new(r_pol,"change", "renewal", member_ids, member_ids, {:member_repo => member_repo})
           out_file.print(ms.serialize)
           out_file.close
-        rescue
-          puts pol.inspect
+        rescue Exception=>e
+          puts pol.inspect + e.message
         end
       end
     end
   end
 end
+
+puts name_array.sort
 
 Caches::MongoidCache.release(Plan)
 Caches::MongoidCache.release(Carrier)
