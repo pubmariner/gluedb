532fc7d377f9b5bf55d1344db6ab0bedc7393280
Trey Evans <lewis.r.evans@gmail.com>
2015-12-08 15:41:21 -0500

    Work on listener for person update.
    Trying to sneak in a little work on demographic create/update for
    people.
    
    Refs #2164.

--- app/models/listeners/individual_event_listener.rb
+++ app/models/listeners/individual_event_listener.rb
@@ -20,14 +20,27 @@ module Listeners
       resource_event_broadcast("error", event_key, ind_id, r_code, body)
     end
 
+    def process_retrieved_resource(delivery_info, individual_id, r_code, remote_resource)
+      change_set = ::ChangeSets::IndividualChangeSet.new(remote_resource)
+      if change_set.individual_exists?
+      else
+        if change_set.create_individual_resource
+          resource_event_broadcast("info", "individual_created", individual_id, r_code, remote_resource)
+          channel.ack(delivery_info.delivery_tag, false)
+        else
+          resource_event_broadcast("error", "individual_created", individual_id, "422", JSON.dump({:resource => remote_resource.to_s, :errors => change_set.full_error_messages }))
+          channel.ack(delivery_info.delivery_tag, false)
+        end
+      end
+    end
+
     def on_message(delivery_info, properties, body)
       m_headers = (properties.headers || {}).to_hash.stringify_keys
       individual_id = m_headers["individual_id"].to_s
       r_code, resource_or_body = ::RemoteResources::IndividualResource.retrieve(self, individual_id)
       case r_code.to_s
       when "200"
-        resource_event_broadcast("info", "individual_created", individual_id, r_code, resource_or_body)
-        channel.ack(delivery_info.delivery_tag, false)
+        process_retrieved_resource(delivery_info, individual_id, r_code, resource_or_body)
       when "404"
         resource_error_broadcast("resource_not_found", individual_id, r_code)
         channel.ack(delivery_info.delivery_tag, false)
