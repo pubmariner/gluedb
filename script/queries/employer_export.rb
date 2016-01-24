3c950b635d57d0144aea21904b4653b92ecfae3f
Zoheb <Zoheb2@zohebneomputer2.westell.com>
2016-01-24 15:09:54 -0500

    Trivial changes to scripts

--- script/queries/employer_export.rb
+++ script/queries/employer_export.rb
@@ -1,7 +1,9 @@
 require 'csv'
 require 'pry'
 
-file = CSV.open("employer_export.csv", "w")
+timestamp = Time.now.strftime('%Y%m%d%H%M')
+
+file = CSV.open("employer_export_#{timestamp}.csv", "w")
 file << %w{ employer.name employer.hbx_id employer.name_first employer.name_middle employer.name_last employer.fein
             employer.addresses.address_1, employer.addresses.address_2, employer.addresses.city, employer.addresses.state, employer.addresses.zip, employer.phones.phone_number, employer.emails.email_address
             plan_year.start_date, 
@@ -12,7 +14,7 @@ Employer.all.each do |employer|
   row = []
 
   employer.plan_years.each do |plan_year|
-    row = [employer.name, employer.hbx_id, employer.name_first, employer.name_middle, employer.name_last, employer.fein, employer.addresses.last.address_1, employer.addresses.last.address_2, employer.addresses.last.city, employer.addresses.last.state, employer.addresses.last.zip, employer.phones.last.phone_number, employer.emails.try(:last).try(:email_address)]
+    row = [employer.name, employer.hbx_id, employer.name_first, employer.name_middle, employer.name_last, employer.fein, employer.addresses.last.address_1, employer.addresses.last.address_2, employer.addresses.last.city, employer.addresses.last.state, employer.addresses.last.zip, employer.phones.last.try(:phone_number), employer.emails.try(:last).try(:email_address)]
     row << plan_year.start_date
     row << plan_year.broker.name_first << plan_year.broker.name_last << plan_year.broker.npn if plan_year.broker
     file << row
