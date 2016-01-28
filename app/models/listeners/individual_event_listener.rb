9f95a8a8ec953a77c27907d8224a5e41e0241a5f
Trey Evans <lewis.r.evans@gmail.com>
2016-01-28 15:50:46 -0500

    Argument order misunderstanding.

--- app/models/listeners/individual_event_listener.rb
+++ app/models/listeners/individual_event_listener.rb
@@ -28,7 +28,7 @@ module Listeners
           if change_set.multiple_changes?
             if change_set.process_first_edi_change
               resource_event_broadcast("info", "individual_updated_partially", individual_id, r_code, remote_resource)
-              channel.nack(delivery_info.delivery_tag, false, true)
+              channel.nack(delivery_info.delivery_tag, true, false)
             else
               resource_event_broadcast("error", "individual_updated", individual_id, "422", JSON.dump({:resource => remote_resource.to_s, :errors => change_set.full_error_messages }))
               channel.ack(delivery_info.delivery_tag, false)
@@ -74,10 +74,10 @@ module Listeners
         channel.ack(delivery_info.delivery_tag, false)
       when "503"
         resource_error_broadcast("resource_timeout", individual_id, r_code)
-        channel.nack(delivery_info.delivery_tag, false, true)
+        channel.nack(delivery_info.delivery_tag, true, false)
       else
         resource_error_broadcast("unknown_error", individual_id, r_code, resource_or_body)
-        channel.nack(delivery_info.delivery_tag, false, true)
+        channel.ack(delivery_info.delivery_tag, false)
       end
     end
 
