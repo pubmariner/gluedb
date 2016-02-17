99334770c4383376d2bed74c8d8a8641363ec1a5
Zoheb <Zoheb2@zoheb-nenseys-computer-2.local>
2016-02-17 14:40:06 -0500

    Update renewal plan year generation script to filter by FEIN

--- script/queries/generate_renewal_plan_years.rb
+++ script/queries/generate_renewal_plan_years.rb
@@ -1,13 +1,16 @@
-clone_start_date = Date.new(2015,2,1) # This is the 2014 date plan year start for who needs to be renewed. 
+clone_start_date = Date.new(2015,3,1) # This is the 2014 date plan year start for who needs to be renewed. 
 
-new_start_date = Date.new(2016,2,1) # This is the new plan year. 
-new_end_date = Date.new(2017,1,31) # This is the end of the plan year. 
+new_start_date = Date.new(2016,3,1) # This is the new plan year. 
+new_end_date = Date.new(2017,2,28) # This is the end of the plan year. 
 
 plan_years = PlanYear.where(:start_date => clone_start_date)
 
 count = 0
 
+feins = %w()
+
 plan_years.each do |plan_year|
+  next if feins.include?(plan_year.employer.fein) == false
   count += 1
   conflicts = plan_year.employer.plan_years.detect{ |py| py.start_date == new_start_date}
   if conflicts
