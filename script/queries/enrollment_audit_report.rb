c83817e358cd0d185fe9e8ba9770524e9d19e683
Zoheb <zoheb.nensey@gmail.com>
2016-03-28 17:39:41 -0400

    Refs #6216 Added functions to find date termination sent

--- script/queries/enrollment_audit_report.rb
+++ script/queries/enrollment_audit_report.rb
@@ -33,7 +33,47 @@ potential_terminations.each do |policy|
 	end
 end
 
+all_policies_to_analyze = (created_enrollments + has_terminated_member).uniq!
+
 timestamp = Time.now.strftime('%Y%m%d%H%M')
+
+def different_effective_dates(policy)
+	coverage_start_dates = policy.enrollees.map(&:coverage_start).uniq
+	if coverage_start_dates.size > 1
+		return true
+	elsif coverage_start_dates == 1
+		return false
+	end
+end
+
+def date_term_sent(policy,end_date)
+	formatted_end_date = end_date_formatter(end_date)
+	termination_transactions = []
+	policy.transaction_set_enrollments.each do |tse|
+		if tse.body.read(formatted_end_date) != nil
+			termination_transactions.push(tse)
+		end
+	end
+	if termination_transactions.size > 0
+		termination_transactions.sort_by(&:submitted_at)
+		return termination_transactions.first.submitted_at
+	else
+		return policy.updated_at
+	end
+end
+
+def end_date_formatter(date)
+	year = date.year.to_s
+	month = date.month.to_s
+	if month.length == 1
+		month = "0"+month
+	end
+	day = date.day.to_s
+	if day.length == 1
+		day = "0"+day
+	end
+	return year+month+day
+end
 
 Caches::MongoidCache.with_cache_for(Carrier, Plan, Employer) do
 
@@ -47,6 +87,47 @@ CSV.open("enrollment_audit_report_#{timestamp}.csv","w") do |csv|
 	csv << ["","","","","","","","","","","","","",
 			"January","February","March","April","May","June","July","August","September","October","November","December",
 			"January","February","March","April","May","June","July","August","September","October","November","December"]
+	all_policies_to_analyze.each do |policy|
+		market = policy.market
+		policy_id = policy._id
+		carrier = Caches::MongoidCache.lookup(Carrier, pol.carrier_id) {pol.carrier}
+        carrier_name = carrier.name
+        plan = Caches::MongoidCache.lookup(Plan, pol.plan_id) {pol.plan}
+        plan_hios_id = plan.hios_plan_id
+        plan_name = plan.name
+        plan_metal = plan.metal_level
+        if policy.is_shop?
+        	if policy.enrollees.size == 1
+        		premium_total = policy.pre_amt_tot
+        		employer_contribution = policy.tot_emp_res_amt
+        		employer = Caches::MongoidCache.lookup(Employer, pol.employer_id) {pol.employer}
+        		employer_name = employer.name
+        		employer_fein = employer.fein
+        		policy.enrollees.each do |enrollee|
+        			enrollee_person = person_collection.find("members.hbx_member_id" => enrollee.m_id).first
+        			first_name = enrollee_person["name_first"]
+        			last_name = enrollee_person["name_last"]
+        			hbx_id = enrollee.m_id
+        			dob = enrollee_person["members"].first["dob"]
+        			start_date = enrollee.coverage_start
+        			end_date = nil
+        			if enrollee.end_date != nil
+        				end_date = enrollee.end_date
+        				date_sent = date_term_sent(policy,end_date)
+        				csv << [first_name,last_name,hbx_id,dob,market,policy_id,carrier_name,plan_hios_id,plan_name,
+        						start_date,end_date,date_sent,plan_metal,
+        						premium_total,premium_total,premium_total,premium_total,premium_total,premium_total,premium_total,
+        						premium_total,premium_total,premium_total,premium_total,premium_total,
+        						employer_contribution,employer_contribution,employer_contribution,employer_contribution,
+        						employer_contribution,employer_contribution,employer_contribution,employer_contribution,
+        						employer_contribution,employer_contribution,employer_contribution,employer_contribution,
+        						employer_name,employer_fein]
+        			end
+        		end  # Ends enrollee count. 
+        	end # Ends enrollee count evaluator
+        else ## If it's an IVL policy
+        end # Ends SHOP/IVL evaluator
+	end
 end
 
 end # ends MongoidCache
