e1f3f9b1a24e4295ef77f6e28b794892bde3b544
Trey Evans <lewis.r.evans@gmail.com>
2016-04-22 13:14:28 -0400

    Updated query.

--- script/queries/all_users_by_role.rb
+++ script/queries/all_users_by_role.rb
@@ -7,14 +7,14 @@ def user_state(approved, last_sign_in_at)
   last_sign_in_at.blank? ? "never active" : "disabled"
 end
 
-CSV.open("all_glue_users_#{timestamp}.csv","w") do |csv|
-	csv << ["User","Role", "Status", "Inactive Date (if applicable)"]
+CSV.open("all_glue_users_#{timestamp}_by_role.csv","w") do |csv|
+	csv << ["User","Role", "Status", "Creation Date", "Inactive Date (if applicable)"]
 	User.all.each do |user|
 		user_email = user.email
 		user_approval_status = user.approved
 		user_role = user.role
     user_status = user_state(user.approved, user.last_sign_in_at)
     deactivation_date = (user_status == "disabled") ? user.last_sign_in_at.strftime("%Y-%m-%dT%l:%M:%S%z"): nil 
-		csv << [user_email,user_role,user_status,deactivation_date]
+		csv << [user_email,user_role,user_status,nil,deactivation_date]
 	end
 end
