6497af30f7c55ae591952e436188f2e254ba5343
Matt Williams <matt.williams2@dc.gov>
2016-05-24 13:37:42 -0400

    change unix sockets filename to coincide with chef recipe

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,6 +3,6 @@ pid "/var/www/deployments/gluedb/shared/pids/unicorn.pid"
 stderr_path "/var/www/deployments/gluedb/shared/log/unicorn.log"
 stdout_path "/var/www/deployments/gluedb/shared/log/unicorn.log"
 
-listen "/tmp/unicorn.ap.sock"
+listen "/tmp/unicorn_gluedb.ap.sock"
 worker_processes 10
 timeout 180
