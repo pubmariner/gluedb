7811321d536659a8bd06bc4e676e17f25e1cbf71
Battula <vrbattula@outlook.com>
2016-09-16 11:08:25 -0400

    refs #10655 include dental policies

--- script/queries/10655_export_multi_row.rb
+++ script/queries/10655_export_multi_row.rb
@@ -2,25 +2,26 @@ batch_size = 500
 offset = 0
 policy_count = Policy.count
 
-csv =CSV.open("10655_export_glue_multi_row.csv", "w")
+csv =CSV.open("10655_export_glue_multi_row_sep_16.csv", "w")
 
-csv << %w(policy.eg_id hbx_enrollment.policy.policy_start policy.aasm_state policy.plan.coverage_type policy.plan.metal_level
+csv << %w(family.eg_id policy.eg_id hbx_enrollment.policy.policy_start policy.aasm_state policy.plan.coverage_type policy.plan.metal_level
         person.authority_member_id person.authority_member.is_incarcerated person.authority_member.citizen_status
         person.authority_member.is_state_resident is_dependent is_responsible_party?)
 
 def add_to_csv(csv, policy, person, is_dependent=false, is_responsible_party=false)
-  csv << [policy.eg_id, policy.policy_start, policy.aasm_state, policy.plan.coverage_type, policy.plan.metal_level, person.authority_member_id,
-          person.authority_member.is_incarcerated, person.authority_member.citizen_status,
-          person.authority_member.is_state_resident] + [is_dependent, is_responsible_party]
+  csv << ["FAMILY_ID", policy.eg_id, policy.policy_start, policy.aasm_state, policy.plan.coverage_type, policy.plan.metal_level, person.authority_member_id,
+          person.authority_member.try(:is_incarcerated), person.authority_member.try(:citizen_status),
+          person.authority_member.try(:is_state_resident)] + [is_dependent, is_responsible_party]
 end
 
 while offset < policy_count
-  #Family.offset(offset).limit(batch_size).flat_map(&:households).flat_map(&:hbx_enrollments).each do |hbx_enrollment|
-  Policy.offset(offset).limit(batch_size).each do |policy|
+    # Family.offset(offset).limit(batch_size).flat_map(&:households).flat_map(&:hbx_enrollments).each do |hbx_enrollment|
+    Policy.offset(offset).limit(batch_size).each do |policy|
     begin
       next if policy.nil?
       next if policy.policy_start < Date.new(2016, 01, 01)
-      next if (policy.plan.coverage_type != 'health') || (policy.plan.metal_level == "catastrophic") ||
+      next if !policy.is_active?
+      next if (policy.plan.metal_level == "catastrophic") ||
           (policy.applied_aptc != 0) || (policy.plan.market_type == 'shop')
 
       person = policy.subscriber.person
