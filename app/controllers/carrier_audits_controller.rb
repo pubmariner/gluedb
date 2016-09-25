6fdb3453b2bb71f21fdd7c169847752ce6252f6f
Zoheb Nensey <zoheb.nensey@gmail.com>
2016-09-25 11:52:22 -0400

    edit view to pull in carrier ids directly

--- app/controllers/carrier_audits_controller.rb
+++ app/controllers/carrier_audits_controller.rb
@@ -9,7 +9,8 @@ class CarrierAuditsController < ApplicationController
 		@carrier_audit = CarrierAudit.new(params[:carrier_audit])
 		@carrier_audit.active_start = @carrier_audit.cutoff_date.end_of_month
 		@carrier_audit.active_end = (@carrier_audit.active_start) + 1.year
-		binding.pry
+		@carrier_audit.carriers = Carrier.where(:_id => {"$in" => params["carrier_ids"]})
+		@carrier_audit.save
 		redirect_to new_carrier_audit_path
 	end
 end
