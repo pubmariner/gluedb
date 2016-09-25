f52a14619b7faece8e8d75d2351383f372e8140e
Zoheb Nensey <zoheb.nensey@gmail.com>
2016-09-25 14:23:49 -0400

    refining query

--- app/controllers/carrier_audits_controller.rb
+++ app/controllers/carrier_audits_controller.rb
@@ -1,5 +1,9 @@
 class CarrierAuditsController < ApplicationController
 
+	def index
+		@carrier_audits = CarrierAudit.all
+	end
+
 	def new
 		@carrier_audit = CarrierAudit.new(:submitted_by => current_user.email)
 		@carriers = Carrier.all
@@ -7,9 +11,10 @@ class CarrierAuditsController < ApplicationController
 
 	def create
 		@carrier_audit = CarrierAudit.new(params[:carrier_audit])
-		@carrier_audit.active_start = @carrier_audit.cutoff_date.end_of_month
+		@carrier_audit.active_start = (@carrier_audit.cutoff_date.end_of_month) - 1.year
 		@carrier_audit.active_end = (@carrier_audit.active_start) + 1.year
 		@carrier_audit.carriers = Carrier.where(:_id => {"$in" => params["carrier_ids"]})
+		@carrier_audit.submitted_by = current_user.email
 		@carrier_audit.save
 		if @carrier_audit.market == "individual"
 			@carrier_audit.active_start = @carrier_audit.active_end.beginning_of_year
@@ -18,6 +23,6 @@ class CarrierAuditsController < ApplicationController
 		elsif @carrier_audit.market == "shop"
 			@carrier_audit.generate_shop_audits
 		end
-		redirect_to new_carrier_audit_path
+		redirect_to carrier_audits_path
 	end
 end
