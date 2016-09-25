ace0cbc24ddaaf6322c75a1c87cdb24910b83a3b
Zoheb Nensey <zoheb.nensey@gmail.com>
2016-09-25 12:30:43 -0400

    add shop code

--- app/controllers/carrier_audits_controller.rb
+++ app/controllers/carrier_audits_controller.rb
@@ -11,6 +11,12 @@ class CarrierAuditsController < ApplicationController
 		@carrier_audit.active_end = (@carrier_audit.active_start) + 1.year
 		@carrier_audit.carriers = Carrier.where(:_id => {"$in" => params["carrier_ids"]})
 		@carrier_audit.save
+		if @carrier_audit.market == "individual"
+			#@carrier_audit.generate_ivl_audits
+		elsif @carrier_audit.market == "shop"
+			@carrier_audit.active_start = @carrier_audit.active_end.beginning_of_year
+			@carrier_audit.generate_shop_audits
+		end
 		redirect_to new_carrier_audit_path
 	end
 end
--- app/models/carrier_audit.rb
+++ app/models/carrier_audit.rb
@@ -9,8 +9,91 @@ class CarrierAudit
 
 	has_and_belongs_to_many :carriers
 
-	def select_policies
+	def select_plans
+	  plan_ids = Plan.where(:carrier_id => {"$in" => carrier_ids}).map(&:id)
+	  return plan_ids
 	end
 
+	def select_employers
+	  employer_ids = PlanYear.where(:start_date => {"$gt" => active_start, "$lt" => active_end}).map(&:employer_id)
+	  return employer_ids
+	end
+
+	def select_shop_policies(active_start,employer_ids,plan_ids)
+	  eligible_pols = pols = Policy.where({:enrollees => {"$elemMatch" => {:rel_code => "self",
+	  	                                                                   :coverage_start => {"$gt" => active_start}}}, 
+	  	                                  :employer_id => {"$in" => employer_ids}, 
+	  	                                  :plan_id => {"$in" => plan_ids}}).no_timeout
+	  return eligible_pols
+	end
+
+	def get_member_ids(policies)
+	  m_ids = []
+	  policies.each do |policy|
+	    if !policy.canceled?
+	      policy.enrollees.each do |enrollee|
+	      	m_ids << enrollee.m_id
+	      end
+	    end
+	  end
+	end
+
+	def current_plan_year(employer)
+	  today = Time.now.to_date
+	  employer.plan_years.each do |plan_year|
+	  	py_start = plan_year.start_date
+	  	py_end = plan_year.end_date
+	  	date_range = (py_start..py_end)
+	  	if date_range.include?(today)
+	  	  return plan_year
+	  	end
+	  end
+	end
+
+	def in_current_plan_year?(policy,employer)
+	  plan_year = current_plan_year(employer)
+	  policy_start_date = policy.subscriber.coverage_start
+	  py_start = plan_year.start_date
+	  py_end = plan_year.end_date
+	  date_range = (py_start..py_end)
+	  if date_range.include?(policy_start_date)
+	  	return true
+	  else
+	  	return false
+	  end
+	end
+
+	def generate_shop_audits
+	  plan_ids = select_plans
+	  employer_ids = select_employers
+	  shop_policies = select_shop_policies(active_start,employer_ids,plan_ids)
+	  m_ids = get_member_ids(shop_policies)
+	  m_cache = Caches::MemberCache.new(m_ids)
+
+	  shop_policies.each do |policy|
+	  	if !policy.canceled?
+	  	  if !(policy.subscriber.coverage_start > active_end)
+	  	  	employer = Caches::MongoidCache.lookup(Employer, policy.employer_id) {policy.employer}
+	  	  	subscriber_id = policy.subscriber.m_id
+	  	  	subscriber_member = m_cache.lookup(subscriber_id)
+	  	  	auth_subscriber_id = subscriber_member.person.authority_member_id
+	  	  	if auth_subscriber_id == subscriber_id
+	  	  	  enrollee_list = policy.enrollees.reject { |en| en.canceled? }
+	  	  	  all_ids = enrollee_list.map(&:m_id) | [subscriber_id]
+	  	  	  out_f = File.open(File.join("audits", "#{policy._id}_audit.xml"), 'w')
+	  	  	  ser = CanonicalVocabulary::MaintenanceSerializer.new(policy,
+	  	  	  	                                                   "audit",
+	  	  	  	                                                   "notification_only",
+	  	  	  	                                                   all_ids,
+	  	  	  	                                                   all_ids,
+	  	  	  	                                                   { :term_boundry => active_end,
+	  	  	  	                                                   	 :member_repo => m_cache })
+	  	  	  out_f.write(ser.serialize)
+	  	  	  out_f.close
+	  	  	end
+	  	  end
+	  	end
+	  end
+	end
 end
 
