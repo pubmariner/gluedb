7f693504338b8a84906f62f3a9b831dc70811b9c
Trey Evans <lewis.r.evans@gmail.com>
2016-10-02 20:37:23 -0400

    Further policy details.

--- app/interactors/handle_policy_notification/extract_policy_details.rb
+++ app/interactors/handle_policy_notification/extract_policy_details.rb
@@ -5,7 +5,8 @@ module HandlePolicyNotification
     def call
       policy_cv = context.policy_cv
       policy_details = OpenStruct.new({
-        :enrollment_group_id => parse_enrollment_group_id(policy_cv)
+        :enrollment_group_id => parse_enrollment_group_id(policy_cv),
+        :pre_amt_tot => parse_pre_amt_tot(policy_cv)
       })
       context.policy_details = policy_details
     end
@@ -15,6 +16,11 @@ module HandlePolicyNotification
     def parse_enrollment_group_id(policy_cv)
       return nil if policy_cv.id.blank?
       policy_cv.id.split("#").last
+    end
+
+    def parse_pre_amt_tot(policy_cv)
+      return nil if policy_cv.policy_enrollment.blank?
+      policy_cv.policy_enrollment.premium_total_amount
     end
   end
 end
--- spec/interactors/handle_policy_notification/extract_policy_details_spec.rb
+++ spec/interactors/handle_policy_notification/extract_policy_details_spec.rb
@@ -6,9 +6,10 @@ describe HandlePolicyNotification::ExtractPolicyDetails do
 
   describe "given a policy element" do
     let(:enrollment_group_id) { "2938749827349723974" }
-    let(:policy_cv) { instance_double(Openhbx::Cv2::Policy, :id => policy_id) }
+    let(:pre_amt_tot) { "290.13" }
+    let(:enrollment_element) { instance_double(Openhbx::Cv2::PolicyEnrollment, :premium_total_amount => pre_amt_tot) }
+    let(:policy_cv) { instance_double(Openhbx::Cv2::Policy, :id => policy_id, :policy_enrollment => enrollment_element) }
     let(:policy_id) { "urn:openhbx:hbx:dc0:resources:v1:policy:hbx_id##{enrollment_group_id}" }
-
 
     let(:interaction_context) { 
       OpenStruct.new({
@@ -18,6 +19,10 @@ describe HandlePolicyNotification::ExtractPolicyDetails do
 
     it "extracts the enrollment group id" do
       expect(subject.policy_details.enrollment_group_id).to eq enrollment_group_id
+    end
+
+    it "extracts the pre_amt_tot" do
+      expect(subject.policy_details.pre_amt_tot).to eq pre_amt_tot
     end
   end
 end
