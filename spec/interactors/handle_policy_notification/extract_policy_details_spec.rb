b40054031604fcc966e0a98a2af7748169d3606b
Trey Evans <lewis.r.evans@gmail.com>
2016-10-02 20:41:10 -0400

    Tot res amount.

--- spec/interactors/handle_policy_notification/extract_policy_details_spec.rb
+++ spec/interactors/handle_policy_notification/extract_policy_details_spec.rb
@@ -7,7 +7,8 @@ describe HandlePolicyNotification::ExtractPolicyDetails do
   describe "given a policy element" do
     let(:enrollment_group_id) { "2938749827349723974" }
     let(:pre_amt_tot) { "290.13" }
-    let(:enrollment_element) { instance_double(Openhbx::Cv2::PolicyEnrollment, :premium_total_amount => pre_amt_tot) }
+    let(:tot_res_amt) { "123.13" }
+    let(:enrollment_element) { instance_double(Openhbx::Cv2::PolicyEnrollment, :premium_total_amount => pre_amt_tot, :total_responsible_amount => tot_res_amt) }
     let(:policy_cv) { instance_double(Openhbx::Cv2::Policy, :id => policy_id, :policy_enrollment => enrollment_element) }
     let(:policy_id) { "urn:openhbx:hbx:dc0:resources:v1:policy:hbx_id##{enrollment_group_id}" }
 
@@ -23,6 +24,10 @@ describe HandlePolicyNotification::ExtractPolicyDetails do
 
     it "extracts the pre_amt_tot" do
       expect(subject.policy_details.pre_amt_tot).to eq pre_amt_tot
+    end
+
+    it "extracts the tot_res_amt" do
+      expect(subject.policy_details.tot_res_amt).to eq tot_res_amt
     end
   end
 end
