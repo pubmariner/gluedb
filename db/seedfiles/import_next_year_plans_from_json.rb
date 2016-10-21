bfcc0340ad784243195175e54e27381d6208bde8
Zoheb Nensey <zoheb.nensey@gmail.com>
2016-10-21 12:11:47 -0400

    Refs #11628 updating 2017 import script to update plans if they already exist

--- db/seedfiles/import_next_year_plans_from_json.rb
+++ db/seedfiles/import_next_year_plans_from_json.rb
@@ -8,9 +8,15 @@ plan_data = JSON.load(data)
 puts "Before: total #{Plan.count} plans"
 puts "#{plan_data.size} plans in json file"
 plan_data.each do |pd|
-  plan = Plan.new(pd)
-  plan.id = pd["id"]
-  plan.save!
+  plan = Plan.where(year: year.to_i).and(hios_plan_id:pd["hios_plan_id"]).first
+  if plan.blank?
+    plan = Plan.new(pd)
+    plan.id = pd["id"]
+    plan.save!
+  else
+    plan.update_attributes(pd)
+    plan.save
+  end
 end
 puts "After: total #{Plan.count} plans"
 puts "Finished importing #{year} plans"
