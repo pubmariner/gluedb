0b9af88a1ed837224c8cd70d4521d69bd118e634
Battula <vrbattula@outlook.com>
2016-10-26 13:57:18 -0400

    refs 11093 Add aptc and csr from glue

--- script/queries/report_for_curam_11093.rb
+++ script/queries/report_for_curam_11093.rb
@@ -7,9 +7,24 @@ def policy_calculator(policy)
   renewal_policy
 end
 
-report_csv =CSV.open("/Users/Varun/Desktop/reports/oct_18/report_for_curam_oct_19_6_uqhp.csv", "w")
+def csr_percentage(csr_variant_id)
+  return if csr_variant_id.nil?
+  if csr_variant_id == "04"
+    return variant = "73"
+  elsif csr_variant_id == "05"
+    return variant = "87"
+  elsif csr_variant_id == "06"
+    return variant = "94"
+  elsif csr_variant_id == "02"
+    return variant = "100"
+  else
+    return variant = ""
+  end
+end
 
-report_csv << %w(applid ic_ref firstname lastname aptc csr hbxid coverage_type_1 policy.qhp.eg_id 2016qhp 2017crossover_renewal_qhp 2016_qhp_premium 2017_health_premium coverage_type_2 policy.dental.eg_id 2016_dental 2017crossover_renewal_dental 2016_dental_premium 2017_dental_premium)
+report_csv =CSV.open("/Users/Varun/Desktop/reports/oct_26/report_for_curam_oct_26_uqhp.csv", "w")
+
+report_csv << %w(applid ic_ref firstname lastname hbxid applied_aptc csr_variant_id csr_pct coverage_type_1 policy.qhp.eg_id 2016qhp 2017crossover_renewal_qhp 2016_qhp_premium 2017_health_premium applied_aptc(dental) csr_variant_id csr_pct coverage_type_2 policy.dental.eg_id 2016_dental 2017crossover_renewal_dental 2016_dental_premium 2017_dental_premium)
 begin
   csv = CSV.open('/Users/Varun/Downloads/new_rev101316/uqhp_101316.csv',"r",:headers =>true,:encoding => 'ISO-8859-1')
   @data= csv.to_a
@@ -22,14 +37,14 @@ begin
         active_health_policy = active_health_policies.sort{|x,y| y.enrollees.first.coverage_start <=> x.enrollees.first.coverage_start}.first
         active_dental_policies = policies.select{|p| p.is_active? && p.plan.coverage_type == 'dental'}
         active_dental_policy = active_dental_policies.sort{|x,y| y.enrollees.first.coverage_start <=> x.enrollees.first.coverage_start}.first
-        row = [d["applid"],  d["ic_ref"], d["firstname"], d["lastname"], d["aptc"],  d["csr"], d["hbxid"]]
+        row = [d["applid"],  d["ic_ref"], d["firstname"], d["lastname"], d["hbxid"]]
         if active_health_policy 
-          row += [active_health_policy.coverage_type, active_health_policy.eg_id, active_health_policy.plan.name, active_health_policy.plan.try(:renewal_plan).try(:name), active_health_policy.pre_amt_tot.to_s]
+          row += [active_health_policy.applied_aptc, active_health_policy.plan.csr_variant_id, csr_percentage(active_health_policy.plan.csr_variant_id), active_health_policy.coverage_type, active_health_policy.eg_id, active_health_policy.plan.name, active_health_policy.plan.try(:renewal_plan).try(:name), active_health_policy.pre_amt_tot.to_s]
           renewal_health = policy_calculator(active_health_policy)
           row += [renewal_health.try(:pre_amt_tot)]
         end
         if active_dental_policy
-          row += [active_dental_policy.plan.coverage_type, active_dental_policy.eg_id, active_dental_policy.plan.name, active_dental_policy.plan.try(:renewal_plan).try(:name), active_dental_policy.pre_amt_tot.to_s]
+          row += [active_dental_policy.applied_aptc, active_dental_policy.plan.csr_variant_id, csr_percentage(active_dental_policy.plan.csr_variant_id), active_dental_policy.plan.coverage_type, active_dental_policy.eg_id, active_dental_policy.plan.name, active_dental_policy.plan.try(:renewal_plan).try(:name), active_dental_policy.pre_amt_tot.to_s]
           renewal_dental= policy_calculator(active_dental_policy)
           row += [renewal_dental.try(:pre_amt_tot)]
         end
