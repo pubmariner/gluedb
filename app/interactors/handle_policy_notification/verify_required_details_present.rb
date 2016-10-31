187a00ea8f7f65943b2bac178d3ebf873173555a
Zoheb Nensey <zoheb.nensey@gmail.com>
2016-10-31 01:40:28 -0400

    Refs #11797 added policy details checks

--- app/interactors/handle_policy_notification/verify_required_details_present.rb
+++ app/interactors/handle_policy_notification/verify_required_details_present.rb
@@ -4,14 +4,24 @@ module HandlePolicyNotification
     # - policy_details (Openhbx::Cv2::Policy)
     # - plan_details (HandlePolicyNotification::PlanDetails)
     # - member_detail_collection (array of HandlePolicyNotification::MemberDetails)
-    # - broker_details (HandlePolicyNotification::BrokerDetails may be nil)
-    # - employer_details (HandlePolicyNotification::EmployerDetails may be nil)
+    # -  (HandlePolicyNotification::BrokerDetails may be nil)
+    # - empbroker_detailsloyer_details (HandlePolicyNotification::EmployerDetails may be nil)
     # - processing_errors (HandlePolicyNotification::ProcessingErrors)
     #
     # Call "fail!" if validation does not pass.
     def call
+      if policy_details.market.blank?
+        context.processing_errors.errors.add(:policy_details, "No market found")
+      end
+      if policy_details.enrollment_group_id.blank?
+        context.processing_errors.errors.add(:policy_details, "No Enrollment Group ID was submitted.")
+      end
+      if policy_details.pre_amt_tot.blank? || policy_details.tot_res_amt.blank? || policy_details.tot_emp_res_amt.blank?
+        context.processing_errors.errors.add(:policy_details, "One ore more pieces of premium data was not included."
+      end
+      
       if plan_details.found_plan.nil?
-        processing_errors.errors.add(
+        context.processing_errors.errors.add(
            :plan_details,
            "No plan found with HIOS ID #{plan_details.hios_id} and active year #{plan_details.active_year}"
         )
