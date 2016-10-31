8f943d717a369f8e20a7390824dfe17b41d09fba
Trey Evans <lewis.r.evans@gmail.com>
2016-10-31 17:28:04 -0400

    Revert "Refs #11797 verify_required_details_present validations"
    This reverts commit d9952429b0815e1170995532310ec42b9317e31a.

--- app/interactors/handle_policy_notification/verify_required_details_present.rb
+++ app/interactors/handle_policy_notification/verify_required_details_present.rb
@@ -11,61 +11,9 @@ module HandlePolicyNotification
     #
     # Call "fail!" if validation does not pass.
 
-    def call
-      processing_errors = context.processing_errors
-      @errors = processing_errors.errors
-      policy_cv = context.policy_cv
-      if parse_market.nil?
-        @errors.add(:policy_details, "No market found")
-      end
-
-      if parse_enrollment_group_id(policy_cv).nil?
-        @errors.add(:policy_details, "No Enrollment Group ID was submitted.")
-      end
-
-      if parse_pre_amt_tot.nil? || parse_tot_res_amt.nil? || parse_tot_emp_res_amt.nil?
-        @errors.add(:policy_details, "One ore more pieces of premium data was not included.")
-      end
-
-      if plan_details.found_plan.nil?
-        @errors.add(
-           :plan_details,
-           "No plan found with HIOS ID #{plan_details.hios_id} and active year #{plan_details.active_year}"
-        )
-      end
-
-      if plan_details.found_plan.present? && plan_details.found_plan.try(:year) >= 2017 && plan_details.found_plan.market_type != parse_market
-        @errors.add(:plan_details, "Plan submitted doesn't match the market.")
-      end
-
-      member_details_collection.each do |member_details|
-        if member_details.found_member.blank?
-          @errors.add( :member_details, "No member found with hbx id #{member_details.member_id}")
-        end
-        if member_details.is_subscriber.blank?
-          @errors.add( :member_details, "#{member_details.member_id} doesn't have the subscriber/dependent indicator set.")
-        end
-        if member_details.begin_date.blank?
-          @errors.add( :member_details, "hbx id #{member_details.member_id} does not have a begin date")
-        end
-      end
-
-      if broker_details.found_broker.blank?
-        @errors.add( :broker_details, "No broker found with npn #{broker_details.npn}")
-      end
-
-      if parse_market == "shop" && employer_details.found_employer.blank?
-        @errors.add( :employer_details, "No employer found with fein #{employer_details.fein}")
-      end
-
-      if processing_errors.has_errors?
-        context.fail!
-      end
-    end
-
-    def parse_market
-      return nil if policy_enrollment.blank?
-      policy_enrollment.shop_market.present? ? "shop" : "individual"
+    def parse_market(policy_cv)
+      return nil if policy_cv.policy_enrollment.blank?
+      policy_cv.policy_enrollment.shop_market.present? ? "shop" : "individual"
     end
 
     def parse_enrollment_group_id(policy_cv)
@@ -73,44 +21,74 @@ module HandlePolicyNotification
       policy_cv.id.split("#").last
     end
 
-    def parse_pre_amt_tot
-      return nil if policy_enrollment.blank?
-      policy_enrollment.premium_total_amount
+    def parse_pre_amt_tot(policy_cv)
+      return nil if policy_cv.policy_enrollment.blank?
+      policy_cv.policy_enrollment.premium_total_amount
     end
 
-    def parse_tot_res_amt
-      return nil if policy_enrollment.blank?
-      policy_enrollment.total_responsible_amount
+    def parse_tot_res_amt(policy_cv)
+      return nil if policy_cv.policy_enrollment.blank?
+      policy_cv.policy_enrollment.total_responsible_amount
     end
 
-    def parse_tot_emp_res_amt
-      return nil if policy_enrollment.blank?
-      return nil if policy_enrollment.shop_market.blank?
-      policy_enrollment.shop_market.total_employer_responsible_amount
+    def parse_tot_emp_res_amt(policy_cv)
+      return nil if policy_cv.policy_enrollment.blank?
+      return nil if policy_cv.policy_enrollment.shop_market.blank?
+      policy_cv.policy_enrollment.shop_market.total_employer_responsible_amount
     end
 
-    def policy_details
-      context.policy_cv
-    end
+    def call
+      policy_cv = context.policy_cv
+      if parse_market(policy_cv).nil?
+        context.processing_errors.errors.add(:policy_details, "No market found")
+      end
 
-    def policy_enrollment
-      policy_details.policy_enrollment
-    end
+      if parse_enrollment_group_id(policy_cv).nil?
+        context.processing_errors.errors.add(:policy_details, "No Enrollment Group ID was submitted.")
+      end
 
-    def plan_details
-      policy_enrollment.plan
-    end
+      if parse_pre_amt_tot(policy_cv).nil? || parse_tot_res_amt(policy_cv).nil? || parse_tot_emp_res_amt(policy_cv).nil?
+        context.processing_errors.errors.add(:policy_details, "One ore more pieces of premium data was not included.")
+      end
 
-    def broker_details
-      policy_details.broker_link
-    end
+      if plan_details.found_plan.nil?
+        context.processing_errors.errors.add(
+           :plan_details,
+           "No plan found with HIOS ID #{plan_details.hios_id} and active year #{plan_details.active_year}"
+        )
+      end
 
-    def member_details_collection
-      policy_details.enrollees
-    end
+      if plan_details.found_plan.year >= 2017 && plan_details.found_plan.market_type != policy_details.market
+       context.processing_errors.errors.add(:plan_details, "Plan submitted doesn't match the market.")
+      end
 
-    def employer_details
-      policy_enrollment.shop_market.employer_link
+      member_details_collection.each do |member_details|
+        if member_details.found_member.blank?
+          processing_errors.errors.add( :member_details, "No member found with hbx id #{member_details.member_id}")
+        end
+        if member_details.is_subscriber.blank?
+          processing_errors.errors.add( :member_details, "#{member_details.member_id} doesn't have the subscriber/dependent indicator set.")
+        end
+        if member_details.coverage_start.blank?
+          processing_errors.errors.add( :member_details, "hbx id #{member_details.member_id} does not have a coverage start date")
+        end
+      end
+
+      if broker_details.found_broker.blank?
+        processing_errors.errors.add( :broker_details, "No broker found with npn #{broker_details.npn}")
+      end
+
+      if policy_details.market == "shop" && employer_details.found_employer.blank?
+        processing_errors.errors.add( :employer_details, "No employer found with fein #{employer_details.fein}")
+      end
+
+      if policy_details.market == "shop"
+        processing_errors.errors.add( :market_type, "we don't support shop yet" )
+      end
+
+      if processing_errors.has_errors?
+        fail!
+      end
     end
   end
 end
--- spec/interactors/handle_policy_notification/verify_required_details_present_spec.rb
+++ spec/interactors/handle_policy_notification/verify_required_details_present_spec.rb
@@ -2,85 +2,16 @@ require "rails_helper"
 
 describe HandlePolicyNotification::VerifyRequiredDetailsPresent do
   let(:enrollment_group_id) { "2938749827349723974" }
-  let(:pre_amt_tot) { 290.13 }
-  let(:tot_res_amt) { 123.13 }
-  let(:tot_emp_res_amt) { 234.30 }
-  let(:hios_id) { "88889999888833" }
-
-  let(:member_id) { "2938749827349723974" }
-  let(:is_subscriber) { true }
-  let(:member_premium) { 123.13 }
-  let(:coverage_start) { "20160101" }
-  let(:coverage_end) { "20161231" }
-  let(:eligibility_start) { "20160103" }
-
-  let(:plan_record){ FactoryGirl.create(:plan, year: 2017, market_type: "shop") }
-  let(:broker_record){ instance_double("Broker") }
-  let(:employer_record){ instance_double("Employer") }
-  let(:member_record){ instance_double("Member") }
-
-  let(:enrollee) {
-    ::HandlePolicyNotification::MemberDetails.new({
-       :premium_amount => member_premium,
-       :member_id => member_id,
-       :is_subscriber => true,
-       :begin_date => coverage_start,
-       :end_date => coverage_end,
-       :eligibility_begin_date => eligibility_start
-    })
-  }
-
-  let(:broker_link) {
-    HandlePolicyNotification::BrokerDetails.new({
-       :npn => "3838383838"
-    })
-  }
-
-  let(:policy_cv) {
-    instance_double(
-      Openhbx::Cv2::Policy,
-      :id => policy_id,
-      :policy_enrollment => policy_enrollment,
-      enrollees: [enrollee],
-      broker_link: broker_link
-    )
-  }
-
-  let(:plan_link){
-    ::HandlePolicyNotification::PlanDetails.new({
-      :hios_id => hios_id,
-      :active_year => 2017
-    })
-  }
-
-  let(:policy_enrollment) {
-    instance_double(
-      Openhbx::Cv2::PolicyEnrollment,
-      plan: plan_link,
-      :premium_total_amount => pre_amt_tot,
-      :total_responsible_amount => tot_res_amt,
-      :shop_market => shop_enrollment_element
-    )
-  }
-
-  let(:shop_enrollment_element) {
-    instance_double(
-      Openhbx::Cv2::PolicyEnrollmentShopMarket,
-      :total_employer_responsible_amount => tot_emp_res_amt,
-      employer_link: employer_link
-    )
-  }
-
-  let(:employer_link){
-    ::HandlePolicyNotification::EmployerDetails.new({
-      :fein => "484848484"
-    })
-  }
+  let(:pre_amt_tot) { "290.13" }
+  let(:tot_res_amt) { "123.13" }
+  let(:tot_emp_res_amt) { "234.30" }
+  let(:policy_cv) { instance_double(Openhbx::Cv2::Policy, :id => policy_id, :policy_enrollment => enrollment_element) }
+  let(:shop_enrollment_element) { instance_double(Openhbx::Cv2::PolicyEnrollmentShopMarket, :total_employer_responsible_amount => tot_emp_res_amt) }
+  let(:individual_enrollment_element) { instance_double(Openhbx::Cv2::PolicyEnrollmentIndividualMarket, :is_carrier_to_bill => "is_carrier_to_bill", elected_aptc_percent: "elected_aptc_percent", applied_aptc_amount: "applied_aptc_amount", allocated_aptc_amount: "allocated_aptc_amount") }
 
   let(:interaction_context) {
     OpenStruct.new({
-      :policy_cv => policy_cv,
-      :processing_errors => HandlePolicyNotification::ProcessingErrors.new
+      :policy_cv => policy_cv
     })
   }
 
@@ -88,40 +19,27 @@ describe HandlePolicyNotification::VerifyRequiredDetailsPresent do
 
 
   describe "given a policy element" do
+    let(:enrollment_element) { instance_double(Openhbx::Cv2::PolicyEnrollment, :premium_total_amount => pre_amt_tot, :total_responsible_amount => tot_res_amt, :shop_market => shop_enrollment_element) }
     let(:policy_id) { "urn:openhbx:hbx:dc0:resources:v1:policy:hbx_id##{enrollment_group_id}" }
 
-    before :each do
-      allow(plan_link).to receive(:found_plan).and_return(plan_record)
-      allow(broker_link).to receive(:found_broker).and_return(broker_record)
-      allow(employer_link).to receive(:found_employer).and_return(employer_record)
-      allow(enrollee).to receive(:found_member).and_return(member_record)
-    end
-
     it "extracts the enrollment group id" do
-      expect(subject.policy_cv.id.split("#").last).to eq enrollment_group_id
+      expect(subject.policy_details.enrollment_group_id).to eq enrollment_group_id
     end
 
     it "extracts the pre_amt_tot" do
-      expect(subject.policy_cv.policy_enrollment.premium_total_amount).to eq pre_amt_tot
+      expect(subject.policy_details.pre_amt_tot).to eq pre_amt_tot
     end
 
-    it "should return no error" do
-      expect(subject.processing_errors.has_errors?).to eq false
+    it "extracts the market" do
+      expect(subject.policy_details.market).to eq "shop"
     end
 
+    it "extracts the tot_res_amt" do
+      expect(subject.policy_details.tot_res_amt).to eq tot_res_amt
+    end
   end
 
-  describe "given a policy element" do
-    let(:policy_id) { "urn:openhbx:hbx:dc0:resources:v1:policy:hbx_id##{enrollment_group_id}" }
-
-    it "should return error" do
-      expect(subject.processing_errors.has_errors?).to eq true
-      expect(subject.processing_errors.errors[:employer_details]).to eq ["No employer found with fein 484848484"]
-      expect(subject.processing_errors.errors[:plan_details]).to eq ["No plan found with HIOS ID 88889999888833 and active year 2017"]
-      expect(subject.processing_errors.errors[:member_details]).to eq ["No member found with hbx id 2938749827349723974"]
-      expect(subject.processing_errors.errors[:broker_details]).to eq ["No broker found with npn 3838383838"]
-    end
-
+  describe "given a plan element" do 
+    ## fill in specs here
   end
-
 end
