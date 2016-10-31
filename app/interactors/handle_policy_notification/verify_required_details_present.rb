4da50230c3548fa542dc2299f9197d04476a0334
Zoheb Nensey <zoheb.nensey@gmail.com>
2016-10-31 01:54:29 -0400

    Refs #11797 adding more checks for member details collection passed in as array

--- app/interactors/handle_policy_notification/verify_required_details_present.rb
+++ app/interactors/handle_policy_notification/verify_required_details_present.rb
@@ -19,12 +19,26 @@ module HandlePolicyNotification
       if policy_details.pre_amt_tot.blank? || policy_details.tot_res_amt.blank? || policy_details.tot_emp_res_amt.blank?
         context.processing_errors.errors.add(:policy_details, "One ore more pieces of premium data was not included."
       end
-      
+
       if plan_details.found_plan.nil?
         context.processing_errors.errors.add(
            :plan_details,
            "No plan found with HIOS ID #{plan_details.hios_id} and active year #{plan_details.active_year}"
         )
+      end
+
+      member_details_collection.each do |member_details|
+        if member_details.found_member.blank?
+          processing_errors.errors.add( :member_details, "No member found with hbx id #{member_details.member_id}")
+        end
+      end
+
+      if broker_details.found_broker.blank?
+        processing_errors.errors.add( :broker_details, "No broker found with npn #{broker_details.npn}"
+      end
+
+      if employer_details.found_employer.blank?
+        processing_errors.errors.add( :employer_details, "No employer found with fein #{employer_details.fein}")
       end
 
       if processing_errors.has_errors?
