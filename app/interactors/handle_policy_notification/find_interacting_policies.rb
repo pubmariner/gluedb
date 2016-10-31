b9a577b6a99f5095dbc3c6be6650a50772650d4e
Trey Evans <lewis.r.evans@gmail.com>
2016-10-30 22:10:53 -0400

    Refs #11804.  Overlapping policy work begins.
    Added more information to policy details to make it easier to find the
    policies which might overlap with the enrollment.

--- app/interactors/handle_policy_notification/find_interacting_policies.rb
+++ app/interactors/handle_policy_notification/find_interacting_policies.rb
@@ -5,10 +5,18 @@ module HandlePolicyNotification
     # Context requires:
     # - policy_details (Openhbx::Cv2::Policy)
     # - plan_details (HandlePolicyNotification::PlanDetails)
+    # - employer_details (HandlePolicyNotification::EmployerDetails)
     # - member_detail_collection (array of HandlePolicyNotification::MemberDetails)
     # Context outputs:
-    # - interacting_policies (array of Policy)
+    # - interacting_policies (array of Policy, may be empty)
     def call
+      subscriber_member_details = member_detail_collection.detect { |md| md.is_subscriber? }
+      subscriber_member = Person.where("members.hbx_assigned_id" => subscriber_member_details.member_id).first
+      coverage_start = subscriber_member_details.coverage_start
+      coverage_end = subscriber_member_details.coverage_end
+      if policy_details.market == "shop"
+      else
+      end
     end
   end
 end
--- app/models/handle_policy_notification/policy_details.rb
+++ app/models/handle_policy_notification/policy_details.rb
@@ -2,6 +2,7 @@ module HandlePolicyNotification
   class PolicyDetails
     include Virtus.model
 
+    attribute :market, String # Should be either "shop" or "individual"
     attribute :enrollment_group_id, String
     attribute :pre_amt_tot, String
     attribute :tot_res_amt, String
