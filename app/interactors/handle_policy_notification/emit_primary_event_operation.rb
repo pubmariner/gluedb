73a29ad8c99593414353b95d5fa05a06f8c82b00
Trey Evans <lewis.r.evans@gmail.com>
2016-10-31 13:09:21 -0400

    Fixing typing.

--- app/interactors/handle_policy_notification/emit_primary_event_operation.rb
+++ app/interactors/handle_policy_notification/emit_primary_event_operation.rb
@@ -4,15 +4,17 @@ module HandlePolicyNotification
     include Interactor
 
     # Context requires:
+    # - raw_policy_xml (a Nokogiri node representing the original received policy node)
     # - primary_policy_action (a HandlePolicyNotification::PolicyAction)
     def call
       action_xml = ApplicationController.new.render_to_string({
-        :template => "edi_codec_events/enrollment_event"
+        :template => "edi_codec_events/enrollment_event",
         :format => :xml,
         :locals => {:enrollment_event => context.primary_policy_action}
       })
       edi_builder = EdiCodec::X12::EdiBuilder.new(action_xml)
       x12_xml = edi_builder.call.to_xml
+      publish_to_bus(x12_xml)
     end
 
     def publish_to_bus(x12_payload)
