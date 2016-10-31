a043c92b66fb9c48cf58813fd94d10204f879ee8
vsonnathi <vsonnathi@Vineels-MacBook-Pro.local>
2016-10-31 00:17:33 -0400

    Refs #11790 : Added market in policy_details and updated specs.

--- spec/interactors/handle_policy_notification/extract_policy_details_spec.rb
+++ spec/interactors/handle_policy_notification/extract_policy_details_spec.rb
@@ -4,9 +4,12 @@ describe HandlePolicyNotification::ExtractPolicyDetails do
   let(:enrollment_group_id) { "2938749827349723974" }
   let(:pre_amt_tot) { "290.13" }
   let(:tot_res_amt) { "123.13" }
+  let(:tot_emp_res_amt) { "234.30" }
   let(:policy_cv) { instance_double(Openhbx::Cv2::Policy, :id => policy_id, :policy_enrollment => enrollment_element) }
+  let(:shop_enrollment_element) { instance_double(Openhbx::Cv2::PolicyEnrollmentShopMarket, :total_employer_responsible_amount => tot_emp_res_amt) }
+  let(:individual_enrollment_element) { instance_double(Openhbx::Cv2::PolicyEnrollmentIndividualMarket, :is_carrier_to_bill => "is_carrier_to_bill", elected_aptc_percent: "elected_aptc_percent", applied_aptc_amount: "applied_aptc_amount", allocated_aptc_amount: "allocated_aptc_amount") }
 
-  let(:interaction_context) { 
+  let(:interaction_context) {
     OpenStruct.new({
       :policy_cv => policy_cv
     })
@@ -15,7 +18,7 @@ describe HandlePolicyNotification::ExtractPolicyDetails do
   subject { HandlePolicyNotification::ExtractPolicyDetails.call(interaction_context) }
 
   describe "given a policy element" do
-    let(:enrollment_element) { instance_double(Openhbx::Cv2::PolicyEnrollment, :premium_total_amount => pre_amt_tot, :total_responsible_amount => tot_res_amt, :shop_market => nil) }
+    let(:enrollment_element) { instance_double(Openhbx::Cv2::PolicyEnrollment, :premium_total_amount => pre_amt_tot, :total_responsible_amount => tot_res_amt, :shop_market => shop_enrollment_element) }
     let(:policy_id) { "urn:openhbx:hbx:dc0:resources:v1:policy:hbx_id##{enrollment_group_id}" }
 
     it "extracts the enrollment group id" do
@@ -26,19 +29,30 @@ describe HandlePolicyNotification::ExtractPolicyDetails do
       expect(subject.policy_details.pre_amt_tot).to eq pre_amt_tot
     end
 
+    it "extracts the market" do
+      expect(subject.policy_details.market).to eq "shop"
+    end
+
     it "extracts the tot_res_amt" do
       expect(subject.policy_details.tot_res_amt).to eq tot_res_amt
     end
   end
 
   describe "given a policy element with shop information" do
-    let(:tot_emp_res_amt) { "234.30" }
-    let(:shop_enrollment_element) { instance_double(Openhbx::Cv2::PolicyEnrollmentShopMarket, :total_employer_responsible_amount => tot_emp_res_amt) }
     let(:enrollment_element) { instance_double(Openhbx::Cv2::PolicyEnrollment, :premium_total_amount => pre_amt_tot, :total_responsible_amount => tot_res_amt, :shop_market => shop_enrollment_element) }
     let(:policy_id) { "urn:openhbx:hbx:dc0:resources:v1:policy:hbx_id##{enrollment_group_id}" }
 
     it "extracts the tot_emp_res_amt" do
       expect(subject.policy_details.tot_emp_res_amt).to eq tot_emp_res_amt
+    end
+  end
+
+  describe "given a policy element with ivl information" do
+    let(:enrollment_element) { instance_double(Openhbx::Cv2::PolicyEnrollment, :premium_total_amount => pre_amt_tot, :total_responsible_amount => tot_res_amt, :individual_market => individual_enrollment_element, shop_market: nil) }
+    let(:policy_id) { "urn:openhbx:hbx:dc0:resources:v1:policy:hbx_id##{enrollment_group_id}" }
+
+    it "extracts ivl market" do
+      expect(subject.policy_details.market).to eq "individual"
     end
   end
 end
