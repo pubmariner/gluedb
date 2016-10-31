be6adbe1b007d12426a5f456b4313fe05596ed1a
Trey Evans <lewis.r.evans@gmail.com>
2016-10-30 22:57:43 -0400

    Additional work on interacting policies.

--- app/interactors/handle_policy_notification/find_interacting_policies.rb
+++ app/interactors/handle_policy_notification/find_interacting_policies.rb
@@ -10,13 +10,24 @@ module HandlePolicyNotification
     # Context outputs:
     # - interacting_policies (array of Policy, may be empty)
     def call
-      subscriber_member_details = member_detail_collection.detect { |md| md.is_subscriber? }
-      subscriber_member = Person.where("members.hbx_assigned_id" => subscriber_member_details.member_id).first
+      subscriber_member_details = context.member_detail_collection.detect { |md| md.is_subscriber? }
+      subscriber_person = Person.where("members.hbx_assigned_id" => subscriber_member_details.member_id).first
       coverage_start = subscriber_member_details.coverage_start
       coverage_end = subscriber_member_details.coverage_end
-      if policy_details.market == "shop"
+      possible_matches = []
+      if context.policy_details.market == "shop"
       else
+        possible_matches = subscriber_person.policies.select do |pol|
+          (pol.plan.market_type != "shop") &&
+            (pol.plan.coverage_type == context.plan_details.found_plan_coverage_type) &&
+            ivl_dates_overlap(coverage_start, coverage_end, pol.policy_start, pol.policy_end)
+        end
       end
+      context.interacting_policies = possible_matches.reject { |pol| pol.canceled? }
+    end
+
+    def ivl_dates_overlap(coverage_start, coverage_end, other_start, other_end)
+
     end
   end
 end
