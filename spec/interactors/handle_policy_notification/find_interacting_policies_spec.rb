1c994d3414ee85c851cf803ed443ec193535b899
Kevin Wei <kevin.tw.wei@gmail.com>
2016-10-31 20:14:09 -0400

    Refs #11798 rest of specs and minor fixes that come with writing specs

--- spec/interactors/handle_policy_notification/find_interacting_policies_spec.rb
+++ spec/interactors/handle_policy_notification/find_interacting_policies_spec.rb
@@ -1,6 +1,76 @@
 require "rails_helper"
 
 describe HandlePolicyNotification::FindInteractingPolicies do
+  describe ".call" do
+    let(:member_detail_collection) { double }
+    let(:employer_details) { double(found_employer: employer) }
+    let(:employer) { double }
+    let(:plan_details) { double(found_plan: plan) }
+    let(:plan) { double(coverage_type: "health")}
+    let(:member_details) { double(member_id: "some_id", begin_date: Date.today, end_date: Date.today + 2.weeks) }
+    let(:interacting_policies) {[]}
+    let(:person) { FactoryGirl.create(:person)}
+    let(:policy) { FactoryGirl.create(:policy, broker: nil)}
+
+    let(:interaction_context) {
+      OpenStruct.new({
+        member_detail_collection: member_detail_collection,
+        employer_details: employer_details,
+        policy_details: policy_details,
+        plan_details: plan_details,
+      })
+    }
+
+    subject { HandlePolicyNotification::FindInteractingPolicies.call(interaction_context) }
+
+    before do
+      allow(member_detail_collection).to receive(:detect).and_return(member_details)
+      allow(Person).to receive(:where).with({"members.hbx_member_id" => member_details.member_id}).and_return([person])
+      allow(person).to receive(:policies).and_return([policy])
+      policy.enrollees.each{|e| e.update_attributes(coverage_start: Date.today + 1.week, coverage_end: Date.today + 3.weeks) }
+    end
+
+    context "individual policy" do
+      let(:policy_details) { double(market: "individual") }
+
+      it "should return the policy if they dates overlap" do
+        expect(subject.interacting_policies).to eq [policy]
+      end
+
+      context "with different years" do
+        let(:member_details) { double(member_id: "some_id", begin_date: Date.today + 1.year, end_date: Date.today + 1.year + 2.weeks) }
+
+        it "should return an empty array if they dates overlap" do
+          expect(subject.interacting_policies).to eq []
+        end
+      end
+    end
+
+    context "shop policy" do
+      let(:policy_details) { double(market: "shop") }
+      let(:policy) { FactoryGirl.create(:policy, broker: nil) }
+      let(:employer) { instance_double("Employer", id: "employer_fein", plan_year_of: plan_year) }
+      let(:plan_year) { instance_double("PlanYear", start_date: Date.today) }
+
+      before do
+        allow(policy).to receive(:employer).and_return(employer)
+        policy.plan.update_attributes(market_type: "shop")
+      end
+
+      it "should return the policy if they dates overlap" do
+        expect(subject.interacting_policies).to eq [policy]
+      end
+
+      context "with different years" do
+        let(:member_details) { double(member_id: "some_id", begin_date: Date.today + 1.year, end_date: Date.today + 1.year + 2.weeks) }
+
+        it "should return an empty array if they dates overlap" do
+          expect(subject.interacting_policies).to eq []
+        end
+      end
+    end
+  end
+
   describe ".overlap_date_range" do
     subject { HandlePolicyNotification::FindInteractingPolicies.new.overlap_date_range(event_start,event_end,policy_start,policy_end,event_year,policy_year) }
     let(:start_date) { Date.today }
@@ -37,7 +107,7 @@ describe HandlePolicyNotification::FindInteractingPolicies do
       end
     end
 
-    context "event start is before policy start" do
+    context "event is before policy" do
       let(:event_start) { policy_start - 2.months }
 
       context "event has no end date" do
@@ -65,7 +135,7 @@ describe HandlePolicyNotification::FindInteractingPolicies do
       end
     end
 
-    context "policy start is before event start" do
+    context "policy is before event" do
       let(:policy_start) { event_start - 2.months }
 
       context "policy has no end date" do
