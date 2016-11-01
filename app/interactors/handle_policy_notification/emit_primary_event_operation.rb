ff302d77167b1e517a8b9ac44da39a1d2f81edc0
Trey Evans <lewis.r.evans@gmail.com>
2016-10-31 22:40:51 -0400

    Should be close to complete.

--- app/interactors/handle_policy_notification/emit_primary_event_operation.rb
+++ app/interactors/handle_policy_notification/emit_primary_event_operation.rb
@@ -13,7 +13,6 @@ module HandlePolicyNotification
         :format => :xml,
         :locals => {:enrollment_event => context.primary_policy_action, :raw_policy_xml => context.raw_policy_xml}
       })
-      puts action_xml
       edi_builder = EdiCodec::X12::BenefitEnrollment.new(action_xml)
       x12_xml = edi_builder.call.to_xml
       publish_to_bus(x12_xml)
