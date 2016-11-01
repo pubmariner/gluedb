fc5e23646ab1dc5c090e83008625c8bcd4f853f3
Trey Evans <lewis.r.evans@gmail.com>
2016-11-01 03:49:28 -0400

    Fix brokers.

--- app/interactors/handle_policy_notification/extract_broker_details.rb
+++ app/interactors/handle_policy_notification/extract_broker_details.rb
@@ -8,7 +8,7 @@ module HandlePolicyNotification
     # - broker_details (HandlePolicyNotification::BrokerDetails may be nil)
     def call
       return if context.policy_cv.broker_link.nil?
-      broker_link = policy_cv.broker_link
+      broker_link = context.policy_cv.broker_link
       context.broker_details = HandlePolicyNotification::BrokerDetails.new({
          :npn => broker_link.npn_from_id
       })
