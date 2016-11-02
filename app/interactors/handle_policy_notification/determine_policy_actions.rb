b4322ac8bbe0442bf39494cec13a1d4e7f150353
Trey Evans <lewis.r.evans@gmail.com>
2016-11-02 12:00:25 -0400

    Small fix to address dental issue.
    I've temporarily made 'active' renewals into passives to address the
    dental transmission code issue.

--- app/interactors/handle_policy_notification/determine_policy_actions.rb
+++ app/interactors/handle_policy_notification/determine_policy_actions.rb
@@ -48,8 +48,15 @@ module HandlePolicyNotification
         context.fail!
       elsif context.interacting_policies.any? && context.renewal_policies.any?
         # Either plan change, add, or 'active' renewal
-        context.processing_errors.errors.add(:event_kind, "right now we only handle passive renewals, this looks like a change with possible renewals")
-        context.fail!
+          # Making this a passive renewal for the moment because we had the dental issue.
+          primary_policy_action = build_passive_renewal_on(
+             context.policy_details,
+             context.member_detail_collection,
+             context.plan_details,
+             context.broker_details,
+             context.employer_details)
+#        context.processing_errors.errors.add(:event_kind, "right now we only handle passive renewals, this looks like a change with possible renewals")
+#        context.fail!
       end
       context.primary_policy_action = primary_policy_action
       context.other_policy_actions = other_policy_actions
