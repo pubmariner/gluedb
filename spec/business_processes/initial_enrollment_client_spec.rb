bc20f40e0df3b0389fc5304b53344987d89ae43c
Trey Evans <lewis.r.evans@gmail.com>
2016-11-16 16:42:57 -0500

    Making sure I can reuse it.

--- spec/business_processes/initial_enrollment_client_spec.rb
+++ spec/business_processes/initial_enrollment_client_spec.rb
@@ -1,6 +1,14 @@
 require "rails_helper"
 
 describe InitialEnrollmentClient do
+  it "can be composed inside of other processes" do
+    expect {
+      Middleware::Builder.new do |b|
+        b.use subject.stack
+      end.to_app
+    }
+  end
+
   it "has steps in the stack" do
     expect(subject.steps).not_to be_empty
   end
