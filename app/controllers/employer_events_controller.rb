c9b801df313a78ea65a824e868df9d9275f51641
Trey Evans <lewis.r.evans@gmail.com>
2016-12-09 15:35:21 -0500

    Correct constant name.

--- app/controllers/employer_events_controller.rb
+++ app/controllers/employer_events_controller.rb
@@ -2,6 +2,6 @@ class EmployerEventsController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @employer_events = EmployerEvents.sort(event_time: 1)
+    @employer_events = EmployerEvent.order_by(event_time: 1)
   end
 end
