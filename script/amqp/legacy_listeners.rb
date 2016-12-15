36d29cc0c50edce35b2a28f5920acbd5cf489e44
Trey Evans <lewis.r.evans@gmail.com>
2016-12-15 11:00:00 -0500

    Re-enable person listener.

--- script/amqp/legacy_listeners.rb
+++ script/amqp/legacy_listeners.rb
@@ -1,7 +1,7 @@
 require "multi_forkr"
 Rails.application.eager_load!
 MultiForkr.new({
-#  Listeners::IndividualEventListener => 1,
+  Listeners::IndividualEventListener => 1,
   Listeners::EnrollmentCreator => 4,
   Listeners::PersonMatcher => 5
 }).run
