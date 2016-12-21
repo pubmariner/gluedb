79186a339d4529ec1ced1c41f4eeffa0fbd0e4aa
Trey Evans <lewis.r.evans@gmail.com>
2016-12-21 16:24:30 -0500

    Base code for carrier filtering.

--- app/models/employer_event.rb
+++ app/models/employer_event.rb
@@ -7,6 +7,8 @@ class EmployerEvent
   field :resource_body, type: String
   field :employer_id, type: String
 
+  XML_NS = "http://openhbx.org/api/terms/1.0"
+
   index({event_time: 1, event_name: 1, employer_id: 1})
 
   def self.newest_event?(new_employer_id, new_event_name, new_event_time)
@@ -26,4 +28,38 @@ class EmployerEvent
     end
   end
 
+  def remove_other_carrier_nodes(xml, carrier_hbx_id)
+    doc = Nokogiri::XML(xml)
+
+    data_for_carrier = doc.xpath("//cv:elected_plans/cv:elected_plan/cv:carrier/cv:id/cv:id[contains(., '#{carrier_hbx_id}')]", {:cv => XML_NS}).any?
+
+    doc.xpath("//cv:elected_plans/cv:elected_plan", {:cv => XML_NS}).each do |node|
+      carrier_id = node.at_xpath("cv:carrier/cv:id/cv:id", {:cv => XML_NS}).content
+      if carrier_id != carrier_hbx_id
+        node.remove
+      end
+    end
+    doc.xpath("//cv:employer_census_families", {:cv => XML_NS}).each do |node|
+      node.remove
+    end
+    doc.xpath("//cv:benefit_group/cv:reference_plan", {:cv => XML_NS}).each do |node|
+      node.remove
+    end
+    doc.xpath("//cv:benefit_group/cv:elected_plans[not(cv:elected_plan)]", {:cv => XML_NS}).each do |node|
+      node.remove
+    end
+    doc.xpath("//cv:brokers[not(cv:broker_account)]", {:cv => XML_NS}).each do |node|
+      node.remove
+    end
+    doc.xpath("//cv:benefit_group[not(cv:elected_plans)]", {:cv => XML_NS}).each do |node|
+      node.remove
+    end
+    doc.xpath("//cv:plan_year/cv:benefit_groups[not(cv:benefit_group)]", {:cv => XML_NS}).each do |node|
+      node.remove
+    end
+    doc.xpath("//cv:plan_year[not(cv:benefit_groups)]", {:cv => XML_NS}).each do |node|
+      node.remove
+    end
+    doc.to_xml(:save_with => Nokogiri::XML::Node::SaveOptions::NO_DECLARATION, :indent => 2)
+  end
 end
