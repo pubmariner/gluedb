9a3418e6ed144fdda1b00dd4e9291987cb18be5a
Matt Williams <m.a.dubya@gmail.com>
2016-12-29 08:57:45 -0600

    trying to solve Sprockets::FileNotFound: couldn't find file 'jquery-ui/button'

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
-//= require jquery-ui/button
+//= require jquery.ui.button
 //= require jquery-ui/datepicker
 //= require jquery-ui/slider
 //= require jquery-ui/spinner
