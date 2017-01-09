118638168175ba2479fa1e3b271b4d344c430e28
Trey Evans <lewis.r.evans@gmail.com>
2017-01-09 11:45:12 -0500

    Completed specs for reducer.

--- spec/business_processes/handlers/enrollment_event_reduce_handler_spec.rb
+++ spec/business_processes/handlers/enrollment_event_reduce_handler_spec.rb
@@ -24,9 +24,57 @@ describe Handlers::EnrollmentEventReduceHandler, "given:
     subject.call(notifications)
   end
 
-  it "updates the business process history" do
+  it "updates the business process history of the non-duplicate enrollment" do
     expect(non_duplicate_notification).to receive(:update_business_process_history).with("Handlers::EnrollmentEventReduceHandler")
     subject.call(notifications)
   end
 
+end
+
+describe Handlers::EnrollmentEventReduceHandler, "given:
+- 3 notifications
+- 2 of which should be bucketed together"  do
+  
+  let(:non_duplicate_notification) { instance_double(::ExternalEvents::EnrollmentEventNotification, :hash => 1, :drop_if_marked! => false, :bucket_id => 5) }
+  let(:same_bucket_notification_1) { instance_double(::ExternalEvents::EnrollmentEventNotification, :hash => 3, :drop_if_marked! => false, :bucket_id => 4) }
+  let(:same_bucket_notification_2) { instance_double(::ExternalEvents::EnrollmentEventNotification, :hash => 3, :drop_if_marked! => false, :bucket_id => 4) }
+  let(:notifications) { [same_bucket_notification_1, same_bucket_notification_2, non_duplicate_notification] }
+
+  let(:next_step) { double("The next step in the pipeline") }
+
+  subject { Handlers::EnrollmentEventReduceHandler.new(next_step) }
+
+  before :each do
+    allow(next_step).to receive(:call).with([non_duplicate_notification])
+    allow(next_step).to receive(:call).with([same_bucket_notification_1, same_bucket_notification_2])
+    allow(same_bucket_notification_1).to receive(:check_and_mark_duplication_against).with(same_bucket_notification_2)
+    allow(non_duplicate_notification).to receive(:update_business_process_history).with("Handlers::EnrollmentEventReduceHandler")
+    allow(same_bucket_notification_1).to receive(:update_business_process_history).with("Handlers::EnrollmentEventReduceHandler")
+    allow(same_bucket_notification_2).to receive(:update_business_process_history).with("Handlers::EnrollmentEventReduceHandler")
+  end
+
+  it "sends along a bucket with the single enrollment notification" do
+    expect(next_step).to receive(:call).with([non_duplicate_notification])
+    subject.call(notifications)
+  end
+
+  it "sends along a bucket with the same-bucket enrollment notifications" do
+    expect(next_step).to receive(:call).with([same_bucket_notification_1, same_bucket_notification_2])
+    subject.call(notifications)
+  end
+
+  it "updates the business process history of the single bucket enrollment" do
+    expect(non_duplicate_notification).to receive(:update_business_process_history).with("Handlers::EnrollmentEventReduceHandler")
+    subject.call(notifications)
+  end
+
+  it "updates the business process history of the first group bucket enrollment" do
+    expect(same_bucket_notification_1).to receive(:update_business_process_history).with("Handlers::EnrollmentEventReduceHandler")
+    subject.call(notifications)
+  end
+
+  it "updates the business process history of the second group bucket enrollment" do
+    expect(same_bucket_notification_2).to receive(:update_business_process_history).with("Handlers::EnrollmentEventReduceHandler")
+    subject.call(notifications)
+  end
 end
