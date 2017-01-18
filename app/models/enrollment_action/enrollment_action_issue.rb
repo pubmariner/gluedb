843a6db3d2a17d912d09a2184472d09555ef760b
Trey Evans <lewis.r.evans@gmail.com>
2017-01-18 12:07:54 -0500

    Data.

--- app/models/enrollment_action/enrollment_action_issue.rb
+++ app/models/enrollment_action/enrollment_action_issue.rb
@@ -2,6 +2,7 @@ module EnrollmentAction
   class EnrollmentActionIssue
     include Mongoid::Document
     include Mongoid::Timestamps
+    include AASM
 
     field :error_message, type: String
     field :enrollment_action_uri, type: String
@@ -9,5 +10,12 @@ module EnrollmentAction
     field :received_at, type: Time
     field :hbx_enrollment_vocabulary, type: String
     field :headers, type: Hash
+    field :aasm_state, type: String
+
+    aasm do
+      state :new, initial: true
+      state :resolved
+      state :in_progress
+    end
   end
 end
