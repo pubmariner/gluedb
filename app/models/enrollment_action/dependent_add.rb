9c70d8ba38ef3cee9e1ead151f1cc4ceb774ce23
Trey Evans <lewis.r.evans@gmail.com>
2017-01-18 13:17:07 -0500

    Work on dependent add.

--- app/models/enrollment_action/dependent_add.rb
+++ app/models/enrollment_action/dependent_add.rb
@@ -12,13 +12,36 @@ module EnrollmentAction
       action.all_member_ids - termination.all_member_ids
     end
 
+    # TODO: Create new members, add to policy
+    def persist
+      members = action.policy_cv.enrollees.map(&:member)
+      members_persisted = members.map do |mem|
+        em = ExternalEvents::ExternalMember.new(mem)
+        em.persist
+      end
+      unless members_persisted.all?
+        return false
+      end
+      # Add new hbx_enrollment_id to policy
+      policy_to_change = term.existing_policy
+      policy_to_change.hbx_enrollment_ids << action.hbx_enrollment_id
+      policy_to_change.save
+    end
+
     def publish
       policy_to_change = term.existing_policy
+      member_date_map = {}
+      policy_to_change.enrollees.each do |en|
+        member_date_map[en.m_id] = en.coverage_start
+      end
       change_publish_helper = EnrollmentAction::ActionPublishHandler.new(action.event_xml)
       change_publish_helper.set_policy_id(policy_to_change.eg_id)
       change_publish_helper.filter_affected_members(added_dependents)
       change_publish_helper.set_event_action("urn:openhbx:terms:v1:enrollment#change_member_add")
+      change_publish_helper.set_member_starts(member_date_map)
       change_publish_helper.to_xml
+      amqp_connection = termination.event_responder.connection
+      publish_edi(amqp_connection, action_helper.to_xml, termination.hbx_enrollment_id, termination.employer_hbx_id)
     end
   end
 end
