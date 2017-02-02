dad034f6a6e448397ed187da9118c471213d2693
Trey Evans <lewis.r.evans@gmail.com>
2017-02-02 10:25:29 -0500

    Working on dependent drops.

--- app/models/enrollment_action/dependent_drop.rb
+++ app/models/enrollment_action/dependent_drop.rb
@@ -13,6 +13,8 @@ module EnrollmentAction
       policy_to_change = termination.existing_policy
       policy_to_change.hbx_enrollment_ids << action.hbx_enrollment_id
       policy_to_change.save
+      pol_updater = ExternalEvents::ExternalPolicyMemberDrop.new(policy_to_change, action.policy_cv, dropped_dependents)
+      pol_updater.persist
     end
 
     def dropped_dependents
@@ -32,6 +34,7 @@ module EnrollmentAction
       termination_helper.set_member_starts(member_date_map)
       termination_helper.filter_affected_members(dropped_dependents)
       termination_helper.keep_member_ends(dropped_dependents)
+      # TODO: Fix money amounts - we need to pull the new money totals into the old XML
       publish_edi(amqp_connection, termination_helper.to_xml, existing_policy.eg_id, termination.employer_hbx_id)
     end
   end
