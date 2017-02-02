f0ca5d9dd115979c004b2843231749c4403f8066
Trey Evans <lewis.r.evans@gmail.com>
2017-02-02 10:42:41 -0500

    Correct TIN vs HBX ID loading.

--- app/models/parsers/edi/etf/person_loop.rb
+++ app/models/parsers/edi/etf/person_loop.rb
@@ -135,8 +135,15 @@ module Parsers
         end
 
         def ssn
+          qualifier = @loop["L2100A"]["NM1"][8]
+          return nil if qualifier.blank?
           result = @loop["L2100A"]["NM1"][9]
-          return nil if result.blank? || result.length < 9
+          return nil if result.blank?
+          if qualifier.strip == "34"
+            return nil if (result.length != 9)
+          else
+            return nil if (result == member_id)
+          end
           result
         end
 
