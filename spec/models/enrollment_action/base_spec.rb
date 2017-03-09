3b4f5178c95284166539be86aa6fcb68e1b0faca
Trey Evans <lewis.r.evans@gmail.com>
2017-03-09 15:38:35 -0500

    Specs for Initial Enrollment Action.
    Pair programming demonstration of bare minimum spec for the initial
    enrollment EnrollmentAction sub-class.

---
+++ spec/models/enrollment_action/base_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+describe EnrollmentAction::Base do
+  describe "#publish_edi" do
+    it "NEEDS TESTS!"
+  end
+end
---
+++ spec/models/enrollment_action/renewal_comparison_helper_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+describe EnrollmentAction::RenewalComparisonHelper do
+  describe "#any_renewal_candidates?" do
+    it "needs tests!"
+  end
+end
