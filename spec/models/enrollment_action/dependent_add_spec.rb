29566ecddc9bad9cc80d033df5198179b87c4d3d
Brian Weiner <brian.weiner@dc.gov>
2017-03-10 10:25:41 -0500

    Persist spec for DependentAdd -- Ref#14512

--- spec/models/enrollment_action/dependent_add_spec.rb
+++ spec/models/enrollment_action/dependent_add_spec.rb
@@ -21,3 +21,55 @@ describe EnrollmentAction::DependentAdd, "given an enrollment event set that:
     expect(subject.qualifies?(event_set)).to be_truthy
   end
 end
+
+describe EnrollmentAction::DependentAdd, "given a qualified enrollment set, being persisted" do
+  let(:member_primary) { instance_double(Openhbx::Cv2::EnrolleeMember, id: 1) }
+  let(:member_secondary) { instance_double(Openhbx::Cv2::EnrolleeMember, id: 2) }
+  let(:member_new) { instance_double(Openhbx::Cv2::EnrolleeMember, id: 3) }
+  let(:enrollee_primary) { instance_double(::Openhbx::Cv2::Enrollee, :member => member_primary) }
+  let(:enrollee_secondary) { instance_double(::Openhbx::Cv2::Enrollee, :member => member_secondary) }
+  let(:enrollee_new) { instance_double(::Openhbx::Cv2::Enrollee, :member => member_new) }
+
+  let(:terminated_policy_cv) { instance_double(Openhbx::Cv2::Policy, :enrollees => [enrollee_primary, enrollee_secondary])}
+  let(:new_policy_cv) { instance_double(Openhbx::Cv2::Policy, :enrollees => [enrollee_primary, enrollee_secondary, enrollee_new]) }
+  let(:plan) { instance_double(Plan, :id => 1) }
+  let(:policy) { instance_double(Policy, :hbx_enrollment_ids => [1,2]) }
+  let(:primary_db_record) { instance_double(ExternalEvents::ExternalMember, :persist => true) }
+  let(:secondary_db_record) { instance_double(ExternalEvents::ExternalMember, :persist => true) }
+  let(:new_db_record) { instance_double(ExternalEvents::ExternalMember, :persist => true) }
+
+  let(:dependent_add_event) { instance_double(
+    ::ExternalEvents::EnrollmentEventNotification,
+    :policy_cv => new_policy_cv,
+    :existing_plan => plan,
+    :all_member_ids => [1,2,3],
+    :hbx_enrollment_id => [3]
+    ) }
+  let(:termination_event) { instance_double(
+    ::ExternalEvents::EnrollmentEventNotification,
+    :policy_cv => terminated_policy_cv,
+    :existing_policy => policy,
+    :all_member_ids => [1,2]
+    ) }
+
+  let(:policy_updater) { instance_double(ExternalEvents::ExternalPolicyMemberAdd) }
+
+
+  subject do
+    EnrollmentAction::DependentAdd.new(termination_event, dependent_add_event)
+  end
+
+  before :each do
+    allow(ExternalEvents::ExternalMember).to receive(:new).with(member_primary).and_return(primary_db_record)
+    allow(ExternalEvents::ExternalMember).to receive(:new).with(member_secondary).and_return(secondary_db_record)
+    allow(ExternalEvents::ExternalMember).to receive(:new).with(member_new).and_return(new_db_record)
+
+    allow(policy).to receive(:save!).and_return(true)
+    allow(ExternalEvents::ExternalPolicyMemberAdd).to receive(:new).with(policy, new_policy_cv, [3]).and_return(policy_updater)
+    allow(policy_updater).to receive(:persist).and_return(true)
+  end
+
+  it "successfully creates the new policy" do
+    expect(subject.persist).to be_truthy
+  end
+end
