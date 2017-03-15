33189a02f93c082078b224c9fccad200214d5882
Trey Evans <lewis.r.evans@gmail.com>
2017-03-15 11:31:40 -0400

    Base specs for enrollment action.

--- spec/models/enrollment_action/base_spec.rb
+++ spec/models/enrollment_action/base_spec.rb
@@ -2,6 +2,22 @@ require "rails_helper"
 
 describe EnrollmentAction::Base do
   describe "#publish_edi" do
-    it "NEEDS TESTS!"
+    subject { EnrollmentAction::Base.new(nil, nil) }
+
+    describe "which fails to publish trading partner edi" do
+      it "returns false with the publishing errors"
+
+      it "does not attempt to publish the legacy cv"
+    end
+
+    describe "which publishes trading partner edi" do
+      describe "but fails to publish the legacy cv" do
+        it "returns false with the publishing errors"
+      end
+
+      describe "and to publishes the legacy cv" do
+        it "returns true"
+      end
+    end
   end
 end
