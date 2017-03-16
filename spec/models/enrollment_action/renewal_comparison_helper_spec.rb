3dbe46fd19b16beecbe1d57fcf4d807b915881bf
Brian Weiner <brian.weiner@dc.gov>
2017-03-16 12:33:57 -0400

    ivl_renewal_candidate? specs

--- spec/models/enrollment_action/renewal_comparison_helper_spec.rb
+++ spec/models/enrollment_action/renewal_comparison_helper_spec.rb
@@ -29,9 +29,86 @@ describe EnrollmentAction::RenewalComparisonHelper do
 
   describe "ivl_renewal_candidate?" do
     ## receives a policy, plan, subscriber_id, subscriber_start date, and a boolean if same carrier
+    let(:subscriber) { double(:m_id => 1)}
+    let(:subscriber_id) { 1 }
+    let(:is_shop?) { false }
+    let(:canceled?) { false }
+    let(:terminated?) { false }
+    let(:same_carrier) { true }
+    let(:old_plan) { instance_double(Plan, :year => 2016, :carrier_id => 1, :coverage_type => :some_type)}
+    let(:new_plan) { instance_double(Plan, :year => 2017, :carrier_id => 1, :coverage_type => :some_type) }
+    let(:policy) { instance_double(Policy,
+                    :is_shop? => is_shop?,
+                    :subscriber => subscriber,
+                    :plan => old_plan,
+                    :canceled? => canceled?,
+                    :terminated? => terminated?,
+                    :coverage_period => old_coverage_period )
+                  }
+    let(:subscriber_start) { DateTime.new(2017,1,1) }
+    let(:old_coverage_period) { double(:end => subscriber_start - 1.day) }
+
+    it "returns true" do
+      expect(subject.ivl_renewal_candidate?(policy, new_plan, subscriber_id, subscriber_start, same_carrier)).to be_truthy
+    end
+
+    context "policy is for SHOP" do
+      let(:is_shop?) { true }
+      it "returns false" do
+        expect(subject.ivl_renewal_candidate?(policy, new_plan, subscriber_id, subscriber_start, same_carrier)).to be_falsey
+      end
+    end
+    context "policy subscriber mismatch" do
+      let(:subscriber_id) { 2 }
+      it "returns false" do
+        expect(subject.ivl_renewal_candidate?(policy, new_plan, subscriber_id, subscriber_start, same_carrier)).to be_falsey
+      end
+    end
+    context "plan year mismatch" do
+      let(:old_plan) { instance_double(Plan, :year => 2015, :carrier_id => 1, :coverage_type => :some_type)}
+      it "returns false" do
+        expect(subject.ivl_renewal_candidate?(policy, new_plan, subscriber_id, subscriber_start, same_carrier)).to be_falsey
+      end
+    end
+    context "coverage type mismatch" do
+      let(:old_plan) { instance_double(Plan, :year => 2016, :carrier_id => 1, :coverage_type => :different_type)}
+      it "returns false" do
+        expect(subject.ivl_renewal_candidate?(policy, new_plan, subscriber_id, subscriber_start, same_carrier)).to be_falsey
+      end
+    end
+    context "carrier change but same_carrier is true" do
+      let(:old_plan) { instance_double(Plan, :year => 2016, :carrier_id => 2, :coverage_type => :some_type)}
+      it "returns false" do
+        expect(subject.ivl_renewal_candidate?(policy, new_plan, subscriber_id, subscriber_start, same_carrier)).to be_falsey
+      end
+    end
+    context "carrier unchanged but same_carrier is false" do
+      let(:same_carrier) { false }
+      it "returns false" do
+        expect(subject.ivl_renewal_candidate?(policy, new_plan, subscriber_id, subscriber_start, same_carrier)).to be_falsey
+      end
+    end
+    context "policy cancelled" do
+      let(:canceled?) { true }
+      it "returns false" do
+        expect(subject.ivl_renewal_candidate?(policy, new_plan, subscriber_id, subscriber_start, same_carrier)).to be_falsey
+      end
+    end
+    context "policy terminated" do
+      let(:terminated?) { true }
+      it "returns false" do
+        expect(subject.ivl_renewal_candidate?(policy, new_plan, subscriber_id, subscriber_start, same_carrier)).to be_falsey
+      end
+    end
+    context "start date and end date gap" do
+      let(:old_coverage_period) { double(:end => subscriber_start - 2.day) }
+      it "returns false" do
+        expect(subject.ivl_renewal_candidate?(policy, new_plan, subscriber_id, subscriber_start, same_carrier)).to be_falsey
+      end
+    end
   end
   describe "shop_renewal_candidate?" do
-    ## receives a policy, plan, subscriber_id, subscriber_start date, and a boolean if same carrier
+    ## receives a policy, plan, employer, subscriber_id, subscriber_start, and a boolean if same carrier
     let(:subscriber) { double(:m_id => 1)}
     let(:employer) { double(id: 1) }
     let(:policy_employer_id) { 1 }
