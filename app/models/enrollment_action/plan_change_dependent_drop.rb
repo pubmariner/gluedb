af87071f471cece2469c09b8c000838065fe7c57
Brian Weiner <brian.weiner@dc.gov>
2017-03-20 16:50:49 -0400

    plan change dependent drop sets the correct qualifying life event

--- app/models/enrollment_action/plan_change_dependent_drop.rb
+++ app/models/enrollment_action/plan_change_dependent_drop.rb
@@ -40,6 +40,7 @@ module EnrollmentAction
       termination_helper.set_member_starts(member_date_map)
       termination_helper.filter_affected_members(dropped_dependents)
       termination_helper.keep_member_ends(dropped_dependents)
+      termination_helper.swap_qualifying_event(action.event_xml)
       publish_edi(amqp_connection, termination_helper.to_xml, existing_policy.eg_id, termination.employer_hbx_id)
       publish_edi(amqp_connection, termination_helper.to_xml, existing_policy.eg_id, termination.employer_hbx_id)
       action_helper = EnrollmentAction::ActionPublishHelper.new(action.event_xml)
