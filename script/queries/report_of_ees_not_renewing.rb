e342fe98c248104b08f7f5b270fec2a3435fc261
Ji Wang <traceyw@vt.edu>
2017-04-03 14:46:58 -0400

    Ref#14376 Need to finish query

---
+++ script/queries/report_of_ees_not_renewing.rb
@@ -0,0 +1,106 @@
+require 'csv'
+
+field_names= %w(
+                Subscriber_name,
+                Subscriber_policy_id,
+                Subscriber_enrollment_start,
+                Subscriber_enrollment_end,
+                Employer_legal_name,
+                Employer_next_plan_year_start,
+                Employer_next_plan_year_end
+               )
+
+
+CSV.open('report_of_ees_not_renewing.csv', 'w') do |csv|
+  csv << field_names
+
+end
+
+## Loop through employers
+new_plan_year_start_date = (Date.today.end_of_month) + 1.day
+
+def find_current_plan_year(employer)
+  check_date = Date.today
+  correct_plan_year = employer.plan_years.where(:start_date => {"$lte" => check_date}, :end_date => {"$gte" => check_date})
+  return correct_plan_year
+end
+
+def find_renewal_plan_year(employer)
+  check_date = (Date.today.end_of_month)+1.day
+  correct_plan_year employer.plan_years.where(:start_date => check_date).first
+  return correct_plan_year
+end
+
+Employer.each do |employer|
+  current_plan_year = find_current_plan_year(employer)
+  renewal_plan_year = find_renewal_plan_year(employer)
+  if renewal_plan_year.present?
+    employer_policies = Policy.where(:employer_id => employer._id, :enrollees => {"$elemMatch" => {
+                                                                                    :rel_code => "self",
+                                                                                    :coverage_start => {"$gte" => current_plan_year.start_on,
+                                                                                                        "$lte" => current_plan_year.end_on},
+                                                                                    :coverage_end => nil
+    }})
+    people=employer_policies.map(&:subscriber).map(&:person).uniq
+    people.each do |person|
+      #check whether the person has non_terminated policy in current plan year
+      #check whether the person has non_active policy in the next plan year
+      #out put the person
+      non_terminated_current_health_policy=
+      active_renew_health_policy=
+
+      non_terminated_current_dental_policy=
+      active_renew_dental_policy=
+
+      if non_terminated_current_dental_policy && active_renew_dental_policy.nil? &&non_terminated_current_health_policy && active_renew_health_policy.nil?
+        csv << [
+            person.full_name
+            non_terminated_current_dental_policy.eg_id
+            non_terminated_current_dental_policy.policy_start
+            non_terminated_current_dental_policy.policy_end
+            employer.name
+            employer.plan_year.start_on
+            employer.plan_year.end_on
+        ]
+        csv << [
+            person.full_name
+        non_terminated_current_health_policy.eg_id
+        non_terminated_current_health_policy.policy_start
+        non_terminated_current_health_policy.policy_end
+        employer.name
+        employer.plan_year.start_on
+        employer.plan_year.end_on
+        ]
+      elsif non_terminated_current_health_policy && active_renew_health_policy.nil?
+        csv << [
+            person.full_name
+           non_terminated_current_health_policy.eg_id
+           non_terminated_current_health_policy.policy_start
+           non_terminated_current_health_policy.policy_end
+           employer.name
+          employer.plan_year.start_on
+          employer.plan_year.end_on
+        ]
+      elsif  non_terminated_current_dental_policy && active_renew_dental_policy.nil?
+        csv << [
+            person.full_name
+        non_terminated_current_dental_policy.eg_id
+        non_terminated_current_dental_policy.policy_start
+        non_terminated_current_dental_policy.policy_end
+        employer.name
+        employer.plan_year.start_on
+        employer.plan_year.end_on
+        ]
+
+      end
+
+    end
+
+    end
+  end
+end
+
+#get the employer with certain plan year
+#get the policy using the employer_id and the start date
+
+
