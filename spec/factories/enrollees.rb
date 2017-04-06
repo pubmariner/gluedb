4e1d505cd94233f2a4906ab362c8cd8133f267a2
Zoheb Nensey <zoheb.nensey@gmail.com>
2017-04-06 15:46:55 -0400

    Refs #7860 add extra types of enrollees to spec factory

--- spec/factories/enrollees.rb
+++ spec/factories/enrollees.rb
@@ -16,6 +16,19 @@ FactoryGirl.define do
       rel_code 'self'
     end
 
+    trait :spouse do
+      rel_code 'spouse'
+    end
+
+    trait :child do
+      rel_code 'child'
+    end
+
+    trait :ward do
+      rel_code 'ward'
+    end
+
     factory :subscriber_enrollee, traits: [:self_relationship]
+    factory :spouse_enrollee, traits: [:spouse]
   end
 end
