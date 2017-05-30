7f105ff5c64e479d89e3652aa765c390fce2ace0
Zoheb Nensey <zoheb.nensey@gmail.com>
2017-05-30 11:59:23 -0400

    Refs #16168 Update query for glue enrollment group ids

--- script/queries/print_all_policy_ids.rb
+++ script/queries/print_all_policy_ids.rb
@@ -1,7 +1,24 @@
-pols = Policy.collection.raw_aggregate([{
+pols_1 = Policy.collection.raw_aggregate([{
   "$group" => {"_id" => "$eg_id"}
 }])
 
+pols_2 = Policy.collection.aggregate([{"$group" => {"_id" => "$hbx_enrollment_ids"}}])
+
+pols = []
+
+pols_1.each do |pol|
+  pols << pol["_id"]
+end
+
+pols_2.each do |pol_ids|
+  next if pol_ids["_id"].blank?
+  pol_ids["_id"].each do |id|
+    pols << id
+  end
+end
+
+pols.compact.uniq!
+
 pols.each do |pol|
-  puts pol["_id"]
+  puts pol
 end
