1fc136f810794f272d88652cc12d9203fa71354a
Zoheb Nensey <zoheb.nensey@gmail.com>
2017-06-14 15:34:22 -0400

    Refs #15924 add code to remove plans and carrieres

--- app/data_migrations/remove_carrier.rb
+++ app/data_migrations/remove_carrier.rb
@@ -3,9 +3,42 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 
 class RemoveCarrier < MongoidMigrationTask
   def remove_carrier
+    carrier_size = Carrier.where(abbrev: ENV['abbrev']).size
+    if carrier_size > 1
+      puts "Multiple carriers found with this abbreviation" unless Rails.env.test?
+      return
+    elsif carrier_size == 0 
+      puts "Carrier not found with this abbreviation." unless Rails.env.test?
+    elsif carrier_size == 1
+      carrier = Carrier.where(abbrev: ENV['abbrev']).first
+      carrier_name = carrier.name
+      if carrier.carrier_profiles.size > 0
+        carrier.carrier_profiles.each do |cp|
+          cp.destroy
+        end
+      end
+      carrier.destroy
+      puts "Removed #{carrier_name} with abbreviation #{ENV['abbrev']}" unless Rails.env.test?
+    end
   end
 
   def remove_plans
+    carrier_size = Carrier.where(abbrev: ENV['abbrev']).size
+    if carrier_size > 1
+      puts "Multiple carriers found with this abbreviation" unless Rails.env.test?
+      return
+    elsif carrier_size == 0 
+      puts "Carrier not found with this abbreviation." unless Rails.env.test?
+    elsif carrier_size == 1
+      carrier = Carrier.where(abbrev: ENV['abbrev']).first
+      carrier_name = carrier.name
+      plans = Plan.where(carrier_id: carrier._id)
+      plan_size = plans.size
+      plans.each do |plan|
+        plan.destroy
+      end
+      puts "Removed #{plan_size} plans for carrier #{carrier_name}" unless Rails.env.test?
+    end
   end
 
   def migrate
