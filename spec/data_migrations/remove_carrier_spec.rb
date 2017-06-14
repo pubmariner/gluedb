ebe1144815de13fd8db1dc56eb56d60cb2d52c44
Zoheb Nensey <zoheb.nensey@gmail.com>
2017-06-14 13:02:27 -0400

    Refs #15924 initial version of specs

---
+++ spec/data_migrations/remove_carrier_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+require File.join(Rails.root,"app","data_migrations","remove_carrier")
+
+describe RemoveCarrier, dbclean: :after_each do
+  let(:given_task_name) { "remove_carrier" }
+  let(:carrier) { FactoryGirl.create(:carrier) }
+  let!(:plan) { FactoryGirl.create(:plan, carrier: carrier)}
+  subject { RemoveCarrier.new(given_task_name, double(:current_scope => nil)) }
+
+  describe "given a task name" do
+    it "has the given task name" do
+      expect(subject.name).to eql given_task_name
+    end
+  end
+
+  describe "deleting plans" do
+    before { subject.remove_plans }
+
+    it "should remove the plans" do
+      expect(Plan.find(plan.id)).to be_nil
+    end
+  end
+
+  describe "deleting carrier" do 
+    before(:each) do
+      allow(ENV).to receive(:[]).with("abbrev").and_return(carrier.abbrev)
+    end
+
+    it "should remove the carrier" do
+      abbrev = carrier.abbrev
+      expect(carrier.abbrev).to eq abbrev
+      subject.remove_carrier
+      expect(Carrier.where(abbrev: abbrev).size).to eq 0
+    end
+  end
+end
