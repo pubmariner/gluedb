f7ebdca3cee7fbea1ac7545b31620f7309ace671
Zoheb Nensey <zoheb.nensey@gmail.com>
2017-06-19 17:07:00 -0400

    Refs #16366 add data migration code

---
+++ app/data_migrations/remove_policy_end_date.rb
@@ -0,0 +1,25 @@
+# Removes end dates from policies 
+require File.join(Rails.root, "lib/mongoid_migration_task")
+
+class RemovePolicyEndDate < MongoidMigrationTask
+  def remove_end_dates
+    policy = Policy.where(eg_id: ENV['eg_id']).first
+    policy.enrollees.each do |enrollee|
+      enrollee.emp_stat = "active"
+      enrollee.coverage_status = "active"
+      enrollee.coverage_end = nil
+      enrollee.save!
+    end
+  end
+
+  def change_aasm_state
+    policy = Policy.where(eg_id: ENV['eg_id']).first
+    policy.aasm_state = ENV['aasm_state']
+    policy.save!
+  end
+
+  def migrate
+    remove_end_dates
+    change_aasm_state
+  end
+end
