c5f43a8672f0bb1265fd15b19ebf2fd203655d7b
Zoheb Nensey <zoheb.nensey@gmail.com>
2017-07-21 13:27:19 -0400

    Refs #17266 add rake task + specs for end date changes

---
+++ app/data_migrations/change_policy_end_date.rb
@@ -0,0 +1,30 @@
+# Changes/Addes end dates to policies
+require File.join(Rails.root, "lib/mongoid_migration_task")
+
+class ChangePolicyEndDate < MongoidMigrationTask
+  def change_end_date
+    policy = Policy.where(eg_id: ENV['eg_id']).first
+    policy.enrollees.each do |enrollee|
+      enrollee.emp_stat = "terminated"
+      enrollee.coverage_status = "inactive"
+      enrollee.coverage_end = ENV['end_date'].to_date
+      enrollee.save!
+    end
+  end
+
+  def change_aasm_state
+    policy = Policy.where(eg_id: ENV['eg_id']).first
+    if policy.policy_start == ENV['end_date'].to_date
+      policy.aasm_state = 'canceled'
+    elsif policy.policy_start != ENV['end_date'].to_date
+      policy.aasm_state = 'terminated'
+    end
+    policy.save!
+  end
+
+  def migrate
+    change_end_date
+    change_aasm_state
+    puts "Changed end date for policy #{ENV['eg_id']} to #{ENV['end_date']}" unless Rails.env.test?
+  end
+end
