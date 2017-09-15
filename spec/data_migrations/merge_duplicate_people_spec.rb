eb7d0d0f9af6436111ade77ae002d39e3eb91135
Zoheb Nensey <zoheb.nensey@gmail.com>
2017-09-15 12:26:14 -0400

    Refs #10528 update specs for different person access method

--- spec/data_migrations/merge_duplicate_people_spec.rb
+++ spec/data_migrations/merge_duplicate_people_spec.rb
@@ -16,8 +16,8 @@ describe MergeDuplicatePeople, dbclean: :after_each do
   describe 'merge the people' do 
 
     before(:each) do 
-      allow(ENV).to receive(:[]).with("person_to_keep").and_return(person_to_keep.authority_member_id)
-      allow(ENV).to receive(:[]).with("person_to_remove").and_return(person_to_remove.authority_member_id)
+      allow(ENV).to receive(:[]).with("person_to_keep").and_return(person_to_keep._id)
+      allow(ENV).to receive(:[]).with("person_to_remove").and_return(person_to_remove._id)
     end
 
     it 'should merge any non-duplicate addresses' do
@@ -47,11 +47,9 @@ describe MergeDuplicatePeople, dbclean: :after_each do
     it 'should move all the members from the duplicate person' do
       total_members_before = person_to_keep.members.size + person_to_remove.members.size
       member_ids_before = (person_to_keep.members.map(&:hbx_member_id) + person_to_remove.members.map(&:hbx_member_id)).uniq.sort
-      subject.move_members(ENV['person_to_keep'],ENV['person_to_remove'])
+      subject.move_and_delete_members(ENV['person_to_keep'],ENV['person_to_remove'])
       person_to_keep.reload
       person_to_remove.reload
-      puts "keep: #{person_to_keep.members.map(&:hbx_member_id)}"
-      puts "remove: #{person_to_remove.members.map(&:hbx_member_id)}"
       expect(person_to_keep.members.size).to eq total_members_before
       expect(person_to_remove.members.size).to eq 0
       expect(person_to_keep.members.map(&:hbx_member_id)).to eq member_ids_before
@@ -59,7 +57,7 @@ describe MergeDuplicatePeople, dbclean: :after_each do
     end
 
     it 'should unset the authority member id from the duplicate person' do
-      subject.unset_authority_member_id(ENV['person_to_keep'],ENV['person_to_remove'])
+      subject.unset_authority_member_id(ENV['person_to_remove'])
       person_to_remove.reload
       expect(person_to_remove.authority_member_id).to eq nil 
     end
