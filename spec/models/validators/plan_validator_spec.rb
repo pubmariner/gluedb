9b581a2c3092f75070262702c63fa56f92c6643a
Zoheb Nensey <zoheb.nensey@gmail.com>
2017-11-01 11:45:08 -0400

    Refs #18227 add specs for new plan validator

---
+++ spec/models/validators/plan_validator_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe Validators::PlanValidator do 
+  subject(:validator) { Validators::PlanValidator.new(change_request,plan,listener) }
+
+  let(:change_request) { double }
+  let(:plan) { double }
+  let(:listener) { double }
+
+  context 'when the plan is found' do 
+    before do
+      allow(plan).to receive(:blank?).and_return(false)
+    end
+    it 'does not notify the listener' do
+      expect(listener).not_to receive(:plan_not_found)
+      expect(validator.validate).to eq true
+    end
+  end
+
+  context 'when the plan is not found' do 
+    before do
+      allow(plan).to receive(:blank?).and_return(true)
+    end
+    it 'notifies the listener' do
+      expect(listener).to receive(:plan_not_found)
+      expect(validator.validate).to eq false
+    end
+  end
+
+end
