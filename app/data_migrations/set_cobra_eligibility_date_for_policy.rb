6948801ec35c17783f0f2665134a92e75bc72ac4
Nikhil S <nikhilks219@gmail.com>
2017-11-14 13:14:14 -0500

    19397: added data migration

---
+++ app/data_migrations/set_cobra_eligibility_date_for_policy.rb
@@ -0,0 +1,33 @@
+require File.join(Rails.root, "lib/mongoid_migration_task")
+
+class SetCobraEligibilityDateForPolicy < MongoidMigrationTask
+
+  def migrate
+    
+    policy_count = Policy.all_active_states.count
+    offset_count = 0
+    limit_count = 500
+    processed_count = 0
+
+    while (offset_count <= policy_count) do
+      puts "offset_count: #{offset_count}"
+      Policy.all_active_states.limit(limit_count).offset(offset_count).each do |policy|
+        begin
+          if policy.is_cobra?
+            policy.cobra_eligibility_date = policy.subscriber.coverage_start
+            if policy.save!
+              puts "policy: #{policy.eg_id} updated with cobra eligibility date" unless Rails.env.test?
+              processed_count += 1
+            else
+              puts "unable to save policy: #{policy.eg_id}" unless Rails.env.test?
+            end
+          end
+        rescue => e
+          puts "Policy #{policy.eg_id}" + e.message
+        end
+      end
+      offset_count += limit_count
+    end
+    puts "Total policy processed_count #{processed_count}"
+  end
+end
