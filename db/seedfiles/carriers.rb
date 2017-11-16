bdccb80ab7bf372e4a7ef0e10f9005f4d6eddc9a
Zoheb Nensey <zoheb.nensey@gmail.com>
2017-11-16 08:51:07 -0500

    Refs #19795 Begin updating carrier factorygirl

--- db/seedfiles/carriers.rb
+++ db/seedfiles/carriers.rb
@@ -1,7 +1,11 @@
-CARRIERS = YAML.load_file(Rails.root.join('db', 'seedfiles', 'carriers.yml'))
-puts "Loading: Carriers"
+unless Rails.env.development?
+  CARRIERS = YAML.load_file(Rails.root.join('db', 'seedfiles', 'carriers.yml'))
+  puts "Loading: Carriers"
 
-#Carrier.collection.drop
-CARRIERS.each { |c| Carrier.create!(c) }
+  #Carrier.collection.drop
+  CARRIERS.each { |c| Carrier.create!(c) }
 
-puts "Load complete: Carriers"
+  puts "Load complete: Carriers"
+else
+
+end
--- spec/factories/carriers.rb
+++ spec/factories/carriers.rb
@@ -2,5 +2,66 @@ FactoryGirl.define do
   factory :carrier do
     name 'Super Awesome Carrier'
     sequence(:hbx_carrier_id) { |n| "#{n}" }
+
+    trait :health_and_dental do 
+      name 'Health and Dental Carrier'
+      abbrev 'HDC'
+      ind_hlt true
+      ind_dtl true
+      shp_hlt true
+      shp_dtl true
+    end
+
+    trait :health_only do
+      name 'Health Carrier'
+      abbrev 'DCHC'
+      ind_hlt true
+      shp_hlt true
+    end
+
+    trait :dental_only do 
+      name 'Dental Carrier'
+      abbrev 'DCDC'
+      ind_dtl true
+      shp_dtl true
+    end
+
+    trait :shop_health do 
+      name 'SHOP Health Carrier'
+      abbrev 'SHC'
+      shp_hlt true
+    end
+
+    trait :shop_dental do 
+      name 'SHOP Dental Carrier'
+      abbrev 'SDC'
+      shp_dtl true
+    end
+
+    trait :shop_health_and_dental do 
+      name 'SHOP Health and Dental Carrier'
+      abbrev 'SHDC'
+      shp_hlt true
+      shp_dtl true
+    end
+
+    trait :ivl_health do 
+      name 'IVL Health Carrier'
+      abbrev 'IHC'
+      ind_hlt true
+    end
+
+    trait :ivl_dental do 
+      name 'IVL Dental Carrier'
+      abbrev 'IDC'
+      ind_dtl true
+    end
+
+    trait :ivl_health_and_dental do 
+      name 'IVL Health and Dental Carrier'
+      abbrev 'IHDC'
+      ind_hlt true
+      ind_dtl true
+    end
   end
 end
