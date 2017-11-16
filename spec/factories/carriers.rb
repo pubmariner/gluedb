7218023ac98f67ee8bf9295b44e1656ca83a58c2
Zoheb Nensey <zoheb.nensey@gmail.com>
2017-11-16 12:03:42 -0500

    Refs #19795 create plan factories

--- spec/factories/carriers.rb
+++ spec/factories/carriers.rb
@@ -10,6 +10,14 @@ FactoryGirl.define do
       ind_dtl true
       shp_hlt true
       shp_dtl true
+
+      after(:create) do |carrier, evaluator|
+        create_list(:plan, 1, carrier: carrier)
+        create_list(:ivl_dental_plan, 1, carrier: carrier)
+        create_list(:ivl_assisted_plan, 1, carrier: carrier)
+        create_list(:shop_health_plan, 1, carrier: carrier)
+        create_list(:shop_dental_plan, 1, carrier: carrier)
+      end
     end
 
     trait :health_only do
@@ -17,6 +25,12 @@ FactoryGirl.define do
       abbrev 'DCHC'
       ind_hlt true
       shp_hlt true
+
+      after(:create) do |carrier, evaluator|
+        create_list(:plan, 1, carrier: carrier)
+        create_list(:ivl_assisted_plan, 1, carrier: carrier)
+        create_list(:shop_health_plan, 1, carrier: carrier)
+      end
     end
 
     trait :dental_only do 
@@ -24,18 +38,31 @@ FactoryGirl.define do
       abbrev 'DCDC'
       ind_dtl true
       shp_dtl true
+
+      after(:create) do |carrier, evaluator|
+        create_list(:ivl_dental_plan, 1, carrier: carrier)
+        create_list(:shop_dental_plan, 1, carrier: carrier)
+      end
     end
 
     trait :shop_health do 
       name 'SHOP Health Carrier'
       abbrev 'SHC'
       shp_hlt true
+
+      after(:create) do |carrier, evaluator|
+        create_list(:shop_health_plan, 1, carrier: carrier)
+      end
     end
 
     trait :shop_dental do 
       name 'SHOP Dental Carrier'
       abbrev 'SDC'
       shp_dtl true
+
+      after(:create) do |carrier, evaluator|
+        create_list(:shop_dental_plan, 1, carrier: carrier)
+      end
     end
 
     trait :shop_health_and_dental do 
@@ -43,18 +70,32 @@ FactoryGirl.define do
       abbrev 'SHDC'
       shp_hlt true
       shp_dtl true
+
+      after(:create) do |carrier, evaluator|
+        create_list(:shop_health_plan, 1, carrier: carrier)
+        create_list(:shop_dental_plan, 1, carrier: carrier)
+      end
     end
 
     trait :ivl_health do 
       name 'IVL Health Carrier'
       abbrev 'IHC'
       ind_hlt true
+
+      after(:create) do |carrier, evaluator|
+        create_list(:plan, 1, carrier: carrier)
+        create_list(:ivl_assisted_plan, 1, carrier: carrier)
+      end
     end
 
     trait :ivl_dental do 
       name 'IVL Dental Carrier'
       abbrev 'IDC'
       ind_dtl true
+
+      after(:create) do |carrier, evaluator|
+        create_list(:ivl_dental_plan, 1, carrier: carrier)
+      end
     end
 
     trait :ivl_health_and_dental do 
@@ -62,6 +103,12 @@ FactoryGirl.define do
       abbrev 'IHDC'
       ind_hlt true
       ind_dtl true
+
+      after(:create) do |carrier, evaluator|
+        create_list(:plan, 1, carrier: carrier)
+        create_list(:ivl_dental_plan, 1, carrier: carrier)
+        create_list(:ivl_assisted_plan, 1, carrier: carrier)
+      end
     end
 
     factory :health_and_dental_carrier, traits: [:health_and_dental]
--- spec/factories/plans.rb
+++ spec/factories/plans.rb
@@ -7,8 +7,66 @@ FactoryGirl.define do
     coverage_type 'health'
     metal_level 'bronze'
     market_type 'individual'
+    year Date.today.year
     ehb 0.0
     carrier
 
+    trait :shop_health do
+      name 'SHOP Health Plan'
+      abbrev 'SHPB'
+      hbx_plan_id '7472'
+      hios_plan_id '2747'
+      market_type 'shop'
+      carrier
+    end
+
+    trait :shop_dental do 
+      name 'SHOP Dental Plan'
+      abbrev 'SHDP'
+      hbx_plan_id '7437'
+      hios_plan_id '7347'
+      market_type 'shop'
+      metal_level 'dental'
+      coverage_type 'dental'
+      carrier
+    end
+
+    trait :ivl_assisted do 
+      name 'IVL Assisted Plan'
+      abbrev 'IVAP'
+      hbx_plan_id '4827'
+      hios_plan_id '7284'
+      metal_level 'silver'
+      carrier
+    end
+
+    trait :ivl_dental do 
+      name 'IVL Dental Plan'
+      abbrev 'IVDP'
+      hbx_plan_id '4837'
+      hios_plan_id '7384'
+      metal_level 'dental'
+      coverage_type 'dental'
+      carrier
+    end
+
+    trait :renewal_plan do
+        Plan.where(year: Date.today.year).each do |plan|
+          renewal_plan = plan.clone
+          renewal_plan.carrier = plan.carrier
+          renewal_plan.year = (Date.today + 1.year).year
+          renewal_plan.hios_plan_id = (plan.hios_plan_id.to_i + 1).to_s
+          renewal_plan.hbx_plan_id = (plan.hbx_plan_id.to_i + 1).to_s
+          renewal_plan.save
+          plan.renewal_plan = renewal_plan
+          plan.save
+        end
+    end
+
+    factory :shop_health_plan, traits: [:shop_health]
+    factory :shop_dental_plan, traits: [:shop_dental]
+    factory :ivl_assisted_plan, traits: [:ivl_assisted]
+    factory :ivl_dental_plan, traits: [:ivl_dental]
+    factory :renewal_plans, traits: [:renewal_plan]
   end
 end
