93b31a4d5066ab04f0891cef21c7bbac1b9b4e94
Zoheb Nensey <zoheb.nensey@gmail.com>
2017-11-22 10:55:10 -0500

    Refs #19795 add environment conditional for plan loading

--- db/seedfiles/plans.rb
+++ db/seedfiles/plans.rb
@@ -1,21 +1,25 @@
 require 'csv'
 
 puts "Loading: Plans"
-# Plan.collection.drop
 
-csv_loc = File.join(File.dirname(__FILE__), "plans.csv")
+unless Rails.env.development? || Rails.env.test?
+  # Plan.collection.drop
 
-carrier_hash = Hash.new do |h, k|
-  h[k] = Carrier.where(:hbx_carrier_id => k).first
+  csv_loc = File.join(File.dirname(__FILE__), "plans.csv")
+  
+  carrier_hash = Hash.new do |h, k|
+    h[k] = Carrier.where(:hbx_carrier_id => k).first
+  end
+  
+  plans = []
+  
+  CSV.foreach(File.open(csv_loc), headers: true) do |row|
+    record = row.to_hash.dup
+    carrier_h_id = record.delete("carrier_hbx_id")
+    record["carrier_id"] = carrier_hash[carrier_h_id]._id
+    plans << record
+  end
+  
+  Plan.create!(plans)
+
 end
-
-plans = []
-
-CSV.foreach(File.open(csv_loc), headers: true) do |row|
-  record = row.to_hash.dup
-  carrier_h_id = record.delete("carrier_hbx_id")
-  record["carrier_id"] = carrier_hash[carrier_h_id]._id
-  plans << record
-end
-
-Plan.create!(plans)
