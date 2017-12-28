9da9230bf0d9f87da947c800c2dde8fd74a2ca57
Trey Evans <lewis.r.evans@gmail.com>
2017-12-28 15:28:34 -0500

    Refs #20699. Fix termination status by callback.

--- app/models/external_events/external_policy_cobra_switch.rb
+++ app/models/external_events/external_policy_cobra_switch.rb
@@ -42,7 +42,6 @@ module ExternalEvents
         :tot_emp_res_amt => tot_emp_res_amt,
         :aasm_state => "submitted"
       })
-      @existing_policy.reload
       @existing_policy.hbx_enrollment_ids << extract_enrollment_group_id(@policy_node)
       @existing_policy.save!
     end
@@ -60,10 +59,10 @@ module ExternalEvents
     end
 
     def persist
-      update_policy_information
       @policy_node.enrollees.each do |en|
         update_enrollee(en)
       end
+      update_policy_information
       true
     end
   end
