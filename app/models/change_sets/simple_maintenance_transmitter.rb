fd4a283977b9f98f2573c1c0c7b552851e4ffc13
Trey Evans <lewis.r.evans@gmail.com>
2018-01-03 14:21:35 -0500

    Refs #20959.  Don't transmit terminated members.

--- app/models/change_sets/simple_maintenance_transmitter.rb
+++ app/models/change_sets/simple_maintenance_transmitter.rb
@@ -2,19 +2,21 @@ module ChangeSets
   module SimpleMaintenanceTransmitter
     def notify_policies(op, reason, member_id, policies_to_notify, new_reason)
       policies_to_notify.each do |pol|
-        af = ::BusinessProcesses::AffectedMember.new({
-          :policy => pol,
-          :member_id => member_id
-        })
-        ict = IdentityChangeTransmitter.new(af, pol, new_reason)
-        ict.publish
-        if pol.is_shop?
-          serializer = ::CanonicalVocabulary::MaintenanceSerializer.new(
-            pol, op, reason, [member_id], pol.active_member_ids
-          )
-          cv = serializer.serialize
-          pubber = ::Services::NfpPublisher.new
-          pubber.publish(true, "#{pol.eg_id}.xml", cv)
+        if pol.active_member_ids.include?(member_id)
+          af = ::BusinessProcesses::AffectedMember.new({
+            :policy => pol,
+            :member_id => member_id
+          })
+          ict = IdentityChangeTransmitter.new(af, pol, new_reason)
+          ict.publish
+          if pol.is_shop?
+            serializer = ::CanonicalVocabulary::MaintenanceSerializer.new(
+              pol, op, reason, [member_id], pol.active_member_ids
+            )
+            cv = serializer.serialize
+            pubber = ::Services::NfpPublisher.new
+            pubber.publish(true, "#{pol.eg_id}.xml", cv)
+          end
         end
       end
     end
