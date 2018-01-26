5c1f3d8325024b73c149b6ceb3b75195dfc3d32b
Trey Evans <lewis.r.evans@gmail.com>
2018-01-25 19:02:18 -0500

    Much faster now.

--- app/data_migrations/generate_audits_via_cv2.rb
+++ app/data_migrations/generate_audits_via_cv2.rb
@@ -10,25 +10,22 @@ class TransformSimpleEdiFileSet
   end
 
   def transform(xml_string)
-    enrollment_event_cv = enrollment_event_cv_for(xml_string)
-    if is_publishable?(enrollment_event_cv)
-      edi_builder = EdiCodec::X12::BenefitEnrollment.new(xml_string)
-      x12_xml = edi_builder.call.to_xml
-      publish_to_file(enrollment_event_cv, x12_xml)
-    end
+    enrollment_event_xml = Nokogiri::XML(xml_string)
+    edi_builder = EdiCodec::X12::BenefitEnrollment.new(xml_string)
+    x12_xml = edi_builder.call.to_xml
+    publish_to_file(enrollment_event_xml, x12_xml)
   end
 
-  def publish_to_file(enrollment_event_cv, x12_payload)
-    file_name = determine_file_name(enrollment_event_cv)
+  def publish_to_file(enrollment_event_xml, x12_payload)
+    file_name = determine_file_name(enrollment_event_xml)
     File.open(File.join(@out_path, file_name), 'w') do |f|
       f.write(x12_payload)
     end
   end
 
-  def find_carrier_abbreviation(enrollment_event_cv)
-    policy_cv = extract_policy(enrollment_event_cv)
-    hios_id = extract_hios_id(policy_cv)
-    active_year = extract_active_year(policy_cv)
+  def find_carrier_abbreviation(enrollment_event_xml)
+    hios_id = enrollment_event_xml.xpath("//cv:enrollment/cv:plan/cv:id/cv:id", XML_NS).first.content.strip.split("#").last
+    active_year = enrollment_event_xml.xpath("//cv:enrollment/cv:plan/cv:active_year", XML_NS).first.content.strip
     found_plan = Caches::CustomCache.lookup(Plan, :cv2_hios_active_year_plan_cache, [active_year.to_i, hios_id]) do
      Plan.where(:hios_plan_id => hios_id, :year => active_year.to_i).first
     end
@@ -36,34 +33,21 @@ class TransformSimpleEdiFileSet
     found_carrier.abbrev.upcase
   end
 
-  def determine_file_name(enrollment_event_cv)
-    market_identifier = shop_market?(enrollment_event_cv) ? "S" : "I"
-    carrier_identifier = find_carrier_abbreviation(enrollment_event_cv)
+  def determine_file_name(enrollment_event_xml)
+    market_identifier = shop_market?(enrollment_event_xml) ? "S" : "I"
+    carrier_identifier = find_carrier_abbreviation(enrollment_event_xml)
     category_identifier = "_A_F_"
-    "834_" + transaction_id(enrollment_event_cv) + "_" + carrier_identifier + category_identifier + market_identifier + "_1.xml"
+    "834_" + transaction_id(enrollment_event_xml) + "_" + carrier_identifier + category_identifier + market_identifier + "_1.xml"
   end
 
   protected
 
-  def is_publishable?(enrollment_event_cv)
-    Maybe.new(enrollment_event_cv).event.body.publishable?.value
+  def transaction_id(enrollment_event_xml)
+    enrollment_event_xml.xpath("//cv:enrollment_event_body/cv:transaction_id", XML_NS).first.content.strip
   end
 
-  def is_initial?(enrollment_event_cv)
-    event_name = Maybe.new(enrollment_event_cv).event.body.enrollment.enrollment_type.strip.split("#").last.downcase.value
-    (event_name == "initial")
-  end
-
-  def routing_key(enrollment_event_cv)
-    is_initial?(enrollment_event_cv) ? "hbx.enrollment_messages" : "hbx.maintenance_messages"
-  end
-
-  def transaction_id(enrollment_event_cv)
-    Maybe.new(enrollment_event_cv).event.body.transaction_id.strip.value
-  end
-
-  def shop_market?(enrollment_event_cv)
-    determine_market(enrollment_event_cv) == "shop"
+  def shop_market?(enrollment_event_xml)
+    enrollment_event_xml.xpath("//cv:policy/cv:enrollment/cv:shop_market", XML_NS).any?
   end
 end
 
@@ -79,21 +63,6 @@ class GenerateAudits
                                        :employer_id => {"$in" => employer_ids},
                                        :plan_id => {"$in" => plan_ids}}).no_timeout
 
-    # eligible_policies.each do |policy|
-    #   if !policy.canceled?
-    #     if !(policy.subscriber.coverage_start > active_end)
-    #       employer = policy.employer
-    #       next unless in_current_plan_year?(policy,employer,cutoff_date,active_start,active_end)
-    #       subscriber_id = policy.subscriber.m_id
-    #       subscriber_person = policy.subscriber.person
-    #       next if subscriber_person.blank?
-    #       auth_subscriber_id = subscriber_person.authority_member_id
-    #       if subscriber_id == auth_subscriber_id
-    #         yield policy
-    #       end
-    #     end
-    #   end
-    # end
 
     eligible_policies.each do |policy|
       if !policy.canceled?
