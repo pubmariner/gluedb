b4b880b00483146d5c22cb1df312e4fc1cb26771
Andrej Rasevic <andrej.petar.rasevic@gmail.com>
2018-03-26 15:29:28 -0400

    Refs #20065 fixing spec

--- spec/models/enrollment_action/initial_enrollment_spec.rb
+++ spec/models/enrollment_action/initial_enrollment_spec.rb
@@ -26,7 +26,7 @@ describe EnrollmentAction::InitialEnrollment, "with an initial enrollment event,
     :policy_cv => policy_cv,
     :existing_plan => existing_plan,
     :is_cobra? => false,
-    :kind => enrollment
+    :kind => "coverall"
   ) }
 
   let(:existing_plan) { double }
@@ -39,7 +39,7 @@ describe EnrollmentAction::InitialEnrollment, "with an initial enrollment event,
 
   before :each do
     allow(ExternalEvents::ExternalMember).to receive(:new).with(member_from_xml).and_return(member_database_record)
-    allow(ExternalEvents::ExternalPolicy).to receive(:new).with(policy_cv, existing_plan, false, market_from_payload: subject.action.kind.market).and_return(policy_database_record)
+    allow(ExternalEvents::ExternalPolicy).to receive(:new).with(policy_cv, existing_plan, false, market_from_payload: subject.action.kind).and_return(policy_database_record)
     allow(subject.action).to receive(:existing_policy).and_return(false)
   end
 
