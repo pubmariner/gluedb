b238d5dee04f65a46919dcf87ec40f702cb668e3
Zoheb Nensey <zoheb.nensey@gmail.com>
2018-04-19 16:32:26 -0400

    Refs #23663 update method to make sure we are selecting the correct enrollee

--- app/data_migrations/change_enrollee_relationship.rb
+++ app/data_migrations/change_enrollee_relationship.rb
@@ -6,7 +6,7 @@ class ChangeEnrolleeRelationship < MongoidMigrationTask
   def migrate
     policy = Policy.where(eg_id: ENV['eg_id']).first
     (raise "This policy cannot be found." if policy.blank?) unless Rails.env.test?
-    enrollee = policy.enrollees.detect{|en| en.m_id = ENV['hbx_id']}
+    enrollee = policy.enrollees.detect{|en| en.m_id == ENV['hbx_id']}
     (raise "This enrollee cannot be found." if enrollee.blank?) unless Rails.env.test? 
     enrollee.update_attributes(:rel_code => ENV['new_relationship'])
     puts "Relationship of enrollee is now #{enrollee.rel_code}" unless Rails.env.test?
