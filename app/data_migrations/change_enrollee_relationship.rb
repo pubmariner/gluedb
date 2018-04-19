8318dd41ec6d5711ff6e78db7bc2bb2739727c2a
Zoheb Nensey <zoheb.nensey@gmail.com>
2018-04-19 13:26:22 -0400

    Refs #23663 create rake task to change enrollee relationships

---
+++ app/data_migrations/change_enrollee_relationship.rb
@@ -0,0 +1,15 @@
+# Changes an Enrollee's Member ID 
+require File.join(Rails.root, "lib/mongoid_migration_task")
+
+class ChangeEnrolleeRelationship < MongoidMigrationTask
+
+  def migrate
+    policy = Policy.where(eg_id: ENV['eg_id']).first
+    (raise "This policy cannot be found." if policy.blank?) unless Rails.env.test?
+    enrollee = policy.enrollees.detect{|en| en.m_id = ENV['hbx_id']}
+    (raise "This enrollee cannot be found." if enrollee.blank?) unless Rails.env.test? 
+    enrollee.update_attributes(:rel_code => ENV['new_relationship'])
+    puts "Relationship of enrollee is now #{enrollee.rel_code}" unless Rails.env.test?
+  end
+  
+end
