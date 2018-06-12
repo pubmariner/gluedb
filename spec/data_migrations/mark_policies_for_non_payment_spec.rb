b9325faf2775e92d5eb3e3359ed6bfe858feb989
Andrej Rasevic <andrej.petar.rasevic@gmail.com>
2018-06-12 07:35:06 -0400

    Refs #24765 added additional specs

--- spec/data_migrations/mark_policies_for_non_payment_spec.rb
+++ spec/data_migrations/mark_policies_for_non_payment_spec.rb
@@ -3,10 +3,9 @@ require File.join(Rails.root,"app","data_migrations","mark_policies_for_non_paym
 
 describe MarkPoliciesForNonPayment, dbclean: :after_each do
   let(:given_task_name) { "mark_policies_for_non_payment" }
-  # policy factory generates a policy with the kind atrribute set to "individual" by default
-  # this will verify that the rake task is actually making a change and that the test instance
-  # does not have the correct value already
-  let!(:policy) { FactoryGirl.create(:policy, aasm_state: "terminated") }
+  let!(:policy1) { FactoryGirl.create(:policy, aasm_state: "terminated") }
+  let!(:policy2) { FactoryGirl.create(:policy, aasm_state: "cancelled") }
+  let!(:policy3) { FactoryGirl.create(:policy) }
   subject { MarkPoliciesForNonPayment.new(given_task_name, double(:current_scope => nil)) }
 
   describe "given a task name" do
@@ -15,19 +14,52 @@ describe MarkPoliciesForNonPayment, dbclean: :after_each do
     end
   end
 
-  describe "updating the term_for_np field to be true" do
+  describe "updating the term_for_np field to be true for terminated policy" do
     before(:each) do
-      allow(ENV).to receive(:[]).with("policy_id").and_return(policy.id)
+      allow(ENV).to receive(:[]).with("policy_id").and_return(policy1.id)
     end
 
     it "should initially be set to false" do
-      expect(policy.term_for_np).to eq false
+      expect(policy1.term_for_np).to eq false
     end
 
     it "should be set to true after the rake task is complete" do
       subject.migrate
-      policy.reload
-      expect(policy.term_for_np).to eq true
+      policy1.reload
+      expect(policy1.term_for_np).to eq true
     end
   end
+
+  describe "updating the term_for_np field to be true for cancelled policy" do
+    before(:each) do
+      allow(ENV).to receive(:[]).with("policy_id").and_return(policy2.id)
+    end
+
+    it "should initially be set to false" do
+      expect(policy2.term_for_np).to eq false
+    end
+
+    it "should be set to true after the rake task is complete" do
+      subject.migrate
+      policy2.reload
+      expect(policy2.term_for_np).to eq true
+    end
+  end
+
+  describe "ignoring policies that are neither cancelled or termninated" do
+    before(:each) do
+      allow(ENV).to receive(:[]).with("policy_id").and_return(policy3.id)
+    end
+
+    it "should initially be set to false" do
+      expect(policy3.term_for_np).to eq false
+    end
+
+    it "should remain false after the task is executed" do
+      subject.migrate
+      policy3.reload
+      expect(policy3.term_for_np).to eq false
+    end
+  end
+
 end
