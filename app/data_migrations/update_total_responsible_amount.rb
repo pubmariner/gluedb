ccd0bb06fe5c3688cbfdf9a0fc64f5734a7bd0f2
Zoheb Nensey <zoheb.nensey@gmail.com>
2018-07-17 10:45:49 -0400

    Refs #26378 modifications to policy finder and addition of rescues

--- app/data_migrations/update_total_responsible_amount.rb
+++ app/data_migrations/update_total_responsible_amount.rb
@@ -3,15 +3,21 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 class UpdateTotalResponsibleAmount < MongoidMigrationTask
 
   def migrate
-    policy = Policy.where(id: ENV['eg_id']).first
-    total_responsible_amount= ENV['total_responsible_amount']
-    premium_amount_total = ENV['premium_amount_total']
-    applied_aptc = ENV['applied_aptc']    
+    policy = Policy.where(eg_id: ENV['eg_id']).first
+    if policy.present? && policy.aptc_credits.empty?
+      total_responsible_amount= ENV['total_responsible_amount']
+      premium_amount_total = ENV['premium_amount_total']
+      applied_aptc = ENV['applied_aptc']    
 
-    policy.update_attributes(tot_res_amt: total_responsible_amount) if is_number?(total_responsible_amount)
-    policy.update_attributes(pre_amt_tot: premium_amount_total) if is_number?(premium_amount_total)
-    policy.update_attributes(applied_aptc: applied_aptc) if is_number?(applied_aptc)
-    policy.save!
+      policy.update_attributes(tot_res_amt: total_responsible_amount) if is_number?(total_responsible_amount)
+      policy.update_attributes(pre_amt_tot: premium_amount_total) if is_number?(premium_amount_total)
+      policy.update_attributes(applied_aptc: applied_aptc) if is_number?(applied_aptc)
+      policy.save!
+    elsif policy.blank?
+      puts "Policy not found for eg_id #{ENV['eg_id']}"
+    elsif policy.aptc_credits.size > 0
+      puts "This policy has aptc credit documents. Please inspect and modify those instead."
+    end
   end
 
   def is_number?(string)
