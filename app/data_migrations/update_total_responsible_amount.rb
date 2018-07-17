f5f4e1deb9d6def6dadea0525aefff3833d016c9
Zachary Landes <zach.g.landes@gmail.com>
2018-07-16 21:00:33 -0400

    clean up rake

--- app/data_migrations/update_total_responsible_amount.rb
+++ app/data_migrations/update_total_responsible_amount.rb
@@ -3,16 +3,14 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 class UpdateTotalResponsibleAmount < MongoidMigrationTask
 
   def migrate
-
     policy = Policy.where(id: ENV['eg_id']).first
     total_responsible_amount= ENV['total_responsible_amount']
     premium_amount_total = ENV['premium_amount_total']
     applied_aptc = ENV['applied_aptc']    
-    
+
     policy.update_attributes(tot_res_amt: total_responsible_amount) if is_number?(total_responsible_amount)
     policy.update_attributes(pre_amt_tot: premium_amount_total) if is_number?(premium_amount_total)
-    policy.update_attributes(applied_aptc: applied_aptc) if  is_number?(applied_aptc)
-    
+    policy.update_attributes(applied_aptc: applied_aptc) if is_number?(applied_aptc)
     policy.save!
   end
 
