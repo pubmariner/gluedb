52a4598489ef269b69750358915eda723191881b
Zachary Landes <zach.g.landes@gmail.com>
2018-07-20 10:20:25 -0400

    passing specs for updating aptc credits

--- app/data_migrations/update_aptc_credits.rb
+++ app/data_migrations/update_aptc_credits.rb
@@ -1,9 +1,20 @@
-require 'pry'
 require File.join(Rails.root, "lib/mongoid_migration_task")
 class UpdateAptcCredits < MongoidMigrationTask
 
   def migrate
     policy = Policy.where(eg_id: ENV['eg_id']).first
+
+    if policy.blank?
+      p "unable to find policy #{ENV['eg_id']}"
+    elsif policy.aptc_credits.present? 
+      credit_matcher(policy)
+    else 
+      p "no APTC credits found for policy #{policy.eg_id}"
+    end
+
+  end
+
+  def credit_matcher(policy)
     original_start_on = ENV['original_start_on']
     original_end_on = ENV['original_end_on']
     updated_start_on = ENV['updated_start_on']
@@ -12,17 +23,14 @@ class UpdateAptcCredits < MongoidMigrationTask
     pre_amt_tot = ENV['pre_amt_tot']
     tot_res_amt = ENV['tot_res_amt']
 
-        
-    if policy.blank?
-      puts "unable to find policy #{ENV['eg_id']}"
-    elsif policy.aptc_credits.present? 
-      credit = policy.aptc_credits.where(start_on: original_start_on, end_on: original_end_on).first
-      credit.update_attributes(start_on: updated_start_on, end_on: updated_end_on, aptc: aptc, pre_amt_tot:pre_amt_tot, tot_res_amt: tot_res_amt)
+    matching_credit = policy.aptc_credits.where(start_on: original_start_on, end_on: original_end_on).first
+    if matching_credit 
+      matching_credit.update_attributes(start_on: updated_start_on, end_on: updated_end_on, aptc: aptc, pre_amt_tot:pre_amt_tot, tot_res_amt: tot_res_amt)
       policy.save!
-      puts "APTC credits updated for policy #{policy.eg_id}"
-    else 
-     puts "no APTC credits found for policy #{policy.eg_id}"
+      p "APTC credits updated for policy #{policy.eg_id}"
+    else
+      p "Unable to find matching APTC credits for policy #{policy.eg_id}"   
     end
-
   end
+
 end
