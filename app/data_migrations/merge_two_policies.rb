9ca1fceaba1058bd4e44da97054f42c78ce7a578
Zachary Landes <zach.g.landes@gmail.com>
2018-07-25 17:00:33 -0400

    merging policies without specs

--- app/data_migrations/merge_two_policies.rb
+++ app/data_migrations/merge_two_policies.rb
@@ -1,30 +1,45 @@
+require 'pry'
 require File.join(Rails.root, "lib/mongoid_migration_task")
 
 class MergeTwoPolicies < MongoidMigrationTask
-  
-  policy_to_keep = Policy.where(eg_id: ENV['policy_to_keep']).first
-  policy_to_remove = Policy.where(eg_id: ENV['policy_to_remove']).first
-  tot_res_amt = Policy.where(eg_id:ENV['policy_of_final_tot_res_amt']).first.tot_res_amt
-  pre_amt_tot = Policy.where(eg_id:ENV['policy_of_final_pre_amt']).first.pre_amt_tot
-  tot_emp_res_amt = Policy.where(eg_id:ENV['policy_of_final_tot_emp_res_amt']).first.tot_emp_res_amt
-  rating_area = Policy.where(eg_id:ENV['policy_of_final_rating_area']).first.rating_area
-  composite_rating_tier = Policy.where(eg_id:ENV['policy_of_final_composite_rating_tier']).first.composite_rating_tier
+  def migrate 
 
-  existing_enrollees = policy_to_keep.enrollees.map(&:m_id)
-  enrollees_to_move = policy_to_remove.enrollees.where(:m_id => {"$nin" => existing_enrollees}).to_a
-  policy_to_keep.enrollees += enrollees_to_move
-  policy_to_keep.hbx_enrollment_ids += policy_to_remove.hbx_enrollment_ids
-  policy_to_keep.save
-  
-  policy_to_keep.update_attributes(
-    pre_amt_tot: pre_amt_tot,
-    tot_emp_res_amt: tot_emp_res_amt,
-    tot_res_amt: tot_res_amt,
-    rating_area: rating_area,
-    composite_rating_tier: composite_rating_tier
+    policy_to_keep = Policy.where(eg_id: ENV['policy_to_keep']).first
+    policy_to_remove = Policy.where(eg_id: ENV['policy_to_remove']).first
+    
+    binding.pry
+    
+    tot_res_amt = Policy.where(eg_id:ENV['policy_of_final_tot_res_amt']).first.tot_res_amt
+    pre_amt_tot = Policy.where(eg_id:ENV['policy_of_final_pre_amt_tot']).first.pre_amt_tot
+    tot_emp_res_amt = Policy.where(eg_id:ENV['policy_of_final_tot_emp_res_amt']).first.tot_emp_res_amt
+    rating_area = Policy.where(eg_id:ENV['policy_of_final_rating_area']).first.rating_area
+    composite_rating_tier = Policy.where(eg_id:ENV['policy_of_final_composite_rating_tier']).first.composite_rating_tier
+
+    
+    
+    existing_enrollees = policy_to_keep.enrollees.map(&:m_id)
+    enrollees_to_move = policy_to_remove.enrollees.where(:m_id => {"$nin" => existing_enrollees}).to_a
+    policy_to_keep.enrollees << enrollees_to_move
+    policy_to_keep.hbx_enrollment_ids << policy_to_remove.hbx_enrollment_ids
+    policy_to_keep.hbx_enrollment_ids.uniq
+    policy_to_keep.save
+
+    policy_to_keep.update_attributes!(
+      pre_amt_tot: pre_amt_tot,
+      tot_emp_res_amt: tot_emp_res_amt,
+      tot_res_amt: tot_res_amt,
+      rating_area: rating_area,
+      composite_rating_tier: composite_rating_tier
     )
 
+    policy_to_remove.update_attributes!(eg_id: "#{policy_to_remove.eg_id} - DO NOT USE")
 
+    
+    new_enrollment_ids = []
+    policy_to_remove.hbx_enrollment_ids.each do |id|
+      new_enrollment_ids << "#{id} - DO NOT USE"
+    end
+  end
 
 
 end
--- lib/tasks/merge_two_policies.rake
+++ lib/tasks/merge_two_policies.rake
@@ -1,8 +1,7 @@
 require File.join(Rails.root,"app","data_migrations","merge_two_policies.rb")
 
 # This rake task merges people in Glue.
-# format RAILS_ENV=production bundle exec rake migrations:merge_duplicate_people person_to_keep='person_mongo_id' person_to_remove='person_mongo_id'
-
+# format RAILS_ENV=production bundle exec rake migrations:merge_two_policies policy_to_keep='1' policy_to_remove='2' policy_of_final_tot_res_amt="1" policy_of_final_pre_amt_tot="1" policy_of_final_tot_emp_res_amt='1' policy_of_final_rating_area="1" policy_of_final_composite_rating_tier="1" 
 namespace :migrations do 
   desc "Merge Two Policies"
   MergeTwoPolicies.define_task :merge_two_policies => :environment
