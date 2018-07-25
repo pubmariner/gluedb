0d5f8fbc96ac1d869638d3e9178f0c4430bd95a1
Zachary Landes <zach.g.landes@gmail.com>
2018-07-25 16:00:00 -0400

    initial merge attempt

--- app/data_migrations/merge_two_policies.rb
+++ app/data_migrations/merge_two_policies.rb
@@ -2,8 +2,29 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 
 class MergeTwoPolicies < MongoidMigrationTask
   
-  first_policy = Policy.where(eg_id: ENV['first_policy']).first
-  second_policy = Policy.where(eg_id: ENV['second_policy']).first
+  policy_to_keep = Policy.where(eg_id: ENV['policy_to_keep']).first
+  policy_to_remove = Policy.where(eg_id: ENV['policy_to_remove']).first
+  tot_res_amt = Policy.where(eg_id:ENV['policy_of_final_tot_res_amt']).first.tot_res_amt
+  pre_amt_tot = Policy.where(eg_id:ENV['policy_of_final_pre_amt']).first.pre_amt_tot
+  tot_emp_res_amt = Policy.where(eg_id:ENV['policy_of_final_tot_emp_res_amt']).first.tot_emp_res_amt
+  rating_area = Policy.where(eg_id:ENV['policy_of_final_rating_area']).first.rating_area
+  composite_rating_tier = Policy.where(eg_id:ENV['policy_of_final_composite_rating_tier']).first.composite_rating_tier
+
+  existing_enrollees = policy_to_keep.enrollees.map(&:m_id)
+  enrollees_to_move = policy_to_remove.enrollees.where(:m_id => {"$nin" => existing_enrollees}).to_a
+  policy_to_keep.enrollees += enrollees_to_move
+  policy_to_keep.hbx_enrollment_ids += policy_to_remove.hbx_enrollment_ids
+  policy_to_keep.save
+  
+  policy_to_keep.update_attributes(
+    pre_amt_tot: pre_amt_tot,
+    tot_emp_res_amt: tot_emp_res_amt,
+    tot_res_amt: tot_res_amt,
+    rating_area: rating_area,
+    composite_rating_tier: composite_rating_tier
+    )
+
+
 
 
 end
