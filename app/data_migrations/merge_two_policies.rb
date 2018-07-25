2365ce30961379aca54de9beaffce6d552f8a773
Zachary Landes <zach.g.landes@gmail.com>
2018-07-25 09:22:27 -0400

    first commit

--- app/data_migrations/merge_two_policies.rb
+++ app/data_migrations/merge_two_policies.rb
@@ -1,6 +1,9 @@
 require File.join(Rails.root, "lib/mongoid_migration_task")
 
 class MergeTwoPolicies < MongoidMigrationTask
-
   
+  first_policy = Policy.where(eg_id: ENV['first_policy']).first
+  second_policy = Policy.where(eg_id: ENV['second_policy']).first
+
+
 end
