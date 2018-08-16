c1b5f4b90b3945ed954411c40552ceaa4dd5ff96
Zoheb Nensey <zoheb.nensey@gmail.com>
2018-08-15 22:00:30 -0400

    Refs #24598 fix broken specs

--- spec/models/enrollment_action/dependent_drop_and_add_spec.rb
+++ spec/models/enrollment_action/dependent_drop_and_add_spec.rb
@@ -23,19 +23,28 @@ describe EnrollmentAction::DependentDropAndAdd, "given an enrollment even set th
 end
 
 describe EnrollmentAction::DependentDropAndAdd, "given a qualified enrollment set, being persisted" do 
-  let(:member_primary) { instance_double(Openhbx::Cv2::EnrolleeMember, id: 1) }
-  let(:member_secondary) { instance_double(Openhbx::Cv2::EnrolleeMember, id: 2) }
+  let(:member_primary) { instance_double(Openhbx::Cv2::EnrolleeMember, id: "1") }
+  let(:member_secondary) { instance_double(Openhbx::Cv2::EnrolleeMember, id: "2") }
   let(:member_drop) { instance_double(Openhbx::Cv2::EnrolleeMember, id: 3) }
   let(:benefit_drop) { instance_double(Openhbx::Cv2::EnrolleeBenefit, end_date: "20140304")}
-  let(:member_new) { instance_double(Openhbx::Cv2::EnrolleeMember, id: 4) }
+  let(:benefit_add) { instance_double(Openhbx::Cv2::EnrolleeBenefit, begin_date: "20140304", premium_amount: "100.11")}
+  let(:member_relationship) { instance_double(Openhbx::Cv2::PersonRelationship, subject_individual: "blah#1", relationship_uri: "blah#child", object_individual: "blah#4")}
+  let(:member_new) { instance_double(Openhbx::Cv2::EnrolleeMember, id: '4', :person_relationships => [member_relationship]) }
 
-  let(:enrollee_primary) { instance_double(::Openhbx::Cv2::Enrollee, :member => member_primary) }
-  let(:enrollee_secondary) { instance_double(::Openhbx::Cv2::Enrollee, :member => member_secondary) }
-  let(:enrollee_drop) { instance_double(::Openhbx::Cv2::Enrollee, :member => member_drop, :benefit => benefit_drop) }
-  let(:enrollee_new) { instance_double(::Openhbx::Cv2::Enrollee, :member => member_new) }
+  let(:enrollee_primary) { instance_double(::Openhbx::Cv2::Enrollee, :member => member_primary, :is_subscriber => true) }
+  let(:enrollee_secondary) { instance_double(::Openhbx::Cv2::Enrollee, :member => member_secondary, :is_subscriber => false) }
+  let(:enrollee_drop) { instance_double(::Openhbx::Cv2::Enrollee, :member => member_drop, :benefit => benefit_drop, :is_subscriber => false) }
+  let(:enrollee_new) { instance_double(::Openhbx::Cv2::Enrollee, :member => member_new, :benefit => benefit_add, :is_subscriber => false) }
 
   let(:terminated_policy_cv) { instance_double(Openhbx::Cv2::Policy, :enrollees => [enrollee_primary, enrollee_secondary, enrollee_drop])}
-  let(:new_policy_cv) { instance_double(Openhbx::Cv2::Policy, :enrollees => [enrollee_primary, enrollee_secondary, enrollee_new]) }
+  let(:shop_market) { nil }
+  let(:individual_market) { instance_double(Openhbx::Cv2::PolicyEnrollmentIndividualMarket, :is_carrier_to_bill => true, :applied_aptc_amount => "23.45" ) }
+  let(:new_policy_enrollment) { instance_double(Openhbx::Cv2::PolicyEnrollment, 
+                                                :premium_total_amount => "123.45", 
+                                                :total_responsible_amount => "100.00", 
+                                                :shop_market => shop_market, 
+                                                :individual_market => individual_market )}
+  let(:new_policy_cv) { instance_double(Openhbx::Cv2::Policy, :enrollees => [enrollee_primary, enrollee_secondary, enrollee_new], :policy_enrollment => new_policy_enrollment) }
   let(:plan) { instance_double(Plan, :id => 1) }
   let(:policy) { FactoryGirl.create(:policy)}
   let(:primary_db_record) { instance_double(ExternalEvents::ExternalMember, :persist => true) }
@@ -56,7 +65,7 @@ describe EnrollmentAction::DependentDropAndAdd, "given a qualified enrollment se
     :all_member_ids => [1,2,3]
     ) }
   let(:policy_updater_drop) { ExternalEvents::ExternalPolicyMemberDrop.new(policy,terminated_policy_cv,[3]) }
-  let(:policy_updater_add) { instance_double(ExternalEvents::ExternalPolicyMemberAdd) }
+  let(:policy_updater_add) { ExternalEvents::ExternalPolicyMemberAdd.new(policy,new_policy_cv,['4']) }
 
   subject do
     EnrollmentAction::DependentDropAndAdd.new(dependent_drop_event, dependent_add_event)
@@ -75,15 +84,13 @@ describe EnrollmentAction::DependentDropAndAdd, "given a qualified enrollment se
   end
 
   it 'should add the other dependent' do 
+    allow(enrollee_primary).to receive(:subscriber?).and_return(enrollee_primary.is_subscriber)
+    allow(enrollee_secondary).to receive(:subscriber?).and_return(enrollee_secondary.is_subscriber)
+    allow(enrollee_new).to receive(:subscriber?).and_return(enrollee_new.is_subscriber)
 
-    allow(ExternalEvents::ExternalMember).to receive(:new).with(member_primary).and_return(primary_db_record)
-    allow(ExternalEvents::ExternalMember).to receive(:new).with(member_secondary).and_return(secondary_db_record)
-    allow(ExternalEvents::ExternalMember).to receive(:new).with(member_new).and_return(new_db_record)
 
-    allow(policy).to receive(:save!).and_return(true)
-    allow(ExternalEvents::ExternalPolicyMemberAdd).to receive(:new).with(policy, new_policy_cv, [4]).and_return(policy_updater_add)
-    allow(policy_updater_add).to receive(:persist).and_return(true)
-
+    policy_updater_add.persist
+    policy.reload
     expect(policy.enrollees.map(&:m_id).include?('4')).to be_truthy
   end
   
