7161be4da85799cb1135402e0f68cfb8dba74638
Nikhil S <nikhilks219@gmail.com>
2018-09-10 16:35:41 -0400

    Ref#30568: rake modified to update employer contribution with other aptc credits

--- app/data_migrations/update_total_responsible_amount.rb
+++ app/data_migrations/update_total_responsible_amount.rb
@@ -3,20 +3,25 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 class UpdateTotalResponsibleAmount < MongoidMigrationTask
 
   def migrate
-    policy = Policy.where(eg_id: ENV['eg_id']).first
-    if policy.present? && policy.aptc_credits.empty?
-      total_responsible_amount= ENV['total_responsible_amount']
-      premium_amount_total = ENV['premium_amount_total']
-      applied_aptc = ENV['applied_aptc']    
-
-      policy.update_attributes(tot_res_amt: total_responsible_amount) if is_number?(total_responsible_amount)
-      policy.update_attributes(pre_amt_tot: premium_amount_total) if is_number?(premium_amount_total)
-      policy.update_attributes(applied_aptc: applied_aptc) if is_number?(applied_aptc)
-      policy.save!
-    elsif policy.blank?
-      puts "Policy not found for eg_id #{ENV['eg_id']}"
-    elsif policy.aptc_credits.size > 0
-      puts "This policy has aptc credit documents. Please inspect and modify those instead."
+    premium_amount_total = ENV['premium_amount_total']
+    applied_aptc = ENV['applied_aptc'] 
+    employer_contribution= ENV['employer_contribution']   
+    total_responsible_amount= ENV['total_responsible_amount']
+    eg_id= ENV['eg_id']
+    begin
+      policy = Policy.where(eg_id: eg_id).first
+      if policy.present? && policy.aptc_credits.empty?
+        policy.update_attributes!(pre_amt_tot: premium_amount_total) if is_number?(premium_amount_total)
+        policy.update_attributes!(applied_aptc: applied_aptc) if is_number?(applied_aptc)
+        policy.update_attributes!(employer_contribution: employer_contribution) if is_number?(employer_contribution)
+        policy.update_attributes!(tot_res_amt: total_responsible_amount) if is_number?(total_responsible_amount)
+        policy.save
+        puts "Updated Sucessfully aptc_credits for the eg_id #{eg_id}" unless Rails.env.test?
+      else
+        raise "Policy not found for eg_id #{ENV['eg_id']} or This policy has aptc credit documents." unless Rails.env.test?
+      end
+    rescue Exception => e
+      puts e.message
     end
   end
 
