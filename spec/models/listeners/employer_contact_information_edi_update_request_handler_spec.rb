a1e60d04d1e626b39ef839dcffa98796c0ab2cf5
Nikhil S <nikhilks219@gmail.com>
2018-10-15 13:38:59 -0400

    33518: added an another expect in the spec

--- spec/models/listeners/employer_contact_information_edi_update_request_handler_spec.rb
+++ spec/models/listeners/employer_contact_information_edi_update_request_handler_spec.rb
@@ -12,21 +12,26 @@ describe Listeners::EmployerContactInformationEdiUpdateRequestHandler do
   let!(:message_properties) { double("PROPERTIES", headers: message_headers, :timestamp => nil)}
   let(:employer_hbx_id) { "212344333" }
   let(:event_broadcaster) { instance_double(::Amqp::EventBroadcaster) }
-  let(:test) { double("Publishers::EmployerEnrollmentNotification")}
+  let(:enrollment_notification) { instance_double(Publishers::EmployerEnrollmentNotification)}
   let(:expected_employer_properties) do
     {:routing_key=> "info.application.glue.employer_contact_information_edi_update_request_handler.request_processed",
      :headers=>{"employer_id"=>employer_hbx_id, :return_status=>"200"}}
    end
 
   subject { Listeners::EmployerContactInformationEdiUpdateRequestHandler.new(channel, queue) }
-  
+
   before do
-    allow(Publishers::EmployerEnrollmentNotification).to receive(:new).and_return(test)
-    allow(test).to receive(:process_enrollments_for_edi).and_return("rspec")
+    allow(Publishers::EmployerEnrollmentNotification).to receive(:new).and_return(enrollment_notification)
+    allow(enrollment_notification).to receive(:process_enrollments_for_edi)
     allow(::Amqp::EventBroadcaster).to receive(:new).with(connection).and_return(event_broadcaster)
     allow(event_broadcaster).to receive(:broadcast).and_return(true)
     allow(channel).to receive(:acknowledge).with(message_tag, false)
+
     subject.on_message(delivery_info, message_properties, message_body)
+  end
+
+  it 'calls process_enrollments_for_edi on the enrollment_notification' do
+    expect(enrollment_notification).to have_received(:process_enrollments_for_edi)
   end
  
   it 'broadcasts the message' do
