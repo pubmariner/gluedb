533a3fbf1118fdd1e713b4e18d83d9c314a92c9d
zasman <zanesandin@gmail.com>
2019-02-05 14:56:51 -0500

    Fixes some spacing and tabs.

--- script/queries/add_researched_user_dates.rb
+++ script/queries/add_researched_user_dates.rb
@@ -5,9 +5,9 @@ CSV.foreach("missing_dates.csv", headers: true) do |row|
   created_at = Time.strptime(date + "EST", "%Y-%m-%dT %H:%M:%S %Z")
   user = User.where({email: Regexp.compile(Regexp.escape(email), true)}).first
   if user
-  if user.created_at.blank?
-    puts([email, created_at].inspect)
-    User.where({email: Regexp.compile(Regexp.escape(email), true)}).update({"$set" => {"created_at" => created_at}})
-  end
+    if user.created_at.blank?
+      puts([email, created_at].inspect)
+      User.where({email: Regexp.compile(Regexp.escape(email), true)}).update({"$set" => {"created_at" => created_at}})
+    end
   end
 end
