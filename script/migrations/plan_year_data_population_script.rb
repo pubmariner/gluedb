16b4db047c028add3812e8fdbdf4204305fc7f77
harsha280 <harsha.vskr@gmail.com>
2019-03-04 16:41:42 -0500

    Refs 40021 Updated spec

--- script/migrations/plan_year_data_population_script.rb
+++ script/migrations/plan_year_data_population_script.rb
@@ -21,17 +21,18 @@ class PlanYearDataPopulationScript
     field_names = %w(employer_hbx_id employer_fein plan_year_start plan_year_end carrier_hbx_id carrier_fein matched results)
     timestamp = Time.now.strftime('%Y%m%d%H%M')
     export_csv_path = File.expand_path("#{Rails.root}/plan_year_data_population_#{timestamp}.csv")
-    export_csv_path << field_names
-    @csv.each do |row|    
-      csv << generate__row(row)
+    CSV.open(export_csv_path, 'w', write_headers: true, headers: field_names) do |csv|
+      @csv.each do |row|    
+        csv << generate_row(row)
+      end
     end
   end
 
   def generate_row(row)
-      employer_hbx_id = row.employer_hbx_id
-      employer_fein = row.employer_fein
-      plan_year_start = row.effective_period_start_on
-      plan_year_end = row.effective_period_end_on
+      employer_hbx_id = row['employer_hbx_id']
+      employer_fein = row['employer_fein']
+      plan_year_start = Date.strptime(row['effective_period_start_on'], "%m-%d-%Y")
+      plan_year_end = Date.strptime(row['effective_period_end_on'], "%m-%d-%Y")
       plan_years = pull_plan_year(plan_year_start, employer_fein)
       status = ""
 
@@ -43,25 +44,95 @@ class PlanYearDataPopulationScript
         else
           plan_years.first.update_attributes(start_date: plan_year_start, end_date: plan_year_end)
           status = "matched"
-          results = "updated end date"
+          results = "updated dates"
         end
       when 0
         status = "unmatched"
         year_based_plan_years = fetch_plan_year_by_year(plan_year_start, employer_fein)
         if year_based_plan_years.count > 1 || year_based_plan_years.count == 0
-          results = "found too many plan years with given start_date or bad data"
+          results = "found too many plan years with given start date or bad data"
         else
           year_based_plan_years.first.update_attributes(start_date: plan_year_start, end_date: plan_year_end)
-          results = "udpated both start_date and end date"
+          results = "udpated both start date and end date"
         end
       else
         status = "un matched"
         results = "Did not found matching plan year with start date"
       end
-      [employer_hbx_id, employer_fein, plan_year_start, plan_year_end, row.carrier_hbx_id, row.carrier_fein, status, results]
+      [employer_hbx_id, employer_fein, plan_year_start, plan_year_end, row['carrier_hbx_id'], row['carrier_fein'], status, results]
   end
 end
 
-class_instance = PlanYearDataPopulationScript.new("#{Rails.root}/file_path.csv")
-class_instance.export_csv
+unless Rails.env.test?
+ class_instance = PlanYearDataPopulationScript.new("#{Rails.root}/file_path.csv")  
+ class_instance.export_csv
+end
+require 'csv'
 
+class PlanYearDataPopulationScript
+
+  def initialize(file_path)
+    csv_path = File.read(file_path)
+    @csv = CSV.parse(csv_path, :headers => true)
+  end
+
+  def pull_plan_year(start_date, fein)
+    plan_years = Employer.where(fein: fein).first.plan_years.where(:"start_date" => {"$eq" => start_date})
+    return plan_years
+  end
+
+  def fetch_plan_year_by_year(start_date, fein)
+    plan_years = Employer.where(fein: fein).first.plan_years.select {|py| py.start_date.year >= start_date.year}
+    return plan_years
+  end
+
+  def export_csv
+    field_names = %w(employer_hbx_id employer_fein plan_year_start plan_year_end carrier_hbx_id carrier_fein matched results)
+    timestamp = Time.now.strftime('%Y%m%d%H%M')
+    export_csv_path = File.expand_path("#{Rails.root}/plan_year_data_population_#{timestamp}.csv")
+    CSV.open(export_csv_path, 'w', write_headers: true, headers: field_names) do |csv|
+      @csv.each do |row|
+        csv << generate_row(row)
+      end
+    end
+  end
+
+  def generate_row(row)
+    employer_hbx_id = row['employer_hbx_id']
+    employer_fein = row['employer_fein']
+    plan_year_start = Date.strptime(row['effective_period_start_on'], "%m-%d-%Y")
+    plan_year_end = Date.strptime(row['effective_period_end_on'], "%m-%d-%Y")
+    plan_years = pull_plan_year(plan_year_start, employer_fein)
+    status = ""
+
+    case plan_years.count
+    when 1
+      if (plan_years.first.start_date == plan_year_start) && (plan_years.first.end_date == plan_year_end)
+        status = "matched"
+        results = "not updated"
+      else
+        plan_years.first.update_attributes(start_date: plan_year_start, end_date: plan_year_end)
+        status = "matched"
+        results = "updated dates"
+      end
+    when 0
+      status = "unmatched"
+      year_based_plan_years = fetch_plan_year_by_year(plan_year_start, employer_fein)
+      if year_based_plan_years.count > 1 || year_based_plan_years.count == 0
+        results = "found too many plan years with given start date or bad data"
+      else
+        year_based_plan_years.first.update_attributes(start_date: plan_year_start, end_date: plan_year_end)
+        results = "udpated both start_date and end date"
+      end
+    else
+      status = "un matched"
+      results = "Did not found matching plan year with start date"
+    end
+    [employer_hbx_id, employer_fein, plan_year_start, plan_year_end, row['carrier_hbx_id'], row['carrier_fein'], status, results]
+  end
+end
+
+unless Rails.env.test?
+  class_instance = PlanYearDataPopulationScript.new("#{Rails.root}/file_path.csv")
+  class_instance.export_csv
+end
--- spec/data_migrations/plan_year_data_population_spec.rb
+++ spec/data_migrations/plan_year_data_population_spec.rb
@@ -1,20 +1,49 @@
 require "rails_helper"
 require 'pry'
 
+require File.join(Rails.root, "script", "migrations", "plan_year_data_population_script")
+
 describe 'PlanYearDataPopulationScript', :dbclean => :after_each do
 
-  describe '.export_csv' do
-    let(:employer) { FactoryGirl.create(:employer_with_plan_year) }
-    let(:valid_row_data) {[employer_hbx_id: employer.hbx_id,
-      employer_fein: employer.fein,
-      effective_period_start_on: employer.plan_year_start,
-      effective_period_end_on: employer.plan_year_end,
-      carrier_hbx_id: "",
-      carrier_fein: ""]}
-    it 'finds and assign employer for view' do
-      x= PlanYearDataPopulationScript.new("#{Rails.root}/file_path.csv").generate_row(valid_row_data)
-      x.status == "matched"
+  after :each do
+    File.delete("#{Rails.root}/plan_year_data_test.csv") if File.exist?("#{Rails.root}/plan_year_data_test.csv")
+  end
 
+  describe '.export_csv' do
+
+    it 'finds and assign employer for view' do
+      employer = FactoryGirl.create(:employer_with_plan_year)
+      employer1 = FactoryGirl.create(:employer_with_plan_year)
+      # employer2 = FactoryGirl.create(:employer_with_plan_year) 
+
+      file = "#{Rails.root}/public/user_data.csv"
+
+      field_names = %w(employer_hbx_id employer_fein effective_period_start_on effective_period_end_on carrier_hbx_id carrier_fein)
+      export_csv_path = File.expand_path("#{Rails.root}/plan_year_data_test.csv")
+      CSV.open(export_csv_path, 'w', write_headers: true, headers: field_names) do |csv|
+        csv << [employer.hbx_id, employer.fein, employer.plan_years.first.start_date, employer.plan_years.first.end_date, "", ""]
+        csv << [employer1.hbx_id, employer1.fein, employer1.plan_years.first.start_date, employer1.plan_years.first.end_date + 1.month, "", ""]
+      end
+
+      x = PlanYearDataPopulationScript.new("#{Rails.root}/plan_year_data_test.csv").export_csv
+    end
+  end
+
+  describe "Unmatched Plan Year" do
+
+    it 'finds and assign employer for view' do
+      employer = FactoryGirl.create(:employer_with_plan_year)
+      employer1 = FactoryGirl.create(:employer_with_plan_year)
+      # employer2 = FactoryGirl.create(:employer_with_plan_year)
+
+      field_names = %w(employer_hbx_id employer_fein effective_period_start_on effective_period_end_on carrier_hbx_id carrier_fein)
+      export_csv_path = File.expand_path("#{Rails.root}/plan_year_data_test.csv")
+
+      CSV.open(export_csv_path, 'w', write_headers: true, headers: field_names) do |csv|
+        csv << [employer.hbx_id, employer.fein, employer.plan_years.first.start_date + 1.year, employer.plan_years.first.end_date, "", ""]
+        csv << [employer1.hbx_id, employer1.fein, employer1.plan_years.first.start_date + 1.year, employer1.plan_years.first.end_date + 1.month, "", ""]
+      end
+      x = PlanYearDataPopulationScript.new("#{Rails.root}/plan_year_data_test.csv").export_csv
     end
   end
 end
