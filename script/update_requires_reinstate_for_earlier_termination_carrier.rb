a0426295d0653756fd7d092b27f6c4e1e73db0bb
saimekala07 <saimekala07@gmail.com>
2019-03-11 13:02:55 -0400

    refs #40071 updated script to use hbx_id instead of name

--- script/update_requires_reinstate_for_earlier_termination_carrier.rb
+++ script/update_requires_reinstate_for_earlier_termination_carrier.rb
@@ -1,5 +1,5 @@
  Carrier.all.each do |carrier|
-   unless carrier.name == "Care First"
+   unless carrier.hbx_carrier_id == "116036"
      carrier.update_attributes(requires_reinstate_for_earlier_termination: true)
    end
  end
