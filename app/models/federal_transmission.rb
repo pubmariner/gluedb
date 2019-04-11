ae8377b11ccedd74e173cba8d1a2b59776bf5cc9
harsha280 <harsha.vskr@gmail.com>
2019-04-11 16:17:33 -0400

    Refs 40530 Updated federal transmission report

--- app/models/federal_transmission.rb
+++ app/models/federal_transmission.rb
@@ -3,7 +3,7 @@ class FederalTransmission
   include Mongoid::Timestamps::Created
 
   field :created_at, type: DateTime
-  field :report_type, type: Array
+  field :report_type, type: String
   field :batch_id, type: String
   field :content_file, type: String
   field :record_sequence_number, type: String
