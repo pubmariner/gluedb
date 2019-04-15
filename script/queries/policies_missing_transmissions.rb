64fe9459c9af23b2eea80b99bfb9f05ce58ef9e4
zasman <zanesandin@gmail.com>
2019-04-15 17:38:00 -0400

    REFS 40701 Tweaks policies missing transmissions generator and adds spec
    Fixes messed up file name
    
    stuff

--- script/queries/policies_missing_transmissions.rb
+++ script/queries/policies_missing_transmissions.rb
@@ -1,42 +1,48 @@
-ct_cache = Caches::Mongoid::SinglePropertyLookup.new(Plan, "coverage_type")
+class PoliciesMissingTransmissions
 
-all_pol_ids = Protocols::X12::TransactionSetHeader.collection.aggregate([
-  {"$match" => {
-    "policy_id" => { "$ne" => nil }
-  }},
-  { "$group" => { "_id" => "$policy_id" }}
-]).map { |val| val["_id"] }
+  attr_accessor :ct_cache, :all_pol_ids, :all_policies, :untransmitted_pols, :excluded_policies, :timestamp
 
-# No cancels/terms in this batch!
-pols_2015 = Policy.all.no_timeout
-
-puts pols_2015.length
-
-untransmitted_pols = []
-
-if File.exists?("policy_blacklist.txt")
-  excluded_policies = File.read("policy_blacklist.txt").split("\n").map(&:strip).map(&:to_i)
-else
-  excluded_policies = []
-end
-
-timestamp = Time.now.strftime('%Y%m%d%H%M')
-
-CSV.open("policies_without_transmissions_#{timestamp}.csv","w") do |csv|
-  csv << ["Created At", "Enrollment Group ID", "Carrier", "Employer", "Subscriber Name", "Subscriber HBX ID"]
-  pols_2015.each do |pol|
-    if !all_pol_ids.include?(pol.id)
-      if pol.subscriber.present? && !pol.canceled?
-        unless excluded_policies.include? pol.id
-          created_at = pol.created_at
-          eg_id = pol.eg_id
-          carrier = pol.plan.carrier.abbrev
-          employer = pol.try(:employer).try(:name)
-          subscriber_name = pol.subscriber.person.full_name rescue ""
-          subscriber_hbx_id = pol.subscriber.m_id
-          csv << [created_at,eg_id,carrier,employer,subscriber_name,subscriber_hbx_id]
+  def initialize
+    @ct_cache = Caches::Mongoid::SinglePropertyLookup.new(Plan, "coverage_type")
+    @all_pol_ids = Protocols::X12::TransactionSetHeader.collection.aggregate([
+      {"$match" => {
+        "policy_id" => { "$ne" => nil }
+      }},
+      { "$group" => { "_id" => "$policy_id" }}
+    ]).map { |val| val["_id"] }
+    # No cancels/terms in this batch!
+    @all_policies = Policy.all.no_timeout.to_a
+    puts(@all_policies.length.to_s + " policies present")
+    @untransmitted_pols = []
+    if File.exists?("policy_blacklist.txt")
+      @excluded_policies = File.read("policy_blacklist.txt").split("\n").map(&:strip).map(&:to_i)
+    else
+      @excluded_policies = []
+    end
+    @timestamp = Time.now.strftime('%Y%m%d%H%M')
+  end
+  
+  def process
+    CSV.open("policies_without_transmissions_#{@timestamp}.csv","w") do |csv|
+      csv << ["Created At", "Enrollment Group ID", "Carrier", "Employer", "Subscriber Name", "Subscriber HBX ID"]
+      @all_policies.each do |pol|
+        if !@all_pol_ids.include?(pol.id)
+          if pol.subscriber.present? && !pol.canceled?
+            unless @excluded_policies.include? pol.id
+              created_at = pol.created_at
+              eg_id = pol.eg_id
+              carrier = pol.plan.carrier.abbrev
+              employer = pol.try(:employer).try(:name)
+              subscriber_name = pol.subscriber.person.full_name rescue ""
+              subscriber_hbx_id = pol.subscriber.m_id
+              csv << [created_at,eg_id,carrier,employer,subscriber_name,subscriber_hbx_id]
+            end
+          end
         end
       end
-    end
+      puts("Finished creating policies without transmissions CSV.")
+    end 
   end
 end
+
+PoliciesMissingTransmissions.new.process
