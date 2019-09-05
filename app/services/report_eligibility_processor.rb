ddd108be7f742f2928b2492af93d470f6041dd44
Zachary Landes <zach.g.landes@gmail.com>
2019-09-05 17:43:43 -0400

    created ReportEligiblityProcessor to determine 1095 creation

--- app/services/report_eligibility_processor.rb
+++ app/services/report_eligibility_processor.rb
@@ -1,11 +1,73 @@
 class ReportEligiblityProcessor 
 
-  def initialize(policy)
-    @policy = policy
+  def trigger_1095_creation
+    PolicyReportEligibilityUpdated.all.map(&:eg_id).map do |eg_id|
+      policy = Policy.where(eg_id: eg_id).first
+      if policy.present? 
+        if policy.aasm_state.in?(["canceled", "carrier_canceled"])
+          #send VOID if current policy is canceled and there are federal transmissions present
+          void_params = get_doc_params(policy, "void")
+          generate_1095A_pdf(void_params) if policy.federal_transmissions.present?
+        elsif policy.aasm_state.in?(["terminated", "submitted"])
+          corrected_params = get_doc_params(policy, "corrected")
+          original_params = get_doc_params(policy, "original")
+          #send CORRECTED 1095 if a transmission is present on a submitted/termed policy, send ORGINIAL if there isn't a transmission present
+          policy.federal_transmissions.present? ? generate_1095A_pdf(corrected_params) : generate_1095A_pdf(original_params) 
+        end
+      end
+    end
   end
 
-  def group_by_status
-
+  def get_doc_params(policy, type)
+    {
+      policy_id: policy.id,
+      type: type,
+      void_cancelled_policy_ids: get_void_cancelled_policy_ids(policy),
+      void_active_policy_ids: get_void_active_policy_ids(policy),
+      npt: policy.term_for_np
+    } 
   end
-  
+
+  def get_void_cancelled_policy_ids(policy)
+    policy.subscriber.policies.select do |policy|
+      base_conditional(policy) && policy.aasm_state == 'canceled'
+    end.map(&:id)
+  end
+
+  def base_conditional(policy)
+    policy.plan.year == Date.today.prev_year.year && 
+    policy.market == "individual" && 
+    policy.plan.coverage_type == "health" 
+  end
+
+  def get_void_active_policy_ids(policy)
+    policy.subscriber.policies.select do |policy|
+      base_conditional(policy) && policy.aasm_state.in?(['submitted','terminated'])
+    end.map(&:id)
+  end
+
+
+  def upload_to_s3(file_name, bucket_name)
+    Aws::S3Storage.save(file_name, bucket_name, File.basename(file_name))
+  end
+
+  def delete_1095A_pdf(file_name)
+    File.delete(file_name)
+  end
+
+  def generate_1095A_pdf(params)
+    params[:type] = 'new' if params[:type] == 'original'
+    file_name = Generators::Reports::IrsYearlySerializer.new(params).generate_notice
+    file_name
+    begin
+      if upload_to_s3(file_name, "tax-documents")
+        delete_1095A_pdf(file_name)
+      else
+        raise("File upload failed")
+      end
+      rescue Exception => e
+         e 
+    end
+  end
+
 end
---
+++ spec/services/report_eligibility_processor_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+require File.join(Rails.root, "script", "migrations", "person_family_creator")
+
+describe ReportEligiblityProcessor do
+  let(:plan) { instance_double(Plan, carrier: carrier, year: 2018, coverage_type: "health", hios_plan_id: "1212") }
+  let(:carrier) { instance_double(Carrier, abbrev: "GhmSi") }
+
+  ["submitted", 'terminated', 'canceled'].each_with_index do |status , i|
+    let!(:"#{status}_policy") {double(Policy, aasm_state: status, 
+    responsible_party_id:"",
+    id: (i + 1).to_s,
+    eg_id: (i + 1).to_s,
+    enrollees: [enrollee],
+    plan: plan,
+    spouse:"",
+    market: "individual",
+    term_for_np:false
+    )}
+  end
+
+  let(:policies) {[submitted_policy, terminated_policy, canceled_policy]}
+  let(:person) {double(Person, authority_member_id:"456")}
+  let(:member) {double(Member, person: person, id:"1233", hbx_member_id:"456")}
+  let(:enrollee) {double(Enrollee, m_id: member.hbx_member_id, rel_code: "self", coverage_start: Date.today.prev_year, coverage_end: (Date.today - 1.day))}
+  let(:federal_transmission) {double(FederalTransmission)}
+  let(:federal_transmissions) {[federal_transmission]}
+  let(:policy_report_eligibility_updateds) {[PolicyReportEligibilityUpdated]}
+  let(:policy_report_eligibility_updated_policy_eg_ids) {["1",'2','3']}
+  let(:federal_transmission){ double(FederalTransmission, policy: canceled_policy)}
+  let(:federal_transmissions){[federal_transmission]}
+  let(:void_params) {{:policy_id=>"3", :type=>"void", :void_cancelled_policy_ids=>["3"], :void_active_policy_ids=>["1", "2"], :npt=>false}}
+  let(:corrected_params) {{:policy_id=>"1", :type=>"corrected", :void_cancelled_policy_ids=>["3"], :void_active_policy_ids=>["1", "2"], :npt=>false}}
+  let(:original_params) {{:policy_id=>"2", :type=>"original", :void_cancelled_policy_ids=>["3"], :void_active_policy_ids=>["1", "2"], :npt=>false}}
+# 
+  subject { ReportEligiblityProcessor.new }
+
+  before(:each) do
+    allow(PolicyReportEligibilityUpdated).to receive(:all).and_return(policy_report_eligibility_updateds)
+    allow(policy_report_eligibility_updateds).to receive(:map).and_return(policy_report_eligibility_updated_policy_eg_ids)
+    allow(Policy).to receive(:where).with({:eg_id=>"1"}).and_return([submitted_policy])
+    allow(Policy).to receive(:where).with({:eg_id=>"2"}).and_return([terminated_policy])
+    allow(Policy).to receive(:where).with({:eg_id=>"3"}).and_return([canceled_policy])
+    allow(canceled_policy).to receive(:federal_transmissions).and_return(federal_transmissions)
+    allow(canceled_policy).to receive(:subscriber).and_return(enrollee)
+    allow(terminated_policy).to receive(:subscriber).and_return(enrollee)
+    allow(terminated_policy).to receive(:federal_transmissions).and_return(nil)
+    allow(submitted_policy).to receive(:federal_transmissions).and_return(federal_transmissions)
+    allow(submitted_policy).to receive(:subscriber).and_return(enrollee)
+    allow(canceled_policy).to receive(:canceled?).and_return(true)
+    allow(enrollee).to receive(:canceled?).and_return(false)
+    allow(enrollee).to receive(:policies).and_return(policies)
+    allow(enrollee).to receive(:person).and_return(person)
+    allow(person).to receive(:authority_member).and_return(member)
+    allow_any_instance_of(Generators::Reports::IrsYearlySerializer).to receive(:generate_notice).and_return("file_name")
+    allow(subject).to receive(:generate_1095A_pdf).with(void_params)
+    allow(subject).to receive(:generate_1095A_pdf).with(corrected_params)
+    allow(subject).to receive(:generate_1095A_pdf).with(original_params)
+
+    
+  end
+  
+  context "creating 1095s" do
+    it 'send the the correct params to the 1095 serializer' do
+      subject.trigger_1095_creation
+      expect(subject).to have_received(:generate_1095A_pdf).with(void_params)
+      expect(subject).to have_received(:generate_1095A_pdf).with(corrected_params)
+      expect(subject).to have_received(:generate_1095A_pdf).with(original_params)
+    end
+  end
+end
