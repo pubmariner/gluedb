e9fcca0bb311f451564fe0b9a4f8589b5a5f8ca3
Nikhil S <nikhilks219@gmail.com>
2019-09-06 09:47:39 -0400

    47049: removed unwanted info like event name and payload info from the model

--- app/models/policy_report_eligibility_updated.rb
+++ app/models/policy_report_eligibility_updated.rb
@@ -5,30 +5,26 @@ class PolicyReportEligibilityUpdated
   include Mongoid::Timestamps
 
   field :event_time, type: Time
-  field :event_name, type: String
-  field :resource_body, type: String
   field :policy_id, type: String
   field :eg_id, type: String
 
   XML_NS = "http://openhbx.org/api/terms/1.0"
 
-  index({event_time: 1, event_name: 1, policy_id: 1, eg_id: 1})
+  index({event_time: 1, policy_id: 1, eg_id: 1})
 
-  def self.newest_event?(new_policy_id, new_eg_id, new_event_name, new_event_time)
-    !self.where(:policy_id => new_policy_id, :event_name => new_event_name, :event_time => {"$gte" => new_event_time}).any?
+  def self.newest_event?(new_policy_id, new_eg_id, new_event_time)
+    !self.where(:policy_id => new_policy_id, :eg_id => new_eg_id, :event_time => {"$gte" => new_event_time}).any?
   end
 
   def self.not_yet_seen_by_carrier?(new_policy_id, new_eg_id)
     self.where(:policy_id => new_policy_id, :new_eg_id => new_eg_id).any?
   end
 
-  def self.create_new_event_and_remove_old(new_policy_id, new_eg_id, new_event_name, new_event_time, new_payload, match_criteria)
+  def self.create_new_event_and_remove_old(new_policy_id, new_eg_id, new_event_time, match_criteria)
     new_event = self.create!({
       policy_id: new_policy_id,
       eg_id: new_eg_id,
-      event_name: new_event_name,
-      event_time: new_event_time,
-      resource_body: new_payload
+      event_time: new_event_time
     })
     self.where(match_criteria.merge({:_id => {"$ne" => new_event._id}})).each do |old_record|
       yield old_record
@@ -36,7 +32,7 @@ class PolicyReportEligibilityUpdated
     end
   end
 
-  def self.store_and_yield_deleted(new_policy_id, new_eg_id, new_event_name, new_event_time, new_payload)
+  def self.store_and_yield_deleted(new_policy_id, new_eg_id, new_event_time)
     if not_yet_seen_by_carrier?(new_policy_id, new_eg_id)
       latest_time = ([new_event_time] + self.where(:policy_id => new_policy_id, :eg_id => new_eg_id).map(&:event_time)).max
       create_new_event_and_remove_old(
@@ -44,7 +40,6 @@ class PolicyReportEligibilityUpdated
         new_policy_id,
         new_eg_id,
         latest_time,
-        new_payload,
         {:policy_id => new_policy_id, :eg_id => new_eg_id}) do |old_record|
           yield old_record
       end
@@ -52,10 +47,8 @@ class PolicyReportEligibilityUpdated
       create_new_event_and_remove_old(
         new_policy_id,
         new_eg_id,
-        new_event_name,
         new_event_time,
-        new_payload,
-        {:policy_id => new_policy_id, :eg_id => new_eg_id, :event_name => new_event_name}) do |old_record|
+        {:policy_id => new_policy_id, :eg_id => new_eg_id}) do |old_record|
           yield old_record
       end
     end
