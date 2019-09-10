c26e67c199818e2cc4bf1708b397e5beb9b45a80
Zachary Landes <zach.g.landes@gmail.com>
2019-09-10 12:10:43 -0400

    updating processor to call h41 creation

--- app/services/report_eligibility_processor.rb
+++ app/services/report_eligibility_processor.rb
@@ -8,11 +8,11 @@ class ReportEligiblityProcessor
       if policy.present? 
         if policy.aasm_state.in?(["canceled", "carrier_canceled"])
           void_params = get_doc_params(policy, "void") 
-          transmit(void_params) if policy.federal_transmissions.present?# send VOID if current policy is canceled and there are federal transmissions present
+          transmit(void_params, policy) if policy.federal_transmissions.present?# send VOID if current policy is canceled and there are federal transmissions present
         elsif policy.aasm_state.in?(["terminated", "submitted"])
           corrected_params = get_doc_params(policy, "corrected")
           original_params = get_doc_params(policy, "original")
-          policy.federal_transmissions.present? ? transmit(corrected_params) : transmit(original_params) #send CORRECTED 1095 if a transmission is present on a submitted/termed policy, send ORGINIAL if there isn't a transmission present
+          policy.federal_transmissions.present? ? transmit(corrected_params, policy) : transmit(original_params, policy) #send CORRECTED 1095 if a transmission is present on a submitted/termed policy, send ORGINIAL if there isn't a transmission present
         end
       end
     end
@@ -46,21 +46,29 @@ class ReportEligiblityProcessor
     policy.plan.coverage_type == "health"
   end
 
-  def self.upload_to_s3(file_name, bucket_name)
-    Aws::S3Storage.save(file_name, bucket_name, File.basename(file_name), "h41")
+  def self.upload_to_s3(pfd_file, xml_file, bucket_name)
+    Aws::S3Storage.save(pfd_file, bucket_name, File.basename(file_name))
+    Aws::S3Storage.save(xml_file, bucket_name, File.basename(file_name), "h41")
   end
 
-  def self.publish_to_sftp(file_name, bucket_name)
-    Aws::S3Storage.publish_to_sftp(file_name, bucket_name, File.basename(file_name))
+  def self.publish_to_sftp(pfd_file, xml_file, bucket_name)
+    Aws::S3Storage.publish_to_sftp(pfd_file, bucket_name, File.basename(file_name))
+    Aws::S3Storage.publish_to_sftp(xml_file, bucket_name, File.basename(file_name))
   end
 
-  def self.delete_1095A_pdf(file_name)
-    File.delete(file_name)
+  def self.delete_tax_docs
+    File.delete(@pfd_file) if @pfd_file
+    File.delete(@xml_file) if @xml_file
   end 
 
   def self.generate_1095A_pdf(params)
     params[:type] = 'new' if params[:type] == 'original'
-    @file_name = Generators::Reports::IrsYearlySerializer.new(params).generate_notice
+    @pfd_file = Generators::Reports::IrsYearlySerializer.new(params).generate_notice
+  end
+  
+  def self.generate_h41_xml(params, policy)
+    #passing the true argument to trigger H41 instead of 1095A
+    @xml_file = Generators::Reports::IrsYearlySerializer.new(params, true).proccess_policy(policy)
   end
  
 
@@ -69,13 +77,13 @@ class ReportEligiblityProcessor
     federal_report.federal_report_ingester
   end
 
-  def self.transmit(params)
+  def self.transmit(params, policy)
     begin
-      if generate_1095A_pdf(params)
-         upload_to_s3(@file_name, "tax-documents")
-         publish_to_sftp(@file_name, "tax-documents")
+      if generate_1095A_pdf(params) && generate_h41_xml(params, policy)
+         upload_to_s3(@pfd_file, @xml_file, "tax-documents")
+         publish_to_sftp(@pfd_file, @xml_file, "tax-documents")
          persist_new_doc
-         delete_1095A_pdf(@file_name)
+         delete_tax_docs
          PolicyReportEligibilityUpdated.where(status: "processed").delete_all
       else
         raise("File upload failed")
--- spec/services/report_eligibility_processor_spec.rb
+++ spec/services/report_eligibility_processor_spec.rb
@@ -18,29 +18,28 @@ describe ReportEligiblityProcessor do
     )}
   end
 
-  let!(:records) {[ double(PolicyEvents::ReportingEligibilityUpdated,policy_id: submitted_policy.id),
-                    double(PolicyEvents::ReportingEligibilityUpdated,policy_id: terminated_policy.id),
-                    double(PolicyEvents::ReportingEligibilityUpdated,policy_id: canceled_policy.id)
+  let!(:records) {[ double(PolicyEvents::ReportingEligibilityUpdated, policy_id: submitted_policy.id),
+                    double(PolicyEvents::ReportingEligibilityUpdated, policy_id: terminated_policy.id),
+                    double(PolicyEvents::ReportingEligibilityUpdated, policy_id: canceled_policy.id)
                   ]}
 
-  let(:policies){[submitted_policy,terminated_policy,canceled_policy]}
+  let(:policies){[submitted_policy, terminated_policy, canceled_policy]}
   let(:person) {double(Person, authority_member_id:"456")}
   let(:member) {double(Member, person: person, id:"1233", hbx_member_id:"456")}
   let(:enrollee) {double(Enrollee, m_id: member.hbx_member_id, rel_code: "self", coverage_start: Date.today.prev_year, coverage_end: (Date.today - 1.day))}
   let(:federal_transmission) {double(FederalTransmission)}
   let(:federal_transmissions) {[federal_transmission]}
   let(:policy_report_eligibility_updateds) {[PolicyReportEligibilityUpdated]}
-  let(:policy_report_eligibility_updated_policy_ids) {[submitted_policy.id,terminated_policy.id,canceled_policy.id]}
+  let(:policy_report_eligibility_updated_policy_ids) {[submitted_policy.id, terminated_policy.id, canceled_policy.id]}
   let(:federal_transmission){ double(FederalTransmission, policy: canceled_policy)}
   let(:federal_transmissions){[federal_transmission]}
-  let(:void_params) {{:policy_id=> canceled_policy.id, :type=>"void", :void_cancelled_policy_ids=> [canceled_policy.id], :void_active_policy_ids=>[submitted_policy.id, terminated_policy.id], :npt=> false}}
-  let(:corrected_params) {{:policy_id=> submitted_policy.id, :type=>"corrected", :void_cancelled_policy_ids=> [canceled_policy.id], :void_active_policy_ids=>[submitted_policy.id, terminated_policy.id], :npt=> false}}
-  let(:original_params) {{:policy_id=> terminated_policy.id, :type=>"original", :void_cancelled_policy_ids=>[canceled_policy.id], :void_active_policy_ids=>[submitted_policy.id, terminated_policy.id], :npt=>false}}
+  let(:void_params) {{:policy_id=> canceled_policy.id, :type=>"void", :void_cancelled_policy_ids => [canceled_policy.id], :void_active_policy_ids => [submitted_policy.id, terminated_policy.id], :npt=> false}}
+  let(:corrected_params) {{:policy_id=> submitted_policy.id, :type=>"corrected", :void_cancelled_policy_ids => [canceled_policy.id], :void_active_policy_ids => [submitted_policy.id, terminated_policy.id], :npt=> false}}
+  let(:original_params) {{:policy_id=> terminated_policy.id, :type=>"original", :void_cancelled_policy_ids =>[canceled_policy.id], :void_active_policy_ids => [submitted_policy.id, terminated_policy.id], :npt=>false}}
 # 
   subject { ReportEligiblityProcessor }
 
   before(:each) do
-    allow(policies) {[]}
     allow(PolicyEvents::ReportingEligibilityUpdated).to receive(:events_for_processing).and_return(records)
     allow(Policy).to receive(:find).with(submitted_policy.id).and_return(submitted_policy)
     allow(Policy).to receive(:find).with(terminated_policy.id).and_return(terminated_policy)
@@ -56,14 +55,17 @@ describe ReportEligiblityProcessor do
     allow(enrollee).to receive(:policies).and_return(policies)
     allow(enrollee).to receive(:person).and_return(person)
     allow(person).to receive(:authority_member).and_return(member)
-    allow_any_instance_of(Generators::Reports::IrsYearlySerializer).to receive(:generate_notice).and_return("file_name")
     allow(subject).to receive(:upload_to_s3).and_return(true)
     allow(subject).to receive(:publish_to_sftp).and_return(true)
     allow(subject).to receive(:persist_new_doc).and_return(true)
     allow(subject).to receive(:persist_new_doc).and_return(true)
+    allow(subject).to receive(:delete_tax_docs).and_return(true)
     allow(subject).to receive(:generate_1095A_pdf).with(void_params).and_return(true)
     allow(subject).to receive(:generate_1095A_pdf).with(corrected_params).and_return(true)
     allow(subject).to receive(:generate_1095A_pdf).with(original_params).and_return(true) 
+    allow(subject).to receive(:generate_h41_xml).with(void_params, canceled_policy).and_return(true)
+    allow(subject).to receive(:generate_h41_xml).with(corrected_params, submitted_policy).and_return(true)
+    allow(subject).to receive(:generate_h41_xml).with(original_params, terminated_policy).and_return(true) 
   end
   
   context "creating 1095As" do
@@ -72,17 +74,17 @@ describe ReportEligiblityProcessor do
       allow(subject).to receive(:transmit)
       allow(subject).to receive(:transmit)  
       subject.run
-      expect(subject).to have_received(:transmit).with(void_params)
-      expect(subject).to have_received(:transmit).with(corrected_params)
-      expect(subject).to have_received(:transmit).with(original_params)
+      expect(subject).to have_received(:transmit).with(void_params, canceled_policy)
+      expect(subject).to have_received(:transmit).with(corrected_params,submitted_policy)
+      expect(subject).to have_received(:transmit).with(original_params, terminated_policy)
     end
 
-    it 'transmits the 1095A' do
+    it 'transmits the the files' do
       subject.run
       expect(subject).to have_received(:persist_new_doc).exactly(3).times
       expect(subject).to have_received(:upload_to_s3).exactly(3).times
       expect(subject).to have_received(:publish_to_sftp).exactly(3).times
+      expect(subject).to have_received(:delete_tax_docs).exactly(3).times
     end
-
   end
 end
