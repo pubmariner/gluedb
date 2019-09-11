c425e37c4c7642746bb54451990eaeb26ef6d6fb
Trey Evans <tevans@Just-Plain-Awful.local>
2019-09-11 16:51:31 -0400

    Finished with COR updates.

--- app/models/external_events/external_policy_cobra_switch.rb
+++ app/models/external_events/external_policy_cobra_switch.rb
@@ -43,7 +43,9 @@ module ExternalEvents
         :aasm_state => "submitted"
       })
       @existing_policy.hbx_enrollment_ids << extract_enrollment_group_id(@policy_node)
-      @existing_policy.save!
+      result = @existing_policy.save!
+      Observers::PolicyUpdated.notify(@existing_policy)
+      result
     end
 
     def update_enrollee(enrollee_node)
