e72cbd6ef57386a2fc9ba8465c202f32bdc1bc3a
Trey Evans <tevans@Just-Plain-Awful.local>
2019-09-12 13:10:40 -0400

    Fix routing key and make startup automatic.

--- script/amqp/employer_workers.rb
+++ script/amqp/employer_workers.rb
@@ -3,5 +3,6 @@ Rails.application.eager_load!
 MultiForkr.new({
  Listeners::EmployerEventReducerListener => 1,
  Listeners::EmployerUpdatedListener => 1,
- Listeners::EmployerDigestDropListener => 1
+ Listeners::EmployerDigestDropListener => 1,
+ Listeners::ReportEligibilityUpdatedReducerListener =>  1
 }).run
