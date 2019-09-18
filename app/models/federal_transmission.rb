4d82e4a233fa6885b275742d6a34288bb8024e65
Nikhil S <nikhilks219@gmail.com>
2019-09-18 15:30:50 -0400

    40530: added federal_transmission spec

--- app/models/federal_transmission.rb
+++ app/models/federal_transmission.rb
@@ -2,10 +2,17 @@ class FederalTransmission
   include Mongoid::Document
   include Mongoid::Timestamps::Created
 
-  field :report_type, type: String
+  REPORT_TYPES = %w[ORGINIAL CORRECTED VOIDED].freeze
+  
+  field :report_type, type: String, default: "ORGINIAL"
   field :batch_id, type: String
   field :content_file, type: String
   field :record_sequence_number, type: String
 
+  validates_presence_of :report_type, :batch_id, :content_file, :record_sequence_number
+
+  validates_format_of :content_file, :with => /^\d{5}$/, :message => "should be in the form 00001, 00002..."
+  
   embedded_in :policy
+
 end
