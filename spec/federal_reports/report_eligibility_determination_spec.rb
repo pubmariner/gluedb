72535b2fadc239504562cdee6251f61b9fb21f07
Zachary Landes <zach.g.landes@gmail.com>
2019-09-19 10:46:10 -0400

    splits processor into 3 seperate classes

--- spec/federal_reports/report_eligibility_determination_spec.rb
+++ spec/federal_reports/report_eligibility_determination_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe ::FederalReports::ReportEligiblityDetermination, :dbclean => :after_each do
   let(:plan) { FactoryGirl.create(:plan, carrier: carrier, year: 2018, coverage_type: "health", hios_plan_id: "1212") }
-  let(:plan) { FactoryGirl.create(:plan, carrier: carrier, year: 2018, coverage_type: "health", hios_plan_id: "1212") }
   let(:carrier) { FactoryGirl.create(:carrier, abbrev: "GhmSi") }
   ["submitted", 'terminated', 'canceled', 'effectuated'].each do |status|
     let!(:"#{status}_policy") {FactoryGirl.create(:policy, aasm_state: status, plan: plan, term_for_np: false ) }
@@ -28,40 +27,39 @@ describe ::FederalReports::ReportEligiblityDetermination, :dbclean => :after_eac
   before(:each) do
     Policy.all.each{ |policy| FactoryGirl.create(:person, authority_member_id: policy.subscriber.m_id)}
     allow(::PolicyEvents::ReportingEligibilityUpdated).to receive(:events_for_processing).and_return(records)
-    allow(::FederalReports::ReportProcessor).to receive(:transmit_cancelled_reports_for).with(canceled_policy).and_return(true)
-    allow(::FederalReports::ReportProcessor).to receive(:transmit_active_reports_for).with(submitted_policy).and_return(true)
-    allow(::FederalReports::ReportProcessor).to receive(:transmit_active_reports_for).with(terminated_policy).and_return(true)
+    allow(::FederalReports::ReportProcessor).to receive(:upload_canceled_reports_for).with(canceled_policy).and_return(true)
+    allow(::FederalReports::ReportProcessor).to receive(:upload_active_reports_for).with(submitted_policy).and_return(true)
+    allow(::FederalReports::ReportProcessor).to receive(:upload_active_reports_for).with(terminated_policy).and_return(true)
   end
   
   context "processing cancelled policies" do
-    it 'calls the transmit_cancelled_reports_for() if the policy is cancelled' do
+    it 'calls the upload_canceled_reports_for() if the policy is cancelled' do
       subject.determine_report_transmission_type
-    
-      expect(::FederalReports::ReportProcessor).to have_received(:transmit_cancelled_reports_for).with(canceled_policy)
-      expect(::FederalReports::ReportProcessor).to_not have_received(:transmit_cancelled_reports_for).with(terminated_policy)
-      expect(::FederalReports::ReportProcessor).to_not have_received(:transmit_cancelled_reports_for).with(submitted_policy)
+      expect(::FederalReports::ReportProcessor).to have_received(:upload_canceled_reports_for).with(canceled_policy)
+      expect(::FederalReports::ReportProcessor).to_not have_received(:upload_canceled_reports_for).with(terminated_policy)
+      expect(::FederalReports::ReportProcessor).to_not have_received(:upload_canceled_reports_for).with(submitted_policy)
     end
   end
 
   context "processing terminated policies" do
-    it 'calls the transmit_active_reports_for() if the policy is terminated' do
+    it 'calls the upload_active_reports_for() if the policy is terminated' do
       subject.determine_report_transmission_type
-      expect(::FederalReports::ReportProcessor).to have_received(:transmit_active_reports_for).with(terminated_policy)
-      expect(::FederalReports::ReportProcessor).to_not have_received(:transmit_active_reports_for).with(canceled_policy)
+      expect(::FederalReports::ReportProcessor).to have_received(:upload_active_reports_for).with(terminated_policy)
+      expect(::FederalReports::ReportProcessor).to_not have_received(:upload_active_reports_for).with(canceled_policy)
     end
   end
 
   context "processing submitted policies" do
-    it 'calls the transmit_active_reports_for() if the policy is submitted' do
+    it 'calls the upload_active_reports_for() if the policy is submitted' do
       subject.determine_report_transmission_type
-      expect(::FederalReports::ReportProcessor).to have_received(:transmit_active_reports_for).with(submitted_policy)
+      expect(::FederalReports::ReportProcessor).to have_received(:upload_active_reports_for).with(submitted_policy)
     end
   end
 
-  context "not processing unqualified policies" do
+  context "not process unqualified policies" do
     it 'does not call the ReportProcessor class if the policy does not quality' do
       subject.determine_report_transmission_type
-      expect(::FederalReports::ReportProcessor).to_not have_received(:transmit_active_reports_for).with(effectuated_policy)
+      expect(::FederalReports::ReportProcessor).to_not have_received(:upload_active_reports_for).with(effectuated_policy)
     end
   end
 end
--- spec/federal_reports/report_processor_spec.rb
+++ spec/federal_reports/report_processor_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
-describe ::FederalReports::ReportProcessor, :dbclean => :after_each do
+describe ::FederalReports::ReportProcessor, :dbclean => :around_each do
   let(:plan) { FactoryGirl.create(:plan, carrier: carrier, year: 2018, coverage_type: "health", hios_plan_id: "1212") }
   let(:plan) { FactoryGirl.create(:plan, carrier: carrier, year: 2018, coverage_type: "health", hios_plan_id: "1212") }
   let(:carrier) { FactoryGirl.create(:carrier, abbrev: "GhmSi") }
   ["submitted", 'terminated', 'canceled', 'effectuated'].each do |status|
-    let!(:"#{status}_policy") {FactoryGirl.create(:policy, aasm_state: status, plan: plan, term_for_np: false ) }
+    let(:"#{status}_policy") {FactoryGirl.create(:policy, aasm_state: status, plan: plan, term_for_np: false ) }
   end
-  let(:bad_policy) {FactoryGirl.create(:policy, aasm_state: "effectuated")}
   let!(:records) {[ double(policy_id: submitted_policy.id),
                     double(policy_id: terminated_policy.id),
                     double(policy_id: canceled_policy.id)
@@ -19,22 +18,90 @@ describe ::FederalReports::ReportProcessor, :dbclean => :after_each do
   let(:policy_report_eligibility_updated_policy_ids) {[submitted_policy.id, terminated_policy.id, canceled_policy.id]}
   let(:federal_transmission){ double(FederalTransmission, policy: canceled_policy)}
   let(:federal_transmissions){[federal_transmission]}
-  let(:void_params) {{:policy_id=> canceled_policy.id, :type=>"void", :void_cancelled_policy_ids => [canceled_policy.id], :void_active_policy_ids => [submitted_policy.id, terminated_policy.id], :npt=> false}}
-  let(:corrected_params) {{:policy_id=> submitted_policy.id, :type=>"corrected", :void_cancelled_policy_ids => [canceled_policy.id], :void_active_policy_ids => [submitted_policy.id, terminated_policy.id], :npt=> false}}
-  let(:original_params) {{:policy_id=> terminated_policy.id, :type=>"original", :void_cancelled_policy_ids =>[canceled_policy.id], :void_active_policy_ids => [submitted_policy.id, terminated_policy.id], :npt=>false}}
-  
+  let(:void_params) {{:policy_id=> canceled_policy.id, :type=>"void", :void_cancelled_policy_ids => [canceled_policy.id], :void_active_policy_ids => [], :npt=> false}}
+  let(:termed_corrected_params) {{:policy_id=> terminated_policy.id, :type=>"corrected", :void_cancelled_policy_ids => [], :void_active_policy_ids => [terminated_policy.id], :npt=> false}}
+  let(:termed_original_params) {{:policy_id=> terminated_policy.id, :type=>"original", :void_cancelled_policy_ids =>[], :void_active_policy_ids => [terminated_policy.id], :npt=>false}}
+  let(:submitted_corrected_params) {{:policy_id=> submitted_policy.id, :type=>"corrected", :void_cancelled_policy_ids => [canceled_policy.id], :void_active_policy_ids => [submitted_policy.id], :npt=> false}}
+  let(:submitted_original_params) {{:policy_id=> submitted_policy.id, :type=>"original", :void_cancelled_policy_ids =>[], :void_active_policy_ids => [submitted_policy.id], :npt=>false}}
+  let(:uploader){ ::FederalReports::ReportUploader.new }
   subject { ::FederalReports::ReportProcessor }
 
   before(:each) do
-    Policy.all.each{ |policy| FactoryGirl.create(:person, authority_member_id: policy.subscriber.m_id)}
+    submitted_policy
+    terminated_policy
+    canceled_policy
+    effectuated_policy
+    allow(::FederalReports::ReportUploader).to receive(:new).and_return(uploader)
+    allow(uploader).to receive(:upload).and_return(true)
+    Policy.all.each do  |policy| 
+      person = FactoryGirl.create(:person, authority_member_id: policy.subscriber.m_id)
+      policy.subscriber.update_attributes(m_id: person.authority_member_id)
+    end
   end
   
   context "processing cancelled policies" do
-    it 'calls the transmit_cancelled_reports_for() if the policy is cancelled' do
-      allow(::FederalReports::ReportUploader).to receive(:upload).with(void_params).and_return(true)
-      canceled_policy.federal_transmissions.create!
-      subject.transmit_cancelled_reports_for(canceled_policy)
-      expect(::FederalReports::ReportUploader).to_have received(:upload).with(void_params)
+    it 'calling the upload_canceled_reports_for() if the policy is cancelled but takes no action with no federal transmission' do
+      canceled_policy.subscriber.update_attributes(m_id: canceled_policy.subscriber.m_id )
+      canceled_policy.reload
+      subject.upload_canceled_reports_for(canceled_policy)
+      expect(uploader).to_not have_received(:upload).with(void_params)
+    end
+
+    it 'calling the upload_canceled_reports_for() if the policy is cancelled ' do
+      canceled_policy.federal_transmissions.create!(batch_id: "2017-02-08T14:00:00Z", content_file: "00001",record_sequence_number:"104618")
+      canceled_policy.subscriber.update_attributes(coverage_start: Time.mktime(2018,3,16))
+      canceled_policy.reload
+      subject.upload_canceled_reports_for(canceled_policy)
+      expect(uploader).to have_received(:upload).with(void_params)
+    end
+  end
+    
+  context "processing terminated policies" do
+    it 'calling the upload_active_reports_for() returns original params if the policy has no fed tranmissions' do
+      allow(uploader).to receive(:upload).with(termed_original_params).and_return(true)
+      terminated_policy.subscriber.update_attributes(coverage_start: Time.mktime(2018,3,16))
+      canceled_policy.subscriber.update_attributes(coverage_start: Time.mktime(2018,3,16))
+      canceled_policy.subscriber.update_attributes(m_id: terminated_policy.subscriber.m_id )
+      terminated_policy.reload
+      subject.upload_active_reports_for(terminated_policy)
+      expect(uploader).to have_received(:upload).with(termed_original_params)
+    end
+
+    it 'calling the upload_active_reports_for() returns original params if the policy has no fed tranmissions' do
+      allow(uploader).to receive(:upload).with(termed_corrected_params).and_return(true)
+      terminated_policy.subscriber.update_attributes(coverage_start: Time.mktime(2018,3,16))
+      canceled_policy.subscriber.update_attributes(coverage_start: Time.mktime(2018,3,16))
+      canceled_policy.subscriber.update_attributes(m_id: terminated_policy.subscriber.m_id )
+      terminated_policy.federal_transmissions.create!(batch_id: "2017-02-08T14:00:00Z", content_file: "00001",record_sequence_number:"104618")
+      terminated_policy.reload
+
+      subject.upload_active_reports_for(terminated_policy)
+      expect(uploader).to have_received(:upload).with(termed_corrected_params)
+    end
+  end
+
+  context "processing submitted policies" do
+    it 'calling the upload_active_reports_for() returns original params if the policy has fed tranmissions' do
+      allow(uploader).to receive(:upload).with(submitted_original_params).and_return(true)
+      submitted_policy.subscriber.update_attributes(coverage_start: Time.mktime(2018,3,16))
+      canceled_policy.subscriber.update_attributes(coverage_start: Time.mktime(2018,3,16))
+      canceled_policy.subscriber.update_attributes(m_id: submitted_policy.subscriber.m_id )
+      submitted_policy.reload
+      subject.upload_active_reports_for(submitted_policy)
+      expect(uploader).to have_received(:upload).with(submitted_original_params)
+    end
+
+    it 'calling the upload_active_reports_for() returns original params if the policy has no fed tranmissions' do
+      submitted_policy.reload
+      canceled_policy.reload
+      allow(uploader).to receive(:upload).with(submitted_corrected_params).and_return(true)
+      submitted_policy.subscriber.update_attributes(coverage_start: Time.mktime(2018,3,16))
+      canceled_policy.subscriber.update_attributes(coverage_start: Time.mktime(2018,3,16))
+      canceled_policy.subscriber.update_attributes(m_id: submitted_policy.subscriber.m_id )
+      submitted_policy.federal_transmissions.create!(batch_id: "2017-02-08T14:00:00Z", content_file: "00001",record_sequence_number:"104618")
+      submitted_policy.reload
+      subject.upload_active_reports_for(submitted_policy)
+      expect(uploader).to have_received(:upload).with(submitted_corrected_params)
     end
   end
 end
