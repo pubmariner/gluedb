6321887544b16425aa56a6ec4efede8147783a61
Zachary Landes <zach.g.landes@gmail.com>
2019-09-25 16:05:53 -0400

    adding call to notifer after h41 upload

---
+++ app/models/external_events/external_federal_reporting_notification.rb
@@ -0,0 +1,21 @@
+module ExternalEvents
+  class ExternalFederalReportingNotification
+#this class sends a message to EA that an h41 has been place on the s3 bucket
+    def self.notify(s3_response, policy, time = Time.now)
+      ::Amqp::EventBroadcaster.with_broadcaster do |b|
+        b.broadcast(
+          {
+            :headers => {
+              :file_name =>  s3_response[:object][:key],
+              :policy_id => policy.id,
+              :eg_id => policy.eg_id
+            },
+            :routing_key => "info.events.policy.federal_reporting_eligibility_updated"
+          },
+          ""
+        )
+      end
+    end
+
+  end
+end 
---
+++ spec/models/external_events/external_federal_reporting_notification_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe ExternalEvents::ExternalFederalReportingNotification do
+  let(:today) { Time.mktime(2025, 1, 1) }
+  let(:object) {{
+                  bucket: "examplebucket", 
+                  key: "HappyFace.jpg", 
+                }}
+  let(:uri) { "urn:openhbx:terms:v1:file_storage:s3:bucket:-tax-documents-preprod" }
+  let(:s3_response) { {object: object, uri: uri} }
+  let(:policy) { instance_double(Policy, eg_id: "1", id: "2")}
+  let(:event_broadcaster) { instance_double(Amqp::EventBroadcaster) }
+  let(:subject) { ExternalEvents::ExternalFederalReportingNotification   }
+  before(:each) do
+    allow(Amqp::EventBroadcaster).to receive(:with_broadcaster).and_yield(event_broadcaster)
+    allow(event_broadcaster).to receive(:broadcast).and_return(s3_response)
+  end
+
+  context "after receiving an s3 object" do
+    it "sends the message" do
+      ExternalEvents::ExternalFederalReportingNotification.notify(s3_response, policy, today)
+
+      expect(event_broadcaster).to have_received(:broadcast).with(
+        {
+          :headers => {
+            :file_name =>  s3_response[:object][:key],
+            :policy_id => policy.id,
+            :eg_id => policy.eg_id
+          },
+          :routing_key => "info.events.policy.federal_reporting_eligibility_updated"
+        },
+        ""
+      )
+    end
+  end
+
+end
