03456784f83f2ad427d1e0933908321e248ef62b
Zachary Landes <zach.g.landes@gmail.com>
2019-09-25 12:12:18 -0400

    updates file utils on processor

--- app/models/federal_reports/report_uploader.rb
+++ app/models/federal_reports/report_uploader.rb
@@ -4,29 +4,44 @@ module FederalReports
     def upload_1095(pdf_file, bucket_name)
       Aws::S3Storage.save(pdf_file, bucket_name, File.basename(pdf_file))
     end
+
+    def build_h41_unique_identifier(file)
+      if File.exists?(file)
+        id = Digest::SHA256.file(file).hexdigest[0..5]
+        unique_name = id + file
+        `cp #{file} #{unique_name}`
+        unique_name
+      else 
+        file
+      end
+    end
     
     def upload_h41(xml_file, bucket_name)
-      s3_result = Aws::S3Storage.save(xml_file, bucket_name, File.basename(xml_file), "h41")
-      Aws::S3Storage.publish_to_sftp(xml_file, bucket_name, s3_result[:uri])
-      ExternalEvents::ExternalFederalReportingNotification.notify(s3_result, @policy)
+      #file_name = build_h41_unique_identifier(xml_file) TO DO:build a unique id name to the file, this is not currently uploading the file 
+      s3_result = Aws::S3Storage.save(xml_file, bucket_name, xml_file, "h41")
+      Aws::S3Storage.publish_to_sftp(xml_file, "PushReportEligibilityUpdatedH41", s3_result[:uri])
+      # ExternalEvents::ExternalFederalReportingNotification.notify(s3_result, @policy)
     end
   
-    def remove_tax_docs
+    def remove_tax_docs    
+      FileUtils.rm_rf(Dir["FEP*"])
       FileUtils.rm_rf("#{Rails.root}/H41_federal_report") 
       FileUtils.rm_rf("#{Rails.root}/tmp/irs_notices")
-      base_name =  File.basename(@xml_file, ".zip")  
-      File.delete(base_name) if File.exists?(base_name)
       File.delete(@pdf_file) if File.exists?(@pdf_file)
       File.delete(@xml_file) if File.exists?(@xml_file)
     end 
-  
+    
     def generate_1095A_pdf(params)
+      
       params[:type] = 'new' if params[:type] == 'original'
       @pdf_file = Generators::Reports::IrsYearlySerializer.new(params).generate_notice
     end
     
     def generate_h41_xml(params)
       @xml_file = Generators::Reports::IrsYearlySerializer.new(params).generate_h41
+      #base_name =  File.basename(xml_file, ".zip")  
+      #`mv #{xml_file} #{base_name}` #remove zip file extension from xml file
+      #@xml_file = base_name
     end
     
     def persist_new_doc
@@ -40,10 +55,9 @@ module FederalReports
           generate_1095A_pdf(params)
           generate_h41_xml(params)
           upload_1095(@pdf_file, "tax-documents")
-          upload_h41(@xml_file, "tax-documents")
+          upload_h41(@xml_file, "internal-artifact-transport")
           persist_new_doc
           remove_tax_docs
-          ::PolicyEvents::ReportingEligibilityUpdated.where(status: "processed").delete_all
         rescue Exception => e
           raise FederalReports::ReportUploadError.new(params[:policy_id], e.message)
         end
