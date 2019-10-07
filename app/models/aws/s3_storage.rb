6449539b69ad993422e98a64e284339abb6f17d8
Zachary Landes <zach.g.landes@gmail.com>
2019-10-07 16:32:40 -0400

    removing zip extension from amqp message

--- app/models/aws/s3_storage.rb
+++ app/models/aws/s3_storage.rb
@@ -51,6 +51,7 @@ module Aws
       conn = AmqpConnectionProvider.start_connection
       eb = Amqp::EventBroadcaster.new(conn)
       aws_key = uri.split("#").last
+      filename =  File.basename(filename, ".zip")  
       props = {:headers => {:artifact_key => aws_key, :file_name => filename, :transport_process => transport_process}, :routing_key => "info.events.transport_artifact.transport_requested"}
       eb.broadcast(props, "payload")
       conn.close
