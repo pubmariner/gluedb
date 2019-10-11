056b70e8ba7a4c497388625b7920c2201d4b8d06
Nikhil S <nikhilks219@gmail.com>
2019-10-11 10:29:02 -0400

    40530:  additional check

--- app/models/generators/reports/importers/federal_report_ingester.rb
+++ app/models/generators/reports/importers/federal_report_ingester.rb
@@ -18,22 +18,21 @@ module Generators::Reports::Importers
     def federal_report_ingester
       Dir[EOY_DIRECTORY_FILES].each do |file_name|
         file_numbers = file_name.scan(/\d+/)
-        content_file_number = file_numbers.flatten.select { |fn| fn.length == 5 }.first
+        content_file_number = file_numbers.flatten.detect { |fn| fn.length == 5 }
         file = File.read(file_name)
         doc = Nokogiri::XML(file)
         doc.xpath('//air5.0:Form1095AUpstreamDetail').each do |node|
           record_sequence_number = node.xpath('./air5.0:RecordSequenceNum').text
           federal_policy_id = node.xpath('./air5.0:Policy/air5.0:MarketPlacePolicyNum').text
-  
           if record_sequence_number.present? && federal_policy_id.present? && content_file_number.present? && batch_id.present?
             policy = Policy.where(id: federal_policy_id).first
-            
             if policy.present?
               fed_trans = policy.federal_transmissions.create!(record_sequence_number: record_sequence_number,
                                                               content_file: content_file_number,
                                                               report_type: indicator,
                                                               batch_id: batch_id
                                                               )
+              policy.save!
               if fed_trans.present?
                 puts "New federal transmission record created for policy_id:#{federal_policy_id}" unless Rails.env.test?
               else 
@@ -42,6 +41,8 @@ module Generators::Reports::Importers
             else
               puts "No policy has been found with this policy_id:#{federal_policy_id}" unless Rails.env.test?
             end
+          else
+            puts "Please verify these values record_sequence_number: #{record_sequence_number}, content_file: #{content_file_number}, report_type: #{indicator}, batch_id: #{batch_id}" unless Rails.env.test?
           end
         end
       end
